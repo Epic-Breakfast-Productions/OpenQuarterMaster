e6180702ef8c18fd7f31bdf03ce85249812499fe
Greg Stewart <contact@gjstewart.net>
2021-02-28 09:43:56 -0500

    Initial dump of base quarkus project

---
+++ software/open-qm-base-station/src/native-test/java/com/ebp/openQuarterMaster/baseStation/NativeMyResourceIT.java
@@ -0,0 +1,9 @@
+package com.ebp.openQuarterMaster.baseStation;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeMyResourceIT extends MyResourceTest {
+
+    // Execute the same tests but in native mode.
+}
