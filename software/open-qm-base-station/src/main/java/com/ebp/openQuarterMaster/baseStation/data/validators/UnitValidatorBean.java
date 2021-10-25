0ca93f00faeb40b52976aa45038af75547f5a020
Greg Stewart <contact@gjstewart.net>
2021-10-25 14:28:37 -0400

    Finished work on inventory item validator, tweaking dependencies, playing with tests and crud interface

---
+++ software/open-qm-base-station/src/main/java/com/ebp/openQuarterMaster/baseStation/data/validators/UnitValidatorBean.java
@@ -0,0 +1,10 @@
+package com.ebp.openQuarterMaster.baseStation.data.validators;
+
+import com.ebp.openQuarterMaster.lib.core.validation.validators.InventoryItemValidator;
+import com.ebp.openQuarterMaster.lib.core.validation.validators.UnitValidator;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class UnitValidatorBean extends UnitValidator {
+}
