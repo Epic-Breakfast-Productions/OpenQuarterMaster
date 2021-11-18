1d62429ad7afd12aa596782a285bd14d0d0f659b
Greg Stewart <contact@gjstewart.net>
2021-11-18 10:26:18 -0500

    Added template for handling populating unit options

--- software/open-qm-base-station/src/main/java/com/ebp/openQuarterMaster/baseStation/ui/Storage.java
+++ software/open-qm-base-station/src/main/java/com/ebp/openQuarterMaster/baseStation/ui/Storage.java
@@ -6,6 +6,7 @@ import com.ebp.openQuarterMaster.baseStation.service.mongo.search.PagingOptions;
 import com.ebp.openQuarterMaster.baseStation.service.mongo.search.SearchResult;
 import com.ebp.openQuarterMaster.baseStation.service.mongo.search.SearchUtils;
 import com.ebp.openQuarterMaster.baseStation.service.mongo.search.SortType;
+import com.ebp.openQuarterMaster.lib.core.UnitUtils;
 import com.ebp.openQuarterMaster.lib.core.rest.user.UserGetResponse;
 import com.ebp.openQuarterMaster.lib.core.storage.StorageBlock;
 import com.ebp.openQuarterMaster.lib.core.storage.stored.StoredType;
@@ -88,6 +89,7 @@ public class Storage extends UiProvider {
         );
 
         return storage
+                .data("allowedUnitsMap", UnitUtils.ALLOWED_UNITS_MAP)
                 .data("userInfo", UserGetResponse.builder(user).build())
                 .data("showSearch", false)
                 .data("numStorageBlocks", storageBlockService.count())
