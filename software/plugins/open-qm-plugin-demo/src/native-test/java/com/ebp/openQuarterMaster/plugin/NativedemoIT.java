d0c026e7dc5ff6b120d28e8db7fd5e4c78921c7c
Greg Stewart <contact@gjstewart.net>
2021-11-29 11:59:24 -0500

    Added demo plugin for testing and demo purposes

---
+++ software/plugins/open-qm-plugin-demo/src/native-test/java/com/ebp/openQuarterMaster/plugin/NativedemoIT.java
@@ -0,0 +1,9 @@
+package com.ebp.openQuarterMaster.plugin;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativedemoIT extends demoTest {
+
+    // Execute the same tests but in native mode.
+}
