87083e0db7dcddc31de1dc22f065059618d3a5f1
Greg Stewart <contact@gjstewart.net>
2021-11-29 17:15:44 -0500

    Fixed native mode working, hopefully

--- software/open-qm-base-station/src/main/java/com/ebp/openQuarterMaster/baseStation/demo/DemoServiceCaller.java
+++ software/open-qm-base-station/src/main/java/com/ebp/openQuarterMaster/baseStation/demo/DemoServiceCaller.java
@@ -9,7 +9,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/demo")
-@RegisterRestClient
+@RegisterRestClient(configKey = "demoService")
 public interface DemoServiceCaller {
 
     @GET
