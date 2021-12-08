7733fb315fc91ad2691e4622b0511d352b6eda66
Greg Stewart <contact@gjstewart.net>
2021-12-08 16:47:01 -0500

    Now timeouts, async calls to demo service(s) p12

--- software/open-qm-base-station/src/main/java/com/ebp/openQuarterMaster/baseStation/demo/DemoServiceCaller.java
+++ software/open-qm-base-station/src/main/java/com/ebp/openQuarterMaster/baseStation/demo/DemoServiceCaller.java
@@ -7,6 +7,7 @@ import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.concurrent.CompletionStage;
 
 @Path("/demo")
 @RegisterRestClient(configKey = "demoService")
@@ -15,13 +16,13 @@ public interface DemoServiceCaller {
     @GET
     @Path("1")
     @Produces(MediaType.TEXT_PLAIN)
-//    CompletionStage<String> get1(@HeaderParam("authorization") String id);
-    String get1(@HeaderParam("authorization") String id);
+    CompletionStage<String> get1(@HeaderParam("authorization") String id);
+//    String get1(@HeaderParam("authorization") String id);
 
     @GET
     @Path("2")
     @Produces(MediaType.TEXT_PLAIN)
-//    CompletionStage<String> get2(@HeaderParam("authorization") String id);
-    String get2(@HeaderParam("authorization") String id);
+    CompletionStage<String> get2(@HeaderParam("authorization") String id);
+//    String get2(@HeaderParam("authorization") String id);
 
 }
--- software/open-qm-base-station/src/main/java/com/ebp/openQuarterMaster/baseStation/ui/Overview.java
+++ software/open-qm-base-station/src/main/java/com/ebp/openQuarterMaster/baseStation/ui/Overview.java
@@ -11,7 +11,6 @@ import io.quarkus.qute.Location;
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
 import lombok.extern.slf4j.Slf4j;
-import org.eclipse.microprofile.config.ConfigProvider;
 import org.eclipse.microprofile.jwt.JsonWebToken;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.eclipse.microprofile.openapi.annotations.tags.Tags;
@@ -27,6 +26,10 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.SecurityContext;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 @Traced
 @Slf4j
@@ -76,71 +79,71 @@ public class Overview extends UiProvider {
             String authHeaderContent = "Bearer " + this.jwt.getRawToken();
             log.info("Performing rest calls to demo services.");
             {
-//            java.util.Map<Integer, CompletableFuture<String>> completionStages = new HashMap<>(4);
-//
-//            completionStages.put(1, demoService.get1(authHeaderContent).toCompletableFuture());
-//            completionStages.put(2, demoService.get2(authHeaderContent).toCompletableFuture());
-//            completionStages.put(3, externDemoService.get1(authHeaderContent).toCompletableFuture());
-//            completionStages.put(4, externDemoService.get2(authHeaderContent).toCompletableFuture());
-//
-//            for (Map.Entry<Integer, CompletableFuture<String>> curStage : completionStages.entrySet()) {
-//                CompletableFuture<String> future = curStage.getValue();
-//
-//                String result = null;
-//                log.info("Waiting on call {}", curStage.getKey());
-//                try {
-//                    result = future.get(500, TimeUnit.MILLISECONDS);
-//                } catch (Throwable e) {
-//                    log.warn("Failed to make call {}: ", curStage.getKey(), e);
-//                    result = e.getMessage();
-//                }
-//                log.info("Got result from call {} - {}", curStage.getKey(), result);
-//
-//                switch (curStage.getKey()) {
-//                    case 1:
-//                        response1 = result;
-//                        break;
-//                    case 2:
-//                        response2 = result;
-//                        break;
-//                    case 3:
-//                        responseExt1 = result;
-//                        break;
-//                    case 4:
-//                        responseExt2 = result;
-//                        break;
-//                }
-//            }
+            java.util.Map<Integer, CompletableFuture<String>> completionStages = new HashMap<>(4);
+
+            completionStages.put(1, demoService.get1(authHeaderContent).toCompletableFuture());
+            completionStages.put(2, demoService.get2(authHeaderContent).toCompletableFuture());
+            completionStages.put(3, externDemoService.get1(authHeaderContent).toCompletableFuture());
+            completionStages.put(4, externDemoService.get2(authHeaderContent).toCompletableFuture());
+
+            for (Map.Entry<Integer, CompletableFuture<String>> curStage : completionStages.entrySet()) {
+                CompletableFuture<String> future = curStage.getValue();
+
+                String result = null;
+                log.info("Waiting on call {}", curStage.getKey());
+                try {
+                    result = future.get(500, TimeUnit.MILLISECONDS);
+                } catch (Throwable e) {
+                    log.warn("Failed to make call {}: ", curStage.getKey(), e);
+                    result = e.getMessage();
+                }
+                log.info("Got result from call {} - {}", curStage.getKey(), result);
+
+                switch (curStage.getKey()) {
+                    case 1:
+                        response1 = result;
+                        break;
+                    case 2:
+                        response2 = result;
+                        break;
+                    case 3:
+                        responseExt1 = result;
+                        break;
+                    case 4:
+                        responseExt2 = result;
+                        break;
+                }
+            }
             }
             {
-                if(ConfigProvider.getConfig().getValue("quarkus.rest-client.demoService.perform", Boolean.class)) {
-                    try {
-                        response1 = demoService.get1(authHeaderContent);
-                    } catch (Throwable e) {
-                        log.warn("Failed to reach service for 1: ", e);
-                        response1 = e.getMessage();
-                    }
-                    try {
-                        response2 = demoService.get2(authHeaderContent);
-                    } catch (Throwable e) {
-                        log.warn("Failed to reach service for 2: ", e);
-                        response2 = e.getMessage();
-                    }
-                }
-                if(ConfigProvider.getConfig().getValue("quarkus.rest-client.demoServiceExternal.perform", Boolean.class)) {
-                    try {
-                        responseExt1 = externDemoService.get1(authHeaderContent);
-                    } catch (Throwable e) {
-                        log.warn("Failed to reach service for external 1: ", e);
-                        responseExt1 = e.getMessage();
-                    }
-                    try {
-                        responseExt2 = externDemoService.get2(authHeaderContent);
-                    } catch (Throwable e) {
-                        log.warn("Failed to reach service for external 2: ", e);
-                        responseExt2 = e.getMessage();
-                    }
-                }
+//                if(ConfigProvider.getConfig().getValue("quarkus.rest-client.demoService.perform", Boolean.class)) {
+//                    try {
+//                        response1 = demoService.get1(authHeaderContent);
+//                    } catch (Throwable e) {
+//                        log.warn("Failed to reach service for 1: ", e);
+//                        response1 = e.getMessage();
+//                    }
+//                    try {
+//                        response2 = demoService.get2(authHeaderContent);
+//                    } catch (Throwable e) {
+//                        log.warn("Failed to reach service for 2: ", e);
+//                        response2 = e.getMessage();
+//                    }
+//                }
+//                if(ConfigProvider.getConfig().getValue("quarkus.rest-client.demoServiceExternal.perform", Boolean.class)) {
+//                    try {
+//                        responseExt1 = externDemoService.get1(authHeaderContent);
+//                    } catch (Throwable e) {
+//                        log.warn("Failed to reach service for external 1: ", e);
+//                        responseExt1 = e.getMessage();
+//                    }
+//                    try {
+//                        responseExt2 = externDemoService.get2(authHeaderContent);
+//                    } catch (Throwable e) {
+//                        log.warn("Failed to reach service for external 2: ", e);
+//                        responseExt2 = e.getMessage();
+//                    }
+//                }
             }
             log.info("Finished demo service calls: {}/{}/{}/{}", response1, response2, responseExt1, responseExt2);
         }
