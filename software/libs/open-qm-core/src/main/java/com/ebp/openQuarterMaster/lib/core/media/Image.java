5988ba754bdaf5b5f3eb79af4f43ebbb2078a456
Greg Stewart <contact@gjstewart.net>
2021-12-09 14:57:23 -0500

    Added implementation for image resizing, test for the resizing

--- software/libs/open-qm-core/src/main/java/com/ebp/openQuarterMaster/lib/core/media/Image.java
+++ software/libs/open-qm-core/src/main/java/com/ebp/openQuarterMaster/lib/core/media/Image.java
@@ -2,11 +2,22 @@ package com.ebp.openQuarterMaster.lib.core.media;
 
 import com.ebp.openQuarterMaster.lib.core.MainObject;
 import lombok.*;
+import org.apache.commons.io.output.WriterOutputStream;
 
+import javax.imageio.ImageIO;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.CharArrayWriter;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 /**
+ *
  */
 @EqualsAndHashCode(callSuper = true)
 @Data
@@ -16,7 +27,42 @@ import javax.validation.constraints.NotNull;
 public class Image extends MainObject {
     public static final int IMAGE_SIZE = 250;
     public static final String CONVERTED_IMAGE_FORMAT = "jpg";
+    private static final Base64.Encoder BASE_64_ENCODED = Base64.getEncoder();
 
+    public static BufferedImage resize(BufferedImage inputImage) {
+        // creates output image
+        BufferedImage outputImage = new BufferedImage(IMAGE_SIZE, IMAGE_SIZE, inputImage.getType());
+
+        // scales the input image to the output image
+        Graphics2D g2d = outputImage.createGraphics();
+        g2d.drawImage(inputImage, 0, 0, IMAGE_SIZE, IMAGE_SIZE, null);
+        g2d.dispose();
+
+        return outputImage;
+    }
+
+    public static char[] resizeGetBytes(BufferedImage image) {
+        BufferedImage resized = resize(image);
+
+        ByteBuffer byteBuffer;
+        {
+            CharBuffer charBuffer;
+            try (
+                    CharArrayWriter writer = new CharArrayWriter();
+                    WriterOutputStream os = new WriterOutputStream(writer, StandardCharsets.UTF_8);
+            ) {
+                ImageIO.write(resized, CONVERTED_IMAGE_FORMAT, os);
+                os.flush();
+
+                charBuffer = CharBuffer.wrap(writer.toCharArray());
+            } catch (IOException e) {
+                throw new IllegalStateException("Somehow failed to write in-memory.", e);
+            }
+            byteBuffer = StandardCharsets.UTF_8.encode(charBuffer);
+        }
+        byteBuffer = BASE_64_ENCODED.encode(byteBuffer);
+        return byteBuffer.asCharBuffer().array();
+    }
 
     @NonNull
     @NotNull
@@ -25,13 +71,13 @@ public class Image extends MainObject {
 
     /**
      * The image format of the data held.
-     *
+     * <p>
      * Examples:
      * <ul>
      *     <li>jpeg</li>
      *     <li>png</li>
      * </ul>
-     *
+     * <p>
      * TODO:: validator
      */
     private String type;
@@ -46,23 +92,16 @@ public class Image extends MainObject {
     @NotBlank
     private char[] data;
 
-//    public Image(String title, BufferedImage image){
-//        BufferedImage resized = new BufferedImage(IMAGE_SIZE, IMAGE_SIZE, image.getType());
-//
-//        CharArrayWriter writer = new CharArrayWriter();
-//
-//        //TODO:: finish
-//
-//        ImageIO.write(resized, CONVERTED_IMAGE_FORMAT, new WriterOutwriter);
-//        this(title, CONVERTED_IMAGE_FORMAT, os.toByteArray());
-//    }
+    public Image(String title, BufferedImage image) {
+        this(title, CONVERTED_IMAGE_FORMAT, resizeGetBytes(image));
+    }
 
     public String toDataString() {
+        //TODO:: research the best way to do this
         StringBuilder sb = new StringBuilder("data:image/");
         sb.append(this.getType());
         sb.append(";base64,");
         sb.append(this.getData());
-        //TODO:: research the best way to do this
         return sb.toString();
     }
 }
---
+++ software/libs/open-qm-core/src/test/java/com/ebp/openQuarterMaster/lib/core/media/ImageTest.java
@@ -0,0 +1,143 @@
+package com.ebp.openQuarterMaster.lib.core.media;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.time.StopWatch;
+import org.junit.jupiter.api.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Test image facts:
+ * <p>
+ * - center color: FF0000
+ * - TL quad:      00FF00
+ * - TR quad:      0000FF
+ * - BL quad:      000000
+ * - TR quad:      FFFF00
+ */
+@Slf4j
+class ImageTest {
+    private static final Color COLOR_CENTER = new Color(255, 0, 0);
+    private static final Color COLOR_TOP_LEFT = new Color(0, 255, 0);
+    private static final Color COLOR_TOP_RIGHT = new Color(0, 0, 255);
+    private static final Color COLOR_BOT_LEFT = new Color(255, 255, 255);
+    private static final Color COLOR_BOT_RIGHT = new Color(255, 255, 0);
+    private static final Color COLOR_CROSS = new Color(0, 0, 0);
+
+    private enum AtVal {
+        CENTER,
+        TOP_LEFT,
+        TOP_RIGHT,
+        BOT_LEFT,
+        BOT_RIGHT,
+        CENTER_LEFT,
+        CENTER_RIGHT,
+        CENTER_UP,
+        CENTER_DOWN
+    }
+
+    private static Color getColorAt(BufferedImage image, int x, int y) {
+        int clr = image.getRGB(x, y);
+        return new Color(
+                (clr & 0x00ff0000) >> 16,
+                (clr & 0x0000ff00) >> 8,
+                clr & 0x000000ff
+        );
+    }
+
+    private static void assertImageSameAt(BufferedImage imageOrig, BufferedImage imageOut, AtVal where) {
+        final int origHeight = imageOrig.getHeight() - 1,
+                origWidth = imageOrig.getWidth() - 1,
+                origXmid = imageOrig.getWidth() / 2,
+                origYmid = imageOrig.getHeight() / 2,
+
+                outHeight = imageOut.getHeight() - 1,
+                outWidth = imageOut.getWidth() - 1,
+                outXmid = imageOut.getWidth() / 2,
+                outYmid = imageOut.getHeight() / 2;
+
+
+        int origX = 0, origY = 0, outX = 0, outY = 0;
+        switch (where) {
+            case CENTER:
+                origX = origXmid;
+                origY = origYmid;
+                outX = outXmid;
+                outY = outYmid;
+                break;
+            case TOP_LEFT:
+//                origX = 0;
+//                origY = 0;
+//                outX = 0;
+//                outY = 0;
+                break;
+            case TOP_RIGHT:
+                origX = origWidth;
+//                origY = 0;
+                outX = outWidth;
+//                outY = 0;
+                break;
+            case BOT_LEFT:
+//                origX = 0;
+                origY = origHeight;
+//                outX = 0;
+                outY = outHeight;
+                break;
+            case BOT_RIGHT:
+                origX = origWidth;
+                origY = origHeight;
+                outX = outWidth;
+                outY = outHeight;
+                break;
+            case CENTER_LEFT:
+//                origX = 0;
+                origY = origYmid;
+//                outX = 0;
+                outY = outYmid;
+                break;
+            case CENTER_RIGHT:
+                origX = origWidth;
+                origY = origYmid;
+                outX = outWidth;
+                outY = outYmid;
+                break;
+            case CENTER_UP:
+                origX = origXmid;
+//                origY = 0;
+                outX = outXmid;
+//                outY = 0;
+                break;
+            case CENTER_DOWN:
+                origX = origXmid;
+                origY = origHeight;
+                outX = outXmid;
+                outY = outHeight;
+                break;
+        }
+
+        Color colorOrig = getColorAt(imageOrig, origX, origY);
+        Color colorOut = getColorAt(imageOut, outX, outY);
+        log.debug("Colors at {}({}:{}/{}:{}): {}/{}", where, origX, origY, outX, outY, colorOrig, colorOut);
+        assertEquals(colorOrig, colorOut);
+    }
+
+    @Test
+    public void resizeImageTest() throws IOException {
+        BufferedImage imageIn = ImageIO.read(ImageTest.class.getResourceAsStream("/test_image.png"));
+
+        StopWatch sw = StopWatch.createStarted();
+        BufferedImage imageOut = Image.resize(imageIn);
+        sw.stop();
+        log.info("Took {} to resize image.", sw);
+
+        for (AtVal cur : AtVal.values()) {
+            assertImageSameAt(imageIn, imageOut, cur);
+        }
+    }
+
+}
