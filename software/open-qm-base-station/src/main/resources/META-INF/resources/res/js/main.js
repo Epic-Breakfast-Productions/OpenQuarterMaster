c4cb74a8d97394083db6765048b9f5657dbcd8eb
Greg Stewart <contact@gjstewart.net>
2022-01-16 12:46:02 -0500

    Made dropdown to select what to search

--- software/open-qm-base-station/src/main/resources/META-INF/resources/res/js/main.js
+++ software/open-qm-base-station/src/main/resources/META-INF/resources/res/js/main.js
@@ -1,2 +1,11 @@
 
+var navSearchSelect = $('#navSearchSelect');
+var navSearchInput = $('#navSearchInput');
+var navSearchForm = $('#navSearchForm');
+
+function setNavSearch(){
+    var split = navSearchSelect.val().split(";");
+    navSearchForm.attr("action", split[0]);
+    navSearchInput.attr("name", split[1]);
+}
 
