8a6f750cf1c9be2178269c032ff55f8c4d296340
Greg Stewart <contact@gjstewart.net>
2022-01-27 15:21:15 -0500

    Got Preliminary storage tree visualization completed

--- software/open-qm-base-station/src/main/resources/META-INF/resources/res/js/storageBlockTree.js
+++ software/open-qm-base-station/src/main/resources/META-INF/resources/res/js/storageBlockTree.js
@@ -1,123 +1,74 @@
-var config = {
-        container: "#OrganiseChart1",
-        rootOrientation:  'NORTH', // NORTH || EAST || WEST || SOUTH
-        hideRootNode: true,
-        // levelSeparation: 30,
-        siblingSeparation:   40,
-        subTeeSeparation:    30,
-
-        connectors: {
-            type: 'curve'
-        },
+var MAIN_CONFIG = {
+    chart: {
+        container: "",
+        animateOnInit: false,
         node: {
-            HTMLclass: 'nodeExample1'
+            collapsable: true,
+            HTMLclass: 'storageTreeNode'
+        },
+        hideRootNode:true,
+        animation: {
+            nodeAnimation: "easeOutBounce",
+            nodeSpeed: 700,
+            connectorsAnimation: "bounce",
+            connectorsSpeed: 700
         }
     },
-    root = {},
-
-    cto = {
-        parent: root,
-        text:{
-            name: "Joe Linux",
-            title: "Board member",
-            contact: "email: we@aregreat.com"
-        },
-        stackChildren: true,
-        HTMLid: "coo"
-    },
-    cbo = {
-        parent: root,
-        stackChildren: true,
-        text:{
-            name: "Linda May",
-            title: "Board member",
-            contact: "email: we@aregreat.com"
-        },
-        HTMLid: "cbo"
-    },
-    cdo = {
-        parent: root,
-        text:{
-            name: "John Green",
-            title: "Board member, CEO",
-            contact: "email: we@aregreat.com"
-        },
-        HTMLid: "cdo"
-    },
-    cio = {
-        parent: cto,
-        text:{
-            name: "Ron Blomquist",
-            title: "Chief Information Security Officer",
-            contact: "email: we@aregreat.com"
-        },
-        HTMLid: "cio"
-    },
-    ciso = {
-        parent: cto,
-        text:{
-            name: "Michael Rubin",
-            title: "Chief Innovation Officer",
-            contact: "email: we@aregreat.com"
-        },
-        HTMLid: "ciso"
-    },
-    cio2 = {
-        parent: cdo,
-        text:{
-            name: "Erica Reel",
-            title: "Chief Customer Officer",
-            contact: "email: we@aregreat.com"
-        },
-        link: {
-            href: "www.google.com"
-        },
-        HTMLid: "cio2"
-    },
-    ciso2 = {
-        parent: cbo,
-        text:{
-            name: "Alice Lopez",
-            title: "Chief Communications Officer",
-            contact: "email: we@aregreat.com"
-        },
-        HTMLid: "ciso2"
-    },
-    ciso3 = {
-        parent: cbo,
-        text:{
-            name: "Mary Johnson",
-            title: "Chief Brand Officer",
-            contact: "email: we@aregreat.com"
-        },
-        HTMLid: "ciso2"
-    },
-    ciso4 = {
-        parent: cbo,
-        text:{
-            name: "Kirk Douglas",
-            title: "Chief Business Development Officer",
-            contact: "email: we@aregreat.com"
-        },
-        HTMLid: "ciso2"
+    nodeStructure: {
+        children: []
     }
+};
 
-    ALTERNATIVE = [
-        config,
-        root,
-        cto,
-        cbo,
-        cdo,
-        cio,
-        ciso,
-        cio2,
-        ciso2,
-        ciso3,
-        ciso4
-    ];
-
+function addChildrenToList(childrenList, curBlock){
+    var curNode = {
+            text: {
+                name: curBlock.blockLabel
+            },
+            children: []
+        };
+    if(curBlock.children){
+        curBlock.children.forEach(function(curChild, i){
+            addChildrenToList(curNode.children, curChild);
+        });
+    }
+    childrenList.push(curNode);
+}
 
 function showTree(containerSelector){
-    new Treant(ALTERNATIVE);
+    chartConfig = $.extend(true,{}, MAIN_CONFIG);
+
+    chartConfig.chart.container = containerSelector;
+
+//    new Treant(chartConfig);
+//    return;
+
+    doRestCall({
+//        spinnerContainer: $(containerSelector),
+        url: "/api/storage/tree",
+        done: function(data){
+            console.log("Successfully got tree data.");
+            var rootChildrenList = chartConfig.nodeStructure.children;
+            data.rootNodes.forEach(function(curRootNode, i){
+                addChildrenToList(rootChildrenList, curRootNode);
+            });
+
+            console.debug("Treant json: " + JSON.stringify(chartConfig));
+            new Treant(chartConfig);
+        },
+        fail: function(){}
+    });
+
+    // {
+//                       text: {name: "Lana"},
+//                       collapsed: true,
+//                       children: [
+//                           {
+//                               text: {name: "Figgis"}
+//                           }
+//                       ]
+//                   }
+
+
+
 }
 
