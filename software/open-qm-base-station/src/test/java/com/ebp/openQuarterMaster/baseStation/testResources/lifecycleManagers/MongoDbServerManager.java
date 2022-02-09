e5acf413f8ec1be2519202cb8c5b42cd4f4e6840
Greg Stewart <contact@gjstewart.net>
2022-02-09 15:32:52 -0500

    Now using webjars for some js dependencies

---
+++ software/open-qm-base-station/src/test/java/com/ebp/openQuarterMaster/baseStation/testResources/lifecycleManagers/MongoDbServerManager.java
@@ -0,0 +1,18 @@
+package com.ebp.openQuarterMaster.baseStation.testResources.lifecycleManagers;
+
+import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
+
+import java.util.Map;
+
+public class MongoDbServerManager implements QuarkusTestResourceLifecycleManager {
+
+    @Override
+    public Map<String, String> start() {
+        return null;
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
