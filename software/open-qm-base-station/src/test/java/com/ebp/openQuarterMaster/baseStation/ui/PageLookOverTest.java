eac5c7e1a91a2c53cbe80248b82b76777dfe1ed7
Greg Stewart <contact@gjstewart.net>
2022-02-25 13:00:43 -0500

    Rework of page lookover tests, how page scripts are added in template, added accordion storage entries to item form

--- software/open-qm-base-station/src/test/java/com/ebp/openQuarterMaster/baseStation/ui/PageLookOverTest.java
+++ software/open-qm-base-station/src/test/java/com/ebp/openQuarterMaster/baseStation/ui/PageLookOverTest.java
@@ -8,14 +8,28 @@ import com.ebp.openQuarterMaster.lib.core.user.User;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import javax.inject.Inject;
+import java.util.stream.Stream;
 
 @Slf4j
 @QuarkusTest
 @QuarkusTestResource(value = TestResourceLifecycleManager.class, restrictToAnnotatedClass = true)
 public class PageLookOverTest extends WebUiTest {
+	
+	public static Stream<Arguments> getPages(){
+		return Stream.of(
+			Arguments.of(false, "/"),
+			Arguments.of(false, "/accountCreate"),
+			Arguments.of(true,  "/overview"),
+			Arguments.of(true,  "/images"),
+			Arguments.of(true,  "/storage"),
+			Arguments.of(true,  "/items")
+		);
+	}
 	
 	@Inject
 	TestUserService testUserService;
@@ -23,36 +37,33 @@ public class PageLookOverTest extends WebUiTest {
 	@Inject
 	WebDriverWrapper webDriverWrapper;
 	
-	private static final String[] PRE_LOGIN_PAGES = new String[]{
-		"/accountCreate"
-	};
-	
-	private static final String[] LOGGED_IN_PAGES = new String[]{
-		"/overview",
-		"/images",
-		"/storage",
-		"/items"
-	};
-	
 	/**
 	 * Sanity-check type test to prove all pages load
 	 */
-	@Test
-	public void testPages() {
+	@ParameterizedTest
+	@MethodSource("getPages")
+	public void testPages(boolean loggedIn, String endpoint) {
 		User testUser = this.testUserService.getTestUser(true, true);
 		
-		for (String curPage : PRE_LOGIN_PAGES) {
-			log.info("Attempting page: {}", curPage);
-			this.webDriverWrapper.goTo(curPage);
-			this.webDriverWrapper.waitForPageLoad();
+		if(loggedIn){
+			log.info("Logging in user.");
+			this.webDriverWrapper.loginUser(testUser);
 		}
 		
-		this.webDriverWrapper.loginUser(testUser);
+		log.info("Attempting page: {}", endpoint);
+		this.webDriverWrapper.goTo(endpoint);
+		this.webDriverWrapper.waitForPageLoad();
 		
-		for (String curPage : LOGGED_IN_PAGES) {
-			log.info("Attempting page: {}", curPage);
-			this.webDriverWrapper.goTo(curPage);
-			this.webDriverWrapper.waitForPageLoad();
-		}
+//		List<LogEntry> errs = this.webDriverWrapper
+//			.getWebDriver()
+//			.manage()
+//			.logs()
+//			.get("err")
+//			.getAll();
+//
+//		assertEquals(
+//			Collections.emptyList(),
+//			errs
+//		);
 	}
 }
