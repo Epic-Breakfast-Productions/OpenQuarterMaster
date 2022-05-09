f592eb0a41fd2178d3cfa50b050cbec3b8b4239d
Greg Stewart <contact@gjstewart.net>
2022-05-09 11:04:51 -0400

    Cleanup of filters

--- software/open-qm-base-station/src/main/java/com/ebp/openQuarterMaster/baseStation/filters/LoggingFilter.java
+++ software/open-qm-base-station/src/main/java/com/ebp/openQuarterMaster/baseStation/filters/LoggingFilter.java
@@ -1,15 +1,10 @@
 package com.ebp.openQuarterMaster.baseStation.filters;
 
-import io.opentracing.Tracer;
 import io.vertx.core.http.HttpServerRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 
-import javax.inject.Inject;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
-import javax.ws.rs.container.ContainerResponseContext;
-import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.ext.Provider;
@@ -17,19 +12,13 @@ import java.io.IOException;
 
 @Slf4j
 @Provider
-public class LoggingFilter implements ContainerResponseFilter, ContainerRequestFilter {
+public class LoggingFilter implements ContainerRequestFilter {
 	
 	@Context
 	UriInfo uriInfo;
 	
 	@Context
 	HttpServerRequest request;
-	
-	@Inject
-	Tracer tracer;
-	
-	@ConfigProperty(name = "quarkus.jaeger.service-name")
-	String serviceId;
 	
 	@Override
 	public void filter(ContainerRequestContext requestContext) throws IOException {
@@ -44,16 +33,4 @@ public class LoggingFilter implements ContainerResponseFilter, ContainerRequestF
 		);
 		
 	}
-	
-	@Override
-	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
-		log.info(
-			"Response:: Type: {}  status: {}",
-			responseContext.getMediaType(),
-			responseContext.getStatus()
-		);
-		responseContext.getHeaders().add("serviceId", this.serviceId);
-		responseContext.getHeaders().add("traceId", this.tracer.activeSpan().context().toTraceId());
-	}
-	
 }
---
+++ software/open-qm-base-station/src/main/java/com/ebp/openQuarterMaster/baseStation/filters/TraceIdHeaderAdder.java
@@ -0,0 +1,29 @@
+package com.ebp.openQuarterMaster.baseStation.filters;
+
+import io.opentracing.Tracer;
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import javax.inject.Inject;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+@Slf4j
+@Provider
+public class TraceIdHeaderAdder implements ContainerResponseFilter {
+	
+	@Inject
+	Tracer tracer;
+	
+	@ConfigProperty(name = "quarkus.jaeger.service-name")
+	String serviceId;
+	
+	@Override
+	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+		responseContext.getHeaders().add("serviceId", this.serviceId);
+		responseContext.getHeaders().add("traceId", this.tracer.activeSpan().context().toTraceId());
+	}
+}
