fe3b3727e0fbfbd13b956b20b3582db30c979427
anixon <anixon@redhat.com>
2022-05-11 15:36:46 -0400

    Updated build script to match gregs silly version changes

--- build-all.sh
+++ build-all.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-MINOR_VERSION=82
+MINOR_VERSION=95
 if [ "$1" = "jvm" ]; then
 	VERSION=0.0.${MINOR_VERSION}-jvm
 	NAMESPACE=quartermaster-jvm
@@ -21,7 +21,8 @@ pushd software/plugins/open-qm-plugin-demo/
 if [ "$1" = "jvm" ]; then
 	./gradlew clean build -Pquarkus.container-image.build=true -x test -x javadoc
 else
-	./gradlew clean build -Pquarkus.container-image.build=true -Pquarkus.package.type=native -x test -x javadoc
+#	./gradlew clean build -Pquarkus.container-image.build=true -Pquarkus.package.type=native -x test -x javadoc
+        ./gradlew clean build -Pquarkus.package.type=native -Pquarkus.native.container-build=true -Pquarkus.jib.docker-executable-name=podman -x test -x javadoc
 fi
 popd
 
@@ -39,10 +40,10 @@ docker login ${REGISTRY} -u kubeadmin -p $(oc whoami -t)
 
 oc project ${NAMESPACE}
 
-docker tag root/open-qm-plugin-demo:1.0.0-DEV ${REGISTRY}/${NAMESPACE}/open-qm-plugin-demo:${VERSION}
+docker tag root/open-qm-plugin-demo:0.0.1 ${REGISTRY}/${NAMESPACE}/open-qm-plugin-demo:${VERSION}
 docker push ${REGISTRY}/${NAMESPACE}/open-qm-plugin-demo:${VERSION}
 
-docker tag root/open-qm-base-station:0.0.1 ${REGISTRY}/${NAMESPACE}/open-qm-base-station:${VERSION}
+docker tag root/open-qm-base-station:1.0.0-DEV ${REGISTRY}/${NAMESPACE}/open-qm-base-station:${VERSION}
 docker push ${REGISTRY}/${NAMESPACE}/open-qm-base-station:${VERSION}
 
 oc get deployment open-qm-plugin-demo -o yaml | sed -e 's# image: i.*# image: image-registry.openshift-image-registry.svc:5000/'${NAMESPACE}'/open-qm-plugin-demo:'${VERSION}'#g' | oc apply -f -
