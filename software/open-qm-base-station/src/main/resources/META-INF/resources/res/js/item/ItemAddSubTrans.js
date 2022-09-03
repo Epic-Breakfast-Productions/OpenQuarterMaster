1a26f53466a0a254fa3ad981736549cc768746e8
Greg Stewart <contact@gjstewart.net>
2022-09-03 09:24:55 -0400

    Item add/sub/trans form now loading data appropriately

--- software/open-qm-base-station/src/main/resources/META-INF/resources/res/js/item/ItemAddSubTrans.js
+++ software/open-qm-base-station/src/main/resources/META-INF/resources/res/js/item/ItemAddSubTrans.js
@@ -1,33 +1,138 @@
-
-
 var itemAddSubtractTransferModal = $("#itemAddSubtractTransferModal");
 var itemAddSubtractTransferForm = $("#itemAddSubtractTransferForm");
 var itemAddSubtractTransferFormItemImg = $("#itemAddSubtractTransferFormItemImg");
 var itemAddSubtractTransferFormItemNameLabel = $("#itemAddSubtractTransferFormItemNameLabel");
 var itemAddSubtractTransferFormItemIdInput = itemAddSubtractTransferForm.find("input[name=itemId]");
 var itemAddSubtractTransferFormItemTypeInput = itemAddSubtractTransferForm.find("input[name=itemStorageType]");
+var itemAddSubtractTransferOpSelect = $("#itemAddSubtractTransferOpSelect");
+var itemAddSubtractTransferFromSelect = $("#itemAddSubtractTransferFromSelect");
+var itemAddSubtractTransferFromSelectContainer = $("#itemAddSubtractTransferFromSelectContainer");
+var itemAddSubtractTransferToSelect = $("#itemAddSubtractTransferToSelect");
+var itemAddSubtractTransferToSelectContainer = $("#itemAddSubtractTransferToSelectContainer");
+var itemAddSubtractTransferFromControls = $("#itemAddSubtractTransferFromControls");
 
-function resetAddSubTransForm(){
+var itemAddSubtractTransferFormItemData = undefined;
+
+function resetAddSubTransForm() {
+	itemAddSubtractTransferFormItemData = undefined;
 	itemAddSubtractTransferFormItemNameLabel.text("");
 	itemAddSubtractTransferFormItemIdInput.val("");
 	itemAddSubtractTransferFormItemTypeInput.val("");
 	itemAddSubtractTransferFormItemImg.attr("src", "");
+	itemAddSubtractTransferOpSelect.val("add");
+	itemAddSubtractTransferFromSelect.html("");
+	itemAddSubtractTransferToSelect.html("");
+	itemAddSubtractTransferFromSelectContainer.hide();
+	itemAddSubtractTransferToSelectContainer.hide();
+	itemAddSubtractTransferFromControls.val("");
 }
 
+function getSelectedStorageBlock(selectInput){
+	return itemAddSubtractTransferFormItemData.storageMap[selectInput.val()];
+}
 
-function setupAddSubTransForm(itemId){
+function getSelectedFrom(){
+	return getSelectedStorageBlock(itemAddSubtractTransferFromSelect);
+}
+function getSelectedTo(){
+	return getSelectedStorageBlock(itemAddSubtractTransferFromSelect);
+}
+
+/**
+ * Fills form details
+ * @param data
+ */
+function fillAddSubTransFormControlsForAmountSimple() {
+	console.log("Setting up addSubTransForm controls for AMOUNT_SIMPLE");
+
+
+}
+
+function fillAddSubTransFormControlsForAmountList() {
+	console.log("Setting up addSubTransForm controls for AMOUNT_LIST");
+
+}
+
+function fillAddSubTransFormControlsForTracked() {
+	console.log("Setting up addSubTransForm controls for AMOUNT_TRACKED");
+}
+
+function fillAddSubTransFormControls(){
+	let itemType = itemAddSubtractTransferFormItemTypeInput.val();
+	switch (itemType) {
+		case "AMOUNT_SIMPLE":
+			fillAddSubTransFormControlsForAmountSimple();
+			break;
+		case "AMOUNT_LIST":
+			fillAddSubTransFormControlsForAmountList();
+			break;
+		case "TRACKED":
+			fillAddSubTransFormControlsForTracked();
+			break;
+	}
+}
+
+function setupAddSubTransFormFromToControls() {
+	let operationVal = itemAddSubtractTransferOpSelect.val();
+	console.log("Setting up addSubTransForm for " + operationVal);
+	switch (operationVal) {
+		case "add":
+			itemAddSubtractTransferFromSelectContainer.hide();
+			itemAddSubtractTransferToSelectContainer.show();
+			break;
+		case "subtract":
+			itemAddSubtractTransferFromSelectContainer.show();
+			itemAddSubtractTransferToSelectContainer.hide();
+			break;
+		case "transfer":
+			itemAddSubtractTransferToSelectContainer.show();
+			itemAddSubtractTransferFromSelectContainer.show();
+	}
+
+	fillAddSubTransFormControls();
+}
+
+/**
+ * Sets up form controls, including to/from based on
+ * @param data
+ */
+function resetAddSubTransFormActionChanged() {
+	Object.keys(itemAddSubtractTransferFormItemData.storageMap).forEach(curStorageId => {
+		let curSelectFrom = $("<option></option>");
+		let curSelectTo = $("<option></option>");
+		curSelectFrom.val(curStorageId);
+		curSelectTo.val(curStorageId);
+
+		getStorageBlockLabel(
+			curStorageId,
+			function (label) {
+				curSelectFrom.text(label);
+				curSelectTo.text(label);
+				itemAddSubtractTransferFromSelect.append(curSelectFrom);
+				itemAddSubtractTransferToSelect.append(curSelectTo);
+			}
+		);
+
+	});
+
+	setupAddSubTransFormFromToControls();
+}
+
+function setupAddSubTransForm(itemId) {
 	console.log("Setting up addSubTrans form for item " + itemId);
 	resetAddSubTransForm();
 
 	itemAddSubtractTransferFormItemImg.attr("src", "/api/media/image/for/item/" + itemId);
 
 	doRestCall({
-		spinnerContainer: itemAddSubtractTransferModal,
 		url: "/api/inventory/item/" + itemId,
 		done: function (data) {
-			itemAddSubtractTransferFormItemNameLabel.text(data.name);
-			itemAddSubtractTransferFormItemIdInput.val(data.id);
-			itemAddSubtractTransferFormItemTypeInput.val(data.storageType);
+			itemAddSubtractTransferFormItemData = data;
+			itemAddSubtractTransferFormItemNameLabel.text(itemAddSubtractTransferFormItemData.name);
+			itemAddSubtractTransferFormItemIdInput.val(itemAddSubtractTransferFormItemData.id);
+			itemAddSubtractTransferFormItemTypeInput.val(itemAddSubtractTransferFormItemData.storageType);
+			resetAddSubTransFormActionChanged();
 		}
 	});
 }
+
--- software/open-qm-base-station/src/main/resources/META-INF/resources/res/js/obj/getters.js
+++ software/open-qm-base-station/src/main/resources/META-INF/resources/res/js/obj/getters.js
@@ -1,19 +1,21 @@
-
-function getStorageBlockLabel(blockId, doneFunc){
-    doRestCall({
-        	spinnerContainer: null,
-        	url: "/api/storage/" + blockId,
-        	done: function(data){
-        	    doneFunc(data.label);
-        	}
-    });
+function getStorageBlockLabel(blockId, doneFunc) {
+	console.log("Getting label for storage block \"" + blockId + "\"");
+	doRestCall({
+		spinnerContainer: null,
+		url: "/api/inventory/storage-block/" + blockId,
+		done: function (data) {
+			console.log("Got label: " + data.label);
+			doneFunc(data.label);
+		}
+	});
 }
-function getImageName(imageId, doneFunc){
-    doRestCall({
-        	spinnerContainer: null,
-        	url: "/api/media/image/" + imageId,
-        	done: function(data){
-        	    doneFunc(data.title)
-        	}
-    });
+
+function getImageName(imageId, doneFunc) {
+	doRestCall({
+		spinnerContainer: null,
+		url: "/api/media/image/" + imageId,
+		done: function (data) {
+			doneFunc(data.title)
+		}
+	});
 }
