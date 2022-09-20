1d1f5906bb0700e33062ec2579c294b707cddf23
Greg Stewart <contact@gjstewart.net>
2022-09-20 18:09:19 -0400

    Core Lib- improvements to ExtItemLookupProviderInfo, version bump

--- software/libs/open-qm-core/src/main/java/tech/ebp/oqm/lib/core/rest/externalItemLookup/ExtItemLookupProviderInfo.java
+++ software/libs/open-qm-core/src/main/java/tech/ebp/oqm/lib/core/rest/externalItemLookup/ExtItemLookupProviderInfo.java
@@ -3,11 +3,15 @@ package tech.ebp.oqm.lib.core.rest.externalItemLookup;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
 import javax.validation.constraints.NotNull;
 import java.net.URL;
+import java.util.Comparator;
+
+import static java.lang.Boolean.compare;
 
 /**
  * Information about an external item lookup provider
@@ -16,7 +20,7 @@ import java.net.URL;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class ExtItemLookupProviderInfo {
+public class ExtItemLookupProviderInfo implements Comparable<ExtItemLookupProviderInfo> {
 	
 	@NotNull
 	@NonNull
@@ -24,17 +28,38 @@ public class ExtItemLookupProviderInfo {
 	
 	@NotNull
 	@NonNull
-	private String description;
+	@lombok.Builder.Default
+	private String description = "";
 	
 	@NotNull
 	@NonNull
 	private String cost;
 	
-	private boolean acceptsContributions;
+	@lombok.Builder.Default
+	private boolean acceptsContributions = false;
 	
 	@NotNull
 	@NonNull
 	private URL homepage;
 	
 	private boolean enabled;
+	
+	@Override
+	public int compareTo(ExtItemLookupProviderInfo other) {
+		return Comparator.INSTANCE.compare(this, other);
+	}
+	
+	public static class Comparator implements java.util.Comparator<ExtItemLookupProviderInfo> {
+		
+		public static final Comparator INSTANCE = new Comparator();
+		
+		@Override
+		public int compare(ExtItemLookupProviderInfo one, ExtItemLookupProviderInfo two) {
+			int enabledResult = Boolean.compare(two.isEnabled(), one.isEnabled());
+			if (enabledResult != 0) {
+				return enabledResult;
+			}
+			return one.getDisplayName().compareTo(two.getDisplayName());
+		}
+	}
 }
