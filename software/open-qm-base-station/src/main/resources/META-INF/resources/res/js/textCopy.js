ffb9522dfffd0e3c716cb13e804c39c1e3c77e53
Greg Stewart <contact@gjstewart.net>
2022-11-02 21:35:52 -0400

    Base Station- Added id copying to storage, item view

---
+++ software/open-qm-base-station/src/main/resources/META-INF/resources/res/js/textCopy.js
@@ -0,0 +1,16 @@
+
+function copyText(buttonClicked, textContainerId){
+    navigator.clipboard.writeText($("#"+textContainerId).text());
+
+    buttonClicked = $(buttonClicked);
+    let oldContent = buttonClicked.html();
+    console.log("old content: " + oldContent);
+    buttonClicked.html('<i class="fa-solid fa-clipboard-check"></i>');
+    setTimeout(
+        function (){
+            console.log("Setting copy symbol back.");
+            buttonClicked.html(oldContent);
+        },
+        5_000
+    );
+}
