bac0b880035c7f9a34bd73888dce10bb90c96c8a
Greg Stewart <contact@gjstewart.net>
2022-11-07 16:57:49 -0500

    Base Station- fixed item search

--- software/open-qm-base-station/src/main/java/tech/ebp/oqm/baseStation/rest/search/InventoryItemSearch.java
+++ software/open-qm-base-station/src/main/java/tech/ebp/oqm/baseStation/rest/search/InventoryItemSearch.java
@@ -2,10 +2,13 @@ package tech.ebp.oqm.baseStation.rest.search;
 
 import lombok.Getter;
 import lombok.ToString;
+import org.bson.conversions.Bson;
+import tech.ebp.oqm.baseStation.service.mongo.search.SearchUtils;
 import tech.ebp.oqm.lib.core.object.storage.items.InventoryItem;
 
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.QueryParam;
+import java.util.List;
 
 @ToString(callSuper = true)
 @Getter
@@ -19,4 +22,17 @@ public class InventoryItemSearch extends SearchKeyAttObject<InventoryItem> {
 	@HeaderParam("searchFormId") String searchFormIdHeaderVal;
 	@HeaderParam("inputIdPrepend") String inputIdPrependHeaderVal;
 	@HeaderParam("otherModalId") String otherModalIdHeaderVal;
+	
+	@Override
+	public List<Bson> getSearchFilters() {
+		List<Bson> filters = super.getSearchFilters();
+		
+		if (this.getName() != null && !this.getName().isBlank()) {
+			filters.add(
+				SearchUtils.getBasicSearchFilter("name", this.getName())
+			);
+		}
+		
+		return filters;
+	}
 }
