6dcaa36859679d2f2d18d25a22f020bd51c28fa9
Greg Stewart <contact@gjstewart.net>
2022-12-03 11:15:56 -0500

    BS-33 - Base Station- improved top bar search

--- software/open-qm-base-station/src/main/resources/META-INF/resources/res/js/main.js
+++ software/open-qm-base-station/src/main/resources/META-INF/resources/res/js/main.js
@@ -1,21 +1,26 @@
 
 var navSearchInput = $('#navSearchInput');
 var navSearchForm = $('#navSearchForm');
-var navSearchSelectButton = $('#navSearchSelectButton');
+var navSearchTypeSelect = $('#navSearchTypeSelect');
 
-function setNavSearch(action, name, caller){
-    navSearchForm.attr("action", action);
-    navSearchInput.attr("name", name);
-    navSearchSelectButton.text(caller.textContent);
-}
+navSearchTypeSelect.on("change", function(event){
+    console.log(
+        "Changing nav form to " +
+        "action: " + navSearchTypeSelect[0].options[event.target.selectedIndex].dataset.action +
+        " with fieldName: " + navSearchTypeSelect[0].options[event.target.selectedIndex].dataset.field
+    );
+    navSearchForm.attr("action", navSearchTypeSelect[0].options[event.target.selectedIndex].dataset.action);
+    navSearchInput.attr("name", navSearchTypeSelect[0].options[event.target.selectedIndex].dataset.field);
+});
 
-function setCheckboxState(checkbox, checked = false){
-    var check = checkbox.prop('checked');
-    if(checked) {
-        $('.checker').find('span').addClass('checked');
-        $('.checkbox').prop('checked', true);
-    } else {
-        $('.checker').find('span').removeClass('checked');
-        $('.checkbox').prop('checked', false);
-    }
-}
+//think this can be removed but not 100% sure
+// function setCheckboxState(checkbox, checked = false){
+//     var check = checkbox.prop('checked');
+//     if(checked) {
+//         $('.checker').find('span').addClass('checked');
+//         $('.checkbox').prop('checked', true);
+//     } else {
+//         $('.checker').find('span').removeClass('checked');
+//         $('.checkbox').prop('checked', false);
+//     }
+// }
