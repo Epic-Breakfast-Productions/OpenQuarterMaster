9a1f12d08026a90a71774012e516c4e0f4229ce2
Greg Stewart <contact@gjstewart.net>
2023-01-17 23:10:30 -0500

    Core lib- Added validator for interacting entity

--- software/libs/open-qm-core/src/main/java/tech/ebp/oqm/lib/core/object/interactingEntity/InteractingEntityReference.java
+++ software/libs/open-qm-core/src/main/java/tech/ebp/oqm/lib/core/object/interactingEntity/InteractingEntityReference.java
@@ -7,17 +7,20 @@ import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.Setter;
 import org.bson.types.ObjectId;
+import tech.ebp.oqm.lib.core.validation.annotations.ValidInteractingEntityReference;
 
 import javax.validation.constraints.NotNull;
 
+/**
+ * An identifier for an interacting entity.
+ */
 @Data
 @Setter(AccessLevel.PROTECTED)
 @AllArgsConstructor
 @NoArgsConstructor
+@ValidInteractingEntityReference
 public class InteractingEntityReference {
 	
-	@NotNull
-	@NonNull
 	private ObjectId entityId;
 	
 	@NotNull
---
+++ software/libs/open-qm-core/src/main/java/tech/ebp/oqm/lib/core/validation/annotations/ValidInteractingEntityReference.java
@@ -0,0 +1,34 @@
+package tech.ebp.oqm.lib.core.validation.annotations;
+
+import tech.ebp.oqm.lib.core.validation.validators.InteractingEntityReferenceValidator;
+import tech.ebp.oqm.lib.core.validation.validators.ServiceRoleValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * Custom annotation to check the validity of units.
+ * <p>
+ * https://docs.jboss.org/hibernate/validator/5.0/reference/en-US/html/validator-customconstraints.html#validator-customconstraints-validator
+ */
+@Target({ElementType.TYPE_USE, FIELD, METHOD, PARAMETER})
+@Retention(RUNTIME)
+@Constraint(validatedBy = InteractingEntityReferenceValidator.class)
+@Documented
+public @interface ValidInteractingEntityReference {
+	
+	String message() default "Invalid entity reference.";
+	
+	Class<?>[] groups() default {};
+	
+	Class<? extends Payload>[] payload() default {};
+}
