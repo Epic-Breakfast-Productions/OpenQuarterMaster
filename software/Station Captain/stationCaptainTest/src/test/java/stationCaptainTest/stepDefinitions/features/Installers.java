2b4bbfa70c8c9b06a23bb837db8134625f8c242d
Greg Stewart <contact@gjstewart.net>
2023-02-17 20:30:32 -0500

    Station Captain tests- Minor cleanup and adding intellij project files

--- software/Station Captain/stationCaptainTest/src/test/java/stationCaptainTest/stepDefinitions/features/Installers.java
+++ software/Station Captain/stationCaptainTest/src/test/java/stationCaptainTest/stepDefinitions/features/Installers.java
@@ -14,7 +14,6 @@ import java.nio.file.Path;
 import java.util.List;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Installers extends BaseStepDefinitions {
