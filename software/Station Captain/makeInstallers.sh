8b065f17b69b43f6e008fa2fa4266a00ffad20ce
Greg Stewart <contact@gjstewart.net>
2023-02-19 10:06:25 -0500

    Station Captain- features updates, slight refactor of capt script code, fixed empty jar warning in tests

--- software/Station Captain/makeInstallers.sh
+++ software/Station Captain/makeInstallers.sh
@@ -47,8 +47,8 @@ mkdir "$buildDir/$debDir/DEBIAN"
 mkdir "$buildDir/$debDir/bin"
 mkdir -p "$buildDir/$debDir/etc/oqm/static"
 
-cp oqm-captain.sh "$buildDir/$debDir/bin/oqm-captain"
-cp oqm-station-captain-help.txt "$buildDir/$debDir/etc/oqm/static/"
+cp src/oqm-captain.sh "$buildDir/$debDir/bin/oqm-captain"
+cp src/oqm-station-captain-help.txt "$buildDir/$debDir/etc/oqm/static/"
 
 # TODO:: license information https://www.debian.org/doc/packaging-manuals/copyright-format/1.0/
 # https://www.debian.org/doc/debian-policy/ch-controlfields.html#s-binarycontrolfiles
