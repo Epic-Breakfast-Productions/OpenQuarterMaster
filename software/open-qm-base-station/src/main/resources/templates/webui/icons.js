8fc0317805b31631d01ac066067f9cc68ffb468f
GregJohnStewart <contact@gjstewart.net>
2023-03-04 21:41:46 -0500

    Base Station- Added apply button with safety

--- software/open-qm-base-station/src/main/resources/templates/webui/icons.js
+++ software/open-qm-base-station/src/main/resources/templates/webui/icons.js
@@ -4,5 +4,7 @@ const Icons = {
 	copy: '{#icons/copy}{/icons/copy}',
 	copyChecked: '{#icons/copyChecked}{/icons/copyChecked}',
 	info: '{#icons/info}{/icons/info}',
+	locked: '{#icons/locked}{/icons/locked}',
 	remove: '{#icons/remove}{/icons/remove}',
+	unlocked: '{#icons/unlocked}{/icons/unlocked}',
 }
