8eb08e9a775460b075616a32c12051b34d2def35
GregJohnStewart <contact@gjstewart.net>
2023-04-30 10:23:43 -0400

    Base Station- Added barcode scanning library, implemented to fill in input

---
+++ software/open-qm-base-station/src/main/resources/META-INF/resources/res/js/CodeScanning.js
@@ -0,0 +1,47 @@
+/**
+ *
+ * Requires:
+ *
+ *  -
+ *
+ * @type {{scanningModal: (*|jQuery|HTMLElement), codeScanner}}
+ */
+const CodeScanner = {
+	scanningModal: $("#codeScannerModal"),
+	scanningModalObj: new bootstrap.Modal($("#codeScannerModal")),
+	codeScanner: new Html5QrcodeScanner(
+		"codeScannerRender",
+		{fps: 15, qrbox: {width: 250, height: 250}},
+		/* verbose= */ false
+	),
+	codeDestInput: null,
+	resetScanner(){
+		CodeScanner.codeScanner.clear();
+		CodeScanner.scanningModalObj.hide();
+	},
+	onScanSuccess(decodedText, decodedResult) {
+		// handle the scanned code as you like, for example:
+		console.log(`Code matched = ${decodedText}`, decodedResult);
+
+		if(CodeScanner.codeDestInput){
+			CodeScanner.codeDestInput.val(decodedText);
+		} else {
+			console.warn("No destination input!");
+		}
+		CodeScanner.resetScanner();
+
+	},
+	onScanFailure(error) {
+		console.warn(`Code scan error = ${error}`);
+	}
+};
+
+CodeScanner.scanningModal.on('hidden.bs.modal', event => {
+	CodeScanner.codeScanner.clear();
+	CodeScanner.codeDestInput = null;
+});
+CodeScanner.scanningModal.on('show.bs.modal', event => {
+	console.log("Opening Barcode Scanning Modal");
+	CodeScanner.codeScanner.render(CodeScanner.onScanSuccess, CodeScanner.onScanFailure);
+	CodeScanner.codeDestInput = $(event.relatedTarget.nextElementSibling);
+});
