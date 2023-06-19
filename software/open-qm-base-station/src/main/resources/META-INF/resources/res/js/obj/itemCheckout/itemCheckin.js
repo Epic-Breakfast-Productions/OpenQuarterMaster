d52fa5419c9966cb88db90061356b618131c8a29
GregJohnStewart <contact@gjstewart.net>
2023-06-19 17:31:05 -0400

    Base Station- More checkin form setup

--- software/open-qm-base-station/src/main/resources/META-INF/resources/res/js/obj/itemCheckout/itemCheckin.js
+++ software/open-qm-base-station/src/main/resources/META-INF/resources/res/js/obj/itemCheckout/itemCheckin.js
@@ -6,9 +6,41 @@ const ItemCheckin = {
 
 
 	resetCheckinForm(){
+		ItemCheckin.messages.text("");
+		ItemCheckin.itemNameLabel.text("");
+		ItemCheckin.originalStorageLabelLabel.text("");
+		ItemCheckin.storedDetails.text("");
 
 	},
-	setupCheckinForm(checkinId){
-		console.log("Checking in " + checkinId);
+	async setupCheckinForm(checkoutId){
+		console.log("Checking in " + checkoutId);
+		this.resetCheckinForm();
+
+		await doRestCall({
+
+			url: "/api/v1/inventory/item-checkout/" + checkoutId,
+			method: "GET",
+			async: false,
+			done: function (data) {
+				Getters.InventoryItem.getItemName(data.item, function (name){
+					ItemCheckin.itemNameLabel.text(name);
+				});
+				getStorageBlockLabel(data.checkedOutFrom, function (label){
+					ItemCheckin.originalStorageLabelLabel.text(label);
+				});
+				this.storedDetails.append(
+					ItemView.getStoredViewContent(
+						data.checkedOut,
+						data.item,
+						data.checkedOutFrom,
+						false,
+						false,
+						false,
+						true
+					)
+				);
+			},
+			failMessagesDiv: ItemCheckin.messages
+		});
 	}
 };
