f151c715b566e69931293205bf6728fec61cc733
GregJohnStewart <contact@gjstewart.net>
2023-06-22 20:06:25 -0400

    Base Station- Added checkout date, due back display to checkout view

--- software/open-qm-base-station/src/main/resources/META-INF/resources/res/js/obj/itemCheckout/itemCheckoutView.js
+++ software/open-qm-base-station/src/main/resources/META-INF/resources/res/js/obj/itemCheckout/itemCheckoutView.js
@@ -9,6 +9,9 @@ const ItemCheckoutView = {
 	checkedOut: $("#itemCheckoutViewCheckedOut"),
 	checkedOutByLabel: $("#itemCheckoutViewCheckedOutByLabel"),
 	checkedOutForLabel: $("#itemCheckoutViewCheckedOutForLabel"),
+	checkedOutOn: $("#itemCheckoutViewCheckedOutOn"),
+	dueBackOnContainer: $("#itemCheckoutViewDueBackOnContainer"),
+	dueBackOn: $("#itemCheckoutViewDueBackOn"),
 	history: $("#itemCheckoutViewHistoryAccordionCollapse"),
 	viewKeywordsSection: $("#itemCheckoutViewKeywordsSection"),
 	viewAttsSection: $("#itemCheckoutViewAttsSection"),
@@ -24,6 +27,10 @@ const ItemCheckoutView = {
 		ItemCheckoutView.statusLabelContainer.removeClass("bg-success");
 		ItemCheckoutView.checkedOutByLabel.text("");
 		ItemCheckoutView.checkedOutForLabel.text("");
+		ItemCheckoutView.checkedOutOn.text("");
+		ItemCheckoutView.dueBackOnContainer.hide();
+		ItemCheckoutView.dueBackOnContainer.removeClass("bg-danger")
+		ItemCheckoutView.dueBackOn.text("");
 		resetHistorySearch(ItemCheckoutView.history);
 		clearHideKeywordDisplay(ItemCheckoutView.viewKeywordsSection);
 		clearHideAttDisplay(ItemCheckoutView.viewAttsSection);
@@ -40,13 +47,23 @@ const ItemCheckoutView = {
 			method: "GET",
 			async: false,
 			failMessagesDiv: ItemCheckin.messages,
-			done: function (data) {
+			done: function (checkoutData) {
 				let promises = [];
 
-				processKeywordDisplay(ItemCheckoutView.viewKeywordsSection, data.keywords);
-				processAttDisplay(ItemCheckoutView.viewAttsSection, data.attributes);
+				processKeywordDisplay(ItemCheckoutView.viewKeywordsSection, checkoutData.keywords);
+				processAttDisplay(ItemCheckoutView.viewAttsSection, checkoutData.attributes);
+				ItemCheckoutView.checkedOutOn.text(checkoutData.checkoutDate);
 
-				if(data.stillCheckedOut){
+				if(checkoutData.dueBack){
+					ItemCheckoutView.dueBackOnContainer.show();
+					ItemCheckoutView.dueBackOn.text(checkoutData.dueBack);
+
+					if(luxon.DateTime.fromISO(checkoutData.dueBack) < luxon.DateTime.local()){
+						ItemCheckoutView.dueBackOnContainer.addClass("bg-danger");
+					}
+				}
+
+				if(checkoutData.stillCheckedOut){
 					ItemCheckoutView.statusLabel.html(Icons.itemCheckout + " Out");
 				} else {
 					ItemCheckoutView.statusLabel.html(Icons.itemCheckin + " In");
@@ -54,27 +71,27 @@ const ItemCheckoutView = {
 				}
 
 				promises.push(doRestCall({
-					url: "/api/v1/inventory/item/" + data.item,
+					url: "/api/v1/inventory/item/" + checkoutData.item,
 					method: "GET",
 					async: false,
 					failMessagesDiv: ItemCheckin.messages,
 					done: function (itemData) {
 						ItemCheckoutView.itemName.append(
-							Links.getItemViewLink(data.item, itemData.name)
+							Links.getItemViewLink(checkoutData.item, itemData.name)
 						);
 					}
 				}));
 
-				getStorageBlockLabel(data.checkedOutFrom, function (label){
+				getStorageBlockLabel(checkoutData.checkedOutFrom, function (label){
 					ItemCheckoutView.checkedOutFrom.append(
-						Links.getStorageViewLink(data.checkedOutFrom, label)
+						Links.getStorageViewLink(checkoutData.checkedOutFrom, label)
 					);
 				});
 				ItemCheckoutView.checkedOut.append(
 					StoredView.getStoredViewContent(
-						data.checkedOut,
-						data.item,
-						data.checkedOutFrom,
+						checkoutData.checkedOut,
+						checkoutData.item,
+						checkoutData.checkedOutFrom,
 						false,
 						false,
 						false,
@@ -82,8 +99,6 @@ const ItemCheckoutView = {
 					)
 				);
 
-				//TODO:: checkout date
-				//TODO:: checkin due date
 				//TODO:: Who for/by
 				//TODO:: if checked in, show details
 				//TODO:: reason, notes
