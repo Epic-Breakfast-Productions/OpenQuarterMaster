39f376a9325be2a523e2235a8f010441655019f6
GregJohnStewart <contact@gjstewart.net>
2023-07-02 20:54:00 -0400

    Base Station- Added new item checkout page to page lookover test

--- software/open-qm-base-station/src/main/java/tech/ebp/oqm/baseStation/interfaces/endpoints/inventory/ItemCheckoutCrud.java
+++ software/open-qm-base-station/src/main/java/tech/ebp/oqm/baseStation/interfaces/endpoints/inventory/ItemCheckoutCrud.java
@@ -381,7 +381,7 @@ public class ItemCheckoutCrud extends MainObjectProvider<ItemCheckout, ItemCheck
 	@RolesAllowed(Roles.INVENTORY_VIEW)
 	public Response getHistoryForObject(
 		@Context SecurityContext securityContext,
-		@org.jboss.resteasy.annotations.jaxrs.PathParam String id,
+		@PathParam("id") String id,
 		@BeanParam HistorySearch searchObject,
 		@HeaderParam("accept") String acceptHeaderVal,
 		@HeaderParam("searchFormId") String searchFormId
