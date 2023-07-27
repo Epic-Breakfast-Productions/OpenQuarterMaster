55e802d11374681be6b2c54bfe18af7f2b10de3d
GregJohnStewart <contact@gjstewart.net>
2023-07-26 20:49:29 -0400

    Base Station- Some movement towards new auth scheme

--- software/open-qm-base-station/src/main/java/tech/ebp/oqm/baseStation/interfaces/endpoints/externalService/ExternalServiceEp.java
+++ software/open-qm-base-station/src/main/java/tech/ebp/oqm/baseStation/interfaces/endpoints/externalService/ExternalServiceEp.java
@@ -1,223 +1,223 @@
-package tech.ebp.oqm.baseStation.interfaces.endpoints.externalService;
-
-import io.opentelemetry.instrumentation.annotations.WithSpan;
-import lombok.extern.slf4j.Slf4j;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-import org.eclipse.microprofile.openapi.annotations.Operation;
-import org.eclipse.microprofile.openapi.annotations.media.Content;
-import org.eclipse.microprofile.openapi.annotations.media.Schema;
-import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
-import org.eclipse.microprofile.openapi.annotations.tags.Tag;
-import org.eclipse.microprofile.openapi.annotations.tags.Tags;
-import tech.ebp.oqm.baseStation.config.ExtServicesConfig;
-import tech.ebp.oqm.baseStation.interfaces.endpoints.EndpointProvider;
-import tech.ebp.oqm.baseStation.service.JwtService;
-import tech.ebp.oqm.baseStation.service.PasswordService;
-import tech.ebp.oqm.baseStation.service.mongo.ExternalServiceService;
-import tech.ebp.oqm.baseStation.utils.AuthMode;
-import tech.ebp.oqm.lib.core.object.history.events.externalService.ExtServiceSetupEvent;
-import tech.ebp.oqm.lib.core.object.interactingEntity.externalService.ExternalService;
-import tech.ebp.oqm.lib.core.rest.ErrorMessage;
-import tech.ebp.oqm.lib.core.rest.auth.externalService.ExternalServiceLoginRequest;
-import tech.ebp.oqm.lib.core.rest.auth.roles.Roles;
-import tech.ebp.oqm.lib.core.rest.auth.user.UserLoginResponse;
-import tech.ebp.oqm.lib.core.rest.externalService.ExternalServiceSetupRequest;
-import tech.ebp.oqm.lib.core.rest.externalService.ExternalServiceSetupResponse;
-
-import javax.annotation.security.PermitAll;
-import javax.annotation.security.RolesAllowed;
-import javax.enterprise.context.RequestScoped;
-import javax.inject.Inject;
-import javax.validation.Valid;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.SecurityContext;
-
-import static tech.ebp.oqm.baseStation.interfaces.endpoints.EndpointProvider.ROOT_API_ENDPOINT_V1;
-
-@Slf4j
-@Path(ROOT_API_ENDPOINT_V1 + "/externalService")
-@Tags({@Tag(name = "External Service", description = "Endpoints for external services to manage their interactions with this server.")})
-@RequestScoped
-public class ExternalServiceEp extends EndpointProvider {
-	
-	
-	@ConfigProperty(name = "service.authMode")
-	AuthMode authMode;
-	
-	@Inject
-	ExtServicesConfig extServicesConfig;
-	
-	@Inject
-	ExternalServiceService externalServiceService;
-	
-	@Inject
-	PasswordService passwordService;
-	
-	@Inject
-	JwtService jwtService;
-	
-	@POST
-	@Path("setup/self")
-	@Operation(
-		summary = "Authenticates a user"
-	)
-	@APIResponse(
-		responseCode = "202",
-		description = "User was logged in.",
-		content = @Content(
-			mediaType = "application/json",
-			schema = @Schema(implementation = UserLoginResponse.class)
-		)
-	)
-	@APIResponse(
-		responseCode = "400",
-		description = "Incorrect credentials given.",
-		content = @Content(mediaType = "text/plain")
-	)
-	@APIResponse(
-		responseCode = "403",
-		description = "If the account has been locked.",
-		content = @Content(mediaType = "text/plain")
-	)
-	@APIResponse(
-		responseCode = "429",
-		description = "Happens when too many requests to login were sent in a given time period.",
-		content = @Content(mediaType = "text/plain")
-	)
-	@PermitAll
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response setupSelfAuthMode(
-		@Context SecurityContext securityContext,
-		@Valid ExternalServiceSetupRequest setupRequest
-	) {
-		assertSelfAuthMode(this.authMode);
-		
-		ExtServicesConfig.ExtServiceConfig serviceConfig = this.extServicesConfig.extServices().get(setupRequest.getName());
-		
-		if(serviceConfig == null){
-			return Response.status(Response.Status.BAD_REQUEST)
-					   .entity(new ErrorMessage("Service not found in available set."))
-					   .build();
-		}
-		if(!serviceConfig.secret().equals(setupRequest.getSecret())){
-			return Response.status(Response.Status.BAD_REQUEST)
-						   .entity(new ErrorMessage("Bad client secret."))
-						   .build();
-		}
-		
-		ExternalService existentExtService = this.externalServiceService.getFromSetupRequest(setupRequest);
-		
-		String newToken = this.passwordService.getRandString(this.extServicesConfig.secretSizeMin(), this.extServicesConfig.secretSizeMax());
-		
-		existentExtService.setSetupTokenHash(this.passwordService.createPasswordHash(newToken));
-		this.externalServiceService.update(existentExtService);
-		this.externalServiceService.addHistoryFor(
-			existentExtService,
-			null,
-			new ExtServiceSetupEvent(existentExtService, null)
-		);
-		
-		ExternalServiceSetupResponse.Builder<?, ?> builder = ExternalServiceSetupResponse.builder();
-		
-		builder.setupToken(newToken);
-		builder.id(existentExtService.getId());
-		
-		return Response.ok(builder.build()).build();
-	}
-	
-	@POST
-	@Path("setup/external")
-	@Operation(
-		summary = "Sets up an external service. Only call when AuthMode is EXTERNAL"
-	)
-	@APIResponse(
-		responseCode = "202",
-		description = "User was logged in.",
-		content = @Content(
-			mediaType = "application/json",
-			schema = @Schema(implementation = ExternalServiceSetupRequest.class)
-		)
-	)
-	@APIResponse(
-		responseCode = "400",
-		description = "Incorrect credentials given.",
-		content = @Content(mediaType = "text/plain")
-	)
-	@APIResponse(
-		responseCode = "403",
-		description = "If the account has been locked.",
-		content = @Content(mediaType = "text/plain")
-	)
-	@APIResponse(
-		responseCode = "429",
-		description = "Happens when too many requests to login were sent in a given time period.",
-		content = @Content(mediaType = "text/plain")
-	)
-	@RolesAllowed(Roles.EXT_SERVICE)
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response setupExternalAuthMode(
-		@Context SecurityContext securityContext,
-		@Valid ExternalServiceSetupRequest setupRequest
-	) {
-		assertExternalAuthMode(this.authMode);
-		//TODO:: BS-37
-		return Response.ok().build();
-	}
-	
-	@POST
-	@Path("auth")
-	@Operation(
-		summary = "Authenticates an external service"
-	)
-	@APIResponse(
-		responseCode = "202",
-		description = "User was logged in.",
-		content = @Content(
-			mediaType = "application/json",
-			schema = @Schema(implementation = ExternalServiceLoginRequest.class)
-		)
-	)
-	@APIResponse(
-		responseCode = "400",
-		description = "Incorrect credentials given.",
-		content = @Content(mediaType = "text/plain")
-	)
-	@APIResponse(
-		responseCode = "403",
-		description = "If the account has been locked.",
-		content = @Content(mediaType = "text/plain")
-	)
-	@APIResponse(
-		responseCode = "429",
-		description = "Happens when too many requests to login were sent in a given time period.",
-		content = @Content(mediaType = "text/plain")
-	)
-	@PermitAll
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response auth(
-		@Context SecurityContext securityContext,
-		@Valid ExternalServiceLoginRequest loginRequest
-	) {
-		assertSelfAuthMode(this.authMode);
-		
-		ExternalService service = this.externalServiceService.get(loginRequest.getId());
-		
-		if(!this.passwordService.passwordMatchesHash(service, loginRequest)){
-			log.warn("Service gave invalid token.");
-			return Response.status(Response.Status.BAD_REQUEST).entity("Bad token.").build();
-		}
-		
-		return Response.ok().entity(
-			this.jwtService.getExtServiceJwt(service)
-		).build();
-	}
-}
+//package tech.ebp.oqm.baseStation.interfaces.endpoints.externalService;
+//
+//import io.opentelemetry.instrumentation.annotations.WithSpan;
+//import lombok.extern.slf4j.Slf4j;
+//import org.eclipse.microprofile.config.inject.ConfigProperty;
+//import org.eclipse.microprofile.openapi.annotations.Operation;
+//import org.eclipse.microprofile.openapi.annotations.media.Content;
+//import org.eclipse.microprofile.openapi.annotations.media.Schema;
+//import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+//import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+//import org.eclipse.microprofile.openapi.annotations.tags.Tags;
+//import tech.ebp.oqm.baseStation.config.ExtServicesConfig;
+//import tech.ebp.oqm.baseStation.interfaces.endpoints.EndpointProvider;
+//import tech.ebp.oqm.baseStation.service.JwtService;
+//import tech.ebp.oqm.baseStation.service.PasswordService;
+//import tech.ebp.oqm.baseStation.service.mongo.ExternalServiceService;
+//import tech.ebp.oqm.baseStation.utils.AuthMode;
+//import tech.ebp.oqm.lib.core.object.history.events.externalService.ExtServiceSetupEvent;
+//import tech.ebp.oqm.lib.core.object.interactingEntity.externalService.ExternalService;
+//import tech.ebp.oqm.lib.core.rest.ErrorMessage;
+//import tech.ebp.oqm.lib.core.rest.auth.externalService.ExternalServiceLoginRequest;
+//import tech.ebp.oqm.lib.core.rest.auth.roles.Roles;
+//import tech.ebp.oqm.lib.core.rest.auth.user.UserLoginResponse;
+//import tech.ebp.oqm.lib.core.rest.externalService.ExternalServiceSetupRequest;
+//import tech.ebp.oqm.lib.core.rest.externalService.ExternalServiceSetupResponse;
+//
+//import javax.annotation.security.PermitAll;
+//import javax.annotation.security.RolesAllowed;
+//import javax.enterprise.context.RequestScoped;
+//import javax.inject.Inject;
+//import javax.validation.Valid;
+//import javax.ws.rs.Consumes;
+//import javax.ws.rs.POST;
+//import javax.ws.rs.Path;
+//import javax.ws.rs.Produces;
+//import javax.ws.rs.core.Context;
+//import javax.ws.rs.core.MediaType;
+//import javax.ws.rs.core.Response;
+//import javax.ws.rs.core.SecurityContext;
+//
+//import static tech.ebp.oqm.baseStation.interfaces.endpoints.EndpointProvider.ROOT_API_ENDPOINT_V1;
+//
+//@Slf4j
+//@Path(ROOT_API_ENDPOINT_V1 + "/externalService")
+//@Tags({@Tag(name = "External Service", description = "Endpoints for external services to manage their interactions with this server.")})
+//@RequestScoped
+//public class ExternalServiceEp extends EndpointProvider {
+//
+//
+//	@ConfigProperty(name = "service.authMode")
+//	AuthMode authMode;
+//
+//	@Inject
+//	ExtServicesConfig extServicesConfig;
+//
+//	@Inject
+//	ExternalServiceService externalServiceService;
+//
+//	@Inject
+//	PasswordService passwordService;
+//
+//	@Inject
+//	JwtService jwtService;
+//
+//	@POST
+//	@Path("setup/self")
+//	@Operation(
+//		summary = "Authenticates a user"
+//	)
+//	@APIResponse(
+//		responseCode = "202",
+//		description = "User was logged in.",
+//		content = @Content(
+//			mediaType = "application/json",
+//			schema = @Schema(implementation = UserLoginResponse.class)
+//		)
+//	)
+//	@APIResponse(
+//		responseCode = "400",
+//		description = "Incorrect credentials given.",
+//		content = @Content(mediaType = "text/plain")
+//	)
+//	@APIResponse(
+//		responseCode = "403",
+//		description = "If the account has been locked.",
+//		content = @Content(mediaType = "text/plain")
+//	)
+//	@APIResponse(
+//		responseCode = "429",
+//		description = "Happens when too many requests to login were sent in a given time period.",
+//		content = @Content(mediaType = "text/plain")
+//	)
+//	@PermitAll
+//	@Consumes(MediaType.APPLICATION_JSON)
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public Response setupSelfAuthMode(
+//		@Context SecurityContext securityContext,
+//		@Valid ExternalServiceSetupRequest setupRequest
+//	) {
+//		assertSelfAuthMode(this.authMode);
+//
+//		ExtServicesConfig.ExtServiceConfig serviceConfig = this.extServicesConfig.extServices().get(setupRequest.getName());
+//
+//		if(serviceConfig == null){
+//			return Response.status(Response.Status.BAD_REQUEST)
+//					   .entity(new ErrorMessage("Service not found in available set."))
+//					   .build();
+//		}
+//		if(!serviceConfig.secret().equals(setupRequest.getSecret())){
+//			return Response.status(Response.Status.BAD_REQUEST)
+//						   .entity(new ErrorMessage("Bad client secret."))
+//						   .build();
+//		}
+//
+//		ExternalService existentExtService = this.externalServiceService.getFromSetupRequest(setupRequest);
+//
+//		String newToken = this.passwordService.getRandString(this.extServicesConfig.secretSizeMin(), this.extServicesConfig.secretSizeMax());
+//
+//		existentExtService.setSetupTokenHash(this.passwordService.createPasswordHash(newToken));
+//		this.externalServiceService.update(existentExtService);
+//		this.externalServiceService.addHistoryFor(
+//			existentExtService,
+//			null,
+//			new ExtServiceSetupEvent(existentExtService, null)
+//		);
+//
+//		ExternalServiceSetupResponse.Builder<?, ?> builder = ExternalServiceSetupResponse.builder();
+//
+//		builder.setupToken(newToken);
+//		builder.id(existentExtService.getId());
+//
+//		return Response.ok(builder.build()).build();
+//	}
+//
+//	@POST
+//	@Path("setup/external")
+//	@Operation(
+//		summary = "Sets up an external service. Only call when AuthMode is EXTERNAL"
+//	)
+//	@APIResponse(
+//		responseCode = "202",
+//		description = "User was logged in.",
+//		content = @Content(
+//			mediaType = "application/json",
+//			schema = @Schema(implementation = ExternalServiceSetupRequest.class)
+//		)
+//	)
+//	@APIResponse(
+//		responseCode = "400",
+//		description = "Incorrect credentials given.",
+//		content = @Content(mediaType = "text/plain")
+//	)
+//	@APIResponse(
+//		responseCode = "403",
+//		description = "If the account has been locked.",
+//		content = @Content(mediaType = "text/plain")
+//	)
+//	@APIResponse(
+//		responseCode = "429",
+//		description = "Happens when too many requests to login were sent in a given time period.",
+//		content = @Content(mediaType = "text/plain")
+//	)
+//	@RolesAllowed(Roles.EXT_SERVICE)
+//	@Consumes(MediaType.APPLICATION_JSON)
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public Response setupExternalAuthMode(
+//		@Context SecurityContext securityContext,
+//		@Valid ExternalServiceSetupRequest setupRequest
+//	) {
+//		assertExternalAuthMode(this.authMode);
+//		//TODO:: BS-37
+//		return Response.ok().build();
+//	}
+//
+//	@POST
+//	@Path("auth")
+//	@Operation(
+//		summary = "Authenticates an external service"
+//	)
+//	@APIResponse(
+//		responseCode = "202",
+//		description = "User was logged in.",
+//		content = @Content(
+//			mediaType = "application/json",
+//			schema = @Schema(implementation = ExternalServiceLoginRequest.class)
+//		)
+//	)
+//	@APIResponse(
+//		responseCode = "400",
+//		description = "Incorrect credentials given.",
+//		content = @Content(mediaType = "text/plain")
+//	)
+//	@APIResponse(
+//		responseCode = "403",
+//		description = "If the account has been locked.",
+//		content = @Content(mediaType = "text/plain")
+//	)
+//	@APIResponse(
+//		responseCode = "429",
+//		description = "Happens when too many requests to login were sent in a given time period.",
+//		content = @Content(mediaType = "text/plain")
+//	)
+//	@PermitAll
+//	@Consumes(MediaType.APPLICATION_JSON)
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public Response auth(
+//		@Context SecurityContext securityContext,
+//		@Valid ExternalServiceLoginRequest loginRequest
+//	) {
+//		assertSelfAuthMode(this.authMode);
+//
+//		ExternalService service = this.externalServiceService.get(loginRequest.getId());
+//
+//		if(!this.passwordService.passwordMatchesHash(service, loginRequest)){
+//			log.warn("Service gave invalid token.");
+//			return Response.status(Response.Status.BAD_REQUEST).entity("Bad token.").build();
+//		}
+//
+//		return Response.ok().entity(
+//			this.jwtService.getExtServiceJwt(service)
+//		).build();
+//	}
+//}
--- software/open-qm-base-station/src/test/java/tech/ebp/oqm/baseStation/interfaces/endpoints/user/AuthTest.java
+++ software/open-qm-base-station/src/test/java/tech/ebp/oqm/baseStation/interfaces/endpoints/user/AuthTest.java
@@ -1,120 +1,119 @@
-package tech.ebp.oqm.baseStation.interfaces.endpoints.user;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.quarkus.test.common.QuarkusTestResource;
-import io.quarkus.test.common.http.TestHTTPEndpoint;
-import io.quarkus.test.junit.QuarkusTest;
-import io.restassured.http.ContentType;
-import io.restassured.response.ValidatableResponse;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
-import tech.ebp.oqm.baseStation.interfaces.endpoints.EndpointProvider;
-import tech.ebp.oqm.baseStation.interfaces.endpoints.auth.UserAuth;
-import tech.ebp.oqm.baseStation.testResources.TestRestUtils;
-import tech.ebp.oqm.baseStation.testResources.data.TestUserService;
-import tech.ebp.oqm.baseStation.testResources.lifecycleManagers.TestResourceLifecycleManager;
-import tech.ebp.oqm.baseStation.testResources.testClasses.RunningServerTest;
-import tech.ebp.oqm.lib.core.object.interactingEntity.user.User;
-import tech.ebp.oqm.lib.core.rest.ErrorMessage;
-import tech.ebp.oqm.lib.core.rest.auth.TokenCheckResponse;
-import tech.ebp.oqm.lib.core.rest.auth.user.UserLoginRequest;
-import tech.ebp.oqm.lib.core.rest.auth.user.UserLoginResponse;
-
-import javax.inject.Inject;
-import javax.ws.rs.core.Response;
-
-import static io.restassured.RestAssured.given;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-@Tag("integration")
-@Slf4j
-@QuarkusTest
-@QuarkusTestResource(TestResourceLifecycleManager.class)
-@TestHTTPEndpoint(UserAuth.class)
-class AuthTest extends RunningServerTest {
-	
-	@Inject
-	ObjectMapper objectMapper;
-	@Inject
-	TestUserService testUserService;
-	
-	@Test
-	public void testBadLoginNoUser() throws JsonProcessingException {
-		User testUser = this.testUserService.getTestUser(false, true);
-		
-		UserLoginRequest ulr = new UserLoginRequest("bad", "login", true);
-		ErrorMessage errorMessage = given()
-			.contentType(ContentType.JSON)
-			.body(objectMapper.writeValueAsString(ulr))
-			.when()
-			.post()
-			.then()
-			.statusCode(Response.Status.BAD_REQUEST.getStatusCode())
-			.extract().body().as(ErrorMessage.class);
-		
-		log.info("Error Message: {}", errorMessage);
-		assertEquals("User not found.", errorMessage.getDisplayMessage());
-	}
-	
-	@Test
-	public void testBadLoginBadPass() throws JsonProcessingException {
-		User testUser = this.testUserService.getTestUser(false, true);
-		
-		UserLoginRequest ulr = new UserLoginRequest(testUser.getEmail(), "badPass", true);
-		ErrorMessage errorMessage = given()
-			.contentType(ContentType.JSON)
-			.body(objectMapper.writeValueAsString(ulr))
-			.when()
-			.post()
-			.then()
-			.statusCode(Response.Status.BAD_REQUEST.getStatusCode())
-			.extract().body().as(ErrorMessage.class);
-		
-		log.info("Error Message: {}", errorMessage);
-		assertEquals("Invalid Password.", errorMessage.getDisplayMessage());
-	}
-	
-	@Test
-	public void testLogin() throws JsonProcessingException {
-		User testUser = this.testUserService.getTestUser(false, true);
-		
-		UserLoginRequest ulr = new UserLoginRequest(
-			testUser.getEmail(),
-			testUser.getAttributes().get(TestUserService.TEST_PASSWORD_ATT_KEY),
-			true
-		);
-		
-		UserLoginResponse loginResponse = given()
-			.contentType(ContentType.JSON)
-			.body(objectMapper.writeValueAsString(ulr))
-			.when()
-			.post()
-			.then()
-			.statusCode(Response.Status.ACCEPTED.getStatusCode())
-			.extract().body().as(UserLoginResponse.class);
-		
-		assertNotNull(loginResponse);
-		assertFalse(loginResponse.getToken().isBlank());
-		assertNotNull(loginResponse.getExpires());
-		
-		ValidatableResponse response = TestRestUtils.setupJwtCall(
-			given(),
-			loginResponse.getToken()
-		)
-													.contentType(ContentType.JSON)
-													.when()
-										   .basePath(EndpointProvider.ROOT_API_ENDPOINT_V1 + "/auth")
-													.get("/tokenCheck")
-													.then();
-		log.info("token check response: {}", response.extract().body().asString());
-		
-		response.statusCode(Response.Status.OK.getStatusCode());
-		TokenCheckResponse tokenCheckResponse = response.extract().body().as(TokenCheckResponse.class);
-		
-		log.info("Token Check Response: {}", tokenCheckResponse);
-	}
-}
+//package tech.ebp.oqm.baseStation.interfaces.endpoints.user;
+//
+//import com.fasterxml.jackson.core.JsonProcessingException;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import io.quarkus.test.common.QuarkusTestResource;
+//import io.quarkus.test.common.http.TestHTTPEndpoint;
+//import io.quarkus.test.junit.QuarkusTest;
+//import io.restassured.http.ContentType;
+//import io.restassured.response.ValidatableResponse;
+//import lombok.extern.slf4j.Slf4j;
+//import org.junit.jupiter.api.Tag;
+//import org.junit.jupiter.api.Test;
+//import tech.ebp.oqm.baseStation.interfaces.endpoints.EndpointProvider;
+//import tech.ebp.oqm.baseStation.testResources.TestRestUtils;
+//import tech.ebp.oqm.baseStation.testResources.data.TestUserService;
+//import tech.ebp.oqm.baseStation.testResources.lifecycleManagers.TestResourceLifecycleManager;
+//import tech.ebp.oqm.baseStation.testResources.testClasses.RunningServerTest;
+//import tech.ebp.oqm.lib.core.object.interactingEntity.user.User;
+//import tech.ebp.oqm.lib.core.rest.ErrorMessage;
+//import tech.ebp.oqm.lib.core.rest.auth.TokenCheckResponse;
+//import tech.ebp.oqm.lib.core.rest.auth.user.UserLoginRequest;
+//import tech.ebp.oqm.lib.core.rest.auth.user.UserLoginResponse;
+//
+//import javax.inject.Inject;
+//import javax.ws.rs.core.Response;
+//
+//import static io.restassured.RestAssured.given;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertFalse;
+//import static org.junit.jupiter.api.Assertions.assertNotNull;
+//
+//@Tag("integration")
+//@Slf4j
+//@QuarkusTest
+//@QuarkusTestResource(TestResourceLifecycleManager.class)
+//@TestHTTPEndpoint(UserAuth.class)
+//class AuthTest extends RunningServerTest {
+//
+//	@Inject
+//	ObjectMapper objectMapper;
+//	@Inject
+//	TestUserService testUserService;
+//
+//	@Test
+//	public void testBadLoginNoUser() throws JsonProcessingException {
+//		User testUser = this.testUserService.getTestUser(false, true);
+//
+//		UserLoginRequest ulr = new UserLoginRequest("bad", "login", true);
+//		ErrorMessage errorMessage = given()
+//			.contentType(ContentType.JSON)
+//			.body(objectMapper.writeValueAsString(ulr))
+//			.when()
+//			.post()
+//			.then()
+//			.statusCode(Response.Status.BAD_REQUEST.getStatusCode())
+//			.extract().body().as(ErrorMessage.class);
+//
+//		log.info("Error Message: {}", errorMessage);
+//		assertEquals("User not found.", errorMessage.getDisplayMessage());
+//	}
+//
+//	@Test
+//	public void testBadLoginBadPass() throws JsonProcessingException {
+//		User testUser = this.testUserService.getTestUser(false, true);
+//
+//		UserLoginRequest ulr = new UserLoginRequest(testUser.getEmail(), "badPass", true);
+//		ErrorMessage errorMessage = given()
+//			.contentType(ContentType.JSON)
+//			.body(objectMapper.writeValueAsString(ulr))
+//			.when()
+//			.post()
+//			.then()
+//			.statusCode(Response.Status.BAD_REQUEST.getStatusCode())
+//			.extract().body().as(ErrorMessage.class);
+//
+//		log.info("Error Message: {}", errorMessage);
+//		assertEquals("Invalid Password.", errorMessage.getDisplayMessage());
+//	}
+//
+//	@Test
+//	public void testLogin() throws JsonProcessingException {
+//		User testUser = this.testUserService.getTestUser(false, true);
+//
+//		UserLoginRequest ulr = new UserLoginRequest(
+//			testUser.getEmail(),
+//			testUser.getAttributes().get(TestUserService.TEST_PASSWORD_ATT_KEY),
+//			true
+//		);
+//
+//		UserLoginResponse loginResponse = given()
+//			.contentType(ContentType.JSON)
+//			.body(objectMapper.writeValueAsString(ulr))
+//			.when()
+//			.post()
+//			.then()
+//			.statusCode(Response.Status.ACCEPTED.getStatusCode())
+//			.extract().body().as(UserLoginResponse.class);
+//
+//		assertNotNull(loginResponse);
+//		assertFalse(loginResponse.getToken().isBlank());
+//		assertNotNull(loginResponse.getExpires());
+//
+//		ValidatableResponse response = TestRestUtils.setupJwtCall(
+//			given(),
+//			loginResponse.getToken()
+//		)
+//													.contentType(ContentType.JSON)
+//													.when()
+//										   .basePath(EndpointProvider.ROOT_API_ENDPOINT_V1 + "/auth")
+//													.get("/tokenCheck")
+//													.then();
+//		log.info("token check response: {}", response.extract().body().asString());
+//
+//		response.statusCode(Response.Status.OK.getStatusCode());
+//		TokenCheckResponse tokenCheckResponse = response.extract().body().as(TokenCheckResponse.class);
+//
+//		log.info("Token Check Response: {}", tokenCheckResponse);
+//	}
+//}
