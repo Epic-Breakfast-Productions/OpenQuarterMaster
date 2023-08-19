6959399d475705a60b95d2ffb348c6a0463d12f0
Greg Stewart <contact@gjstewart.net>
2023-08-18 20:21:39 -0400

    Added more json schemas, more tweaking towards working module code

---
+++ hardware/mss/mss/MssModInfo.h
@@ -0,0 +1,8 @@
+#ifndef MSS_MOD_INFO_H
+#define MSS_MOD_INFO_H
+
+
+class MssUsbConnector : public MssConnector {
+  public:
+};
+#endif
--- hardware/mss/mss/mss.ino
+++ hardware/mss/mss/mss.ino
@@ -1,26 +1,37 @@
 #include <FastLED.h>
 
 #define LED_PIN     2
-#define NUM_LEDS    32
+#define NUM_LEDS    24
+#define DELAY    500
 
 CRGB leds[NUM_LEDS];
 
 void setup() {
 
-  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
+    FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
 
 }
 
 void loop() {
 
-  for (int i = 0; i <= (NUM_LEDS - 1); i++) {
-    leds[i] = CRGB ( 0, 0, 255);
+    for (int i = 0; i <= (NUM_LEDS - 1); i += 3) {
+        leds[i] = CRGB(255, 0, 0);
+        leds[i+1] = CRGB(0, 255, 0);
+        leds[i+2] = CRGB(0, 0, 255);
+        FastLED.show();
+        delay(DELAY);
+    }
+
+    for (int i = 0; i <= (NUM_LEDS - 1); i++) {
+        leds[i] = CRGB(0, 0, 0);
+    }
     FastLED.show();
-    delay(100);
-  }
-  for (int i = (NUM_LEDS - 1); i >= 0; i--) {
-    leds[i] = CRGB ( 255, 0, 0);
-    FastLED.show();
-    delay(100);
-  }
+
+    for (int i = (NUM_LEDS - 1); i >= 0; i -= 3) {
+        leds[i] = CRGB(255, 0, 0);
+        leds[i-1] = CRGB(0, 255, 0);
+        leds[i-2] = CRGB(0, 0, 255);
+        FastLED.show();
+        delay(DELAY);
+    }
 }
