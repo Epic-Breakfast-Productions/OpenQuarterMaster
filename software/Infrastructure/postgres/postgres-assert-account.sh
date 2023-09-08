2c53a8ee3afa546d75c651483bfb17cca9026857
GregJohnStewart <contact@gjstewart.net>
2023-09-08 19:17:59 -0400

    Infra- Quick fix to postgres account creation script

--- software/Infrastructure/postgres/postgres-assert-account.sh
+++ software/Infrastructure/postgres/postgres-assert-account.sh
@@ -4,12 +4,13 @@ source /etc/oqm/accountScripts/account-assure-base.sh
 # https://stackoverflow.com/questions/14549270/check-if-database-exists-in-postgresql-using-shell
 
 # https://stackoverflow.com/questions/8546759/how-to-check-if-a-postgres-user-exists
-userExists=$(docker exec oqm_infra_postgres psql postgres -U "oqm-admin" -tXAc "SELECT 1 FROM pg_roles WHERE rolname='$usernameToAssert'")
+userExists=$(docker exec oqm_infra_postgres psql postgres -U "oqm_admin" -tXAc "SELECT 1 FROM pg_roles WHERE rolname='$usernameToAssert'")
 
 if [ "$userExists" == "1" ]; then
-#	echo "exists!"
+	echo "Account already exists."
 else
-#	echo "not exists!"
-	docker exec oqm_infra_postgres psql postgres -U "oqm-admin" -tXAc "CREATE USER $usernameToAssert WITH CREATEDB PASSWORD '$passwordToAssert'"
+	echo "Account did not exist. Creating account.."
+	docker exec oqm_infra_postgres psql postgres -U "oqm_admin" -tXAc "CREATE USER $usernameToAssert WITH CREATEDB PASSWORD '$passwordToAssert'"
+	echo "Account created."
 fi
 
