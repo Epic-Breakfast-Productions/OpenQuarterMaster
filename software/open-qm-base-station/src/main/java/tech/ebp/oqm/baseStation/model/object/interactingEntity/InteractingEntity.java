4861f607272661ac9fc6ad69cedb17e7a9342536
Greg Stewart <contact@gjstewart.net>
2023-09-13 22:31:32 -0400

    Base Station - Updated to allow for dumb service handling

--- software/open-qm-base-station/src/main/java/tech/ebp/oqm/baseStation/model/object/interactingEntity/InteractingEntity.java
+++ software/open-qm-base-station/src/main/java/tech/ebp/oqm/baseStation/model/object/interactingEntity/InteractingEntity.java
@@ -10,7 +10,9 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
 import org.bson.codecs.pojo.annotations.BsonDiscriminator;
+import org.eclipse.microprofile.jwt.Claims;
 import org.eclipse.microprofile.jwt.JsonWebToken;
 import tech.ebp.oqm.baseStation.model.object.AttKeywordMainObject;
 import tech.ebp.oqm.baseStation.model.object.interactingEntity.externalService.GeneralService;
@@ -20,7 +22,7 @@ import tech.ebp.oqm.baseStation.service.JwtUtils;
 
 import java.util.Set;
 
-
+@Slf4j
 @JsonTypeInfo(
 	use = JsonTypeInfo.Id.NAME,
 	include = JsonTypeInfo.As.EXISTING_PROPERTY, property = "interactingEntityType"
@@ -59,9 +61,15 @@ public abstract class InteractingEntity extends AttKeywordMainObject {
 	
 	public static InteractingEntity createEntity(JsonWebToken jwt){
 		InteractingEntity newEntity;
-		//TODO:: support services
 		
-		{
+		//TODO:: support services better.
+		if(((String)jwt.getClaim(Claims.upn)).startsWith("service-account-")){
+			GeneralService newService = new GeneralService();
+			
+			newService.setName(jwt.getClaim(Claims.upn));
+			
+			newEntity = newService;
+		} else {
 			User newUser = new User();
 			newEntity = newUser;
 			newUser.setEmail(JwtUtils.getEmail(jwt));
@@ -72,7 +80,7 @@ public abstract class InteractingEntity extends AttKeywordMainObject {
 		newEntity.setAuthProvider(jwt.getIssuer());
 		newEntity.setIdFromAuthProvider(jwt.getSubject());
 		
-		
+		log.debug("New entity: {}", newEntity);
 		return newEntity;
 	}
 	
