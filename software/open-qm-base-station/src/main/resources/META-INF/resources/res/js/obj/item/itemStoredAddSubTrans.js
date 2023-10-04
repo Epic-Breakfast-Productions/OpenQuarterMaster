357bbc8d57ab3eff27363ff06f5b16c5dfe103ef
GregJohnStewart <contact@gjstewart.net>
2023-10-04 00:27:16 -0400

    Base Station- Some more setup for dealing with the different types of items

--- software/open-qm-base-station/src/main/resources/META-INF/resources/res/js/obj/item/itemStoredAddSubTrans.js
+++ software/open-qm-base-station/src/main/resources/META-INF/resources/res/js/obj/item/itemStoredAddSubTrans.js
@@ -4,6 +4,7 @@ const ItemStoredAddSubTransfer = {
 	form: $("#itemStoredAddSubTransForm"),
 	formItemImg: $("#itemStoredAddSubTransFormItemImg"),
 	formItemNameLabel: $("#itemStoredAddSubTransFormItemNameLabel"),
+	toFromExistingStoredCheckbox: $("#itemStoredAddSubTransFormToFromExistingStoredCheckbox"),
 	toSelect: $("#itemStoredAddSubTransFormToSelect"),
 	fromSelect: $("#itemStoredAddSubTransFormFromSelect"),
 
@@ -12,6 +13,15 @@ const ItemStoredAddSubTransfer = {
 		this.formMessages.text("");
 		this.formItemNameLabel.text("");
 	},
+
+	disableExistingStoredOption(){
+		this.toFromExistingStoredCheckbox.prop('checked', false);
+		this.toFromExistingStoredCheckbox.disable();
+	},
+	enableExistingStoredOption(){
+		this.toFromExistingStoredCheckbox.enable();
+	},
+
 	setupForItem(itemId){
 		this.resetForms();
 		this.formItemImg.attr("src", "/api/v1/media/image/for/item/" + itemId);
@@ -25,6 +35,22 @@ const ItemStoredAddSubTransfer = {
 				//TODO:: check for no block ids
 
 				ItemStoredAddSubTransfer.setupFromToSelects(storageBLockIds);
+
+				StoredTypeUtils.foreachStoredType(
+					data.storageType,
+					function (){
+						ItemStoredAddSubTransfer.disableExistingStoredOption();
+						//TODO
+					},
+					function (){
+						ItemStoredAddSubTransfer.enableExistingStoredOption();
+						//TODO
+					},
+					function (){
+						ItemStoredAddSubTransfer.disableExistingStoredOption();
+						//TODO
+					}
+				);
 			}
 		});
 	},
