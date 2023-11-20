06f5ec6717af712a502f9e8478008282497ed794
GregJohnStewart <contact@gjstewart.net>
2023-11-19 22:18:55 -0500

    Base Station- Can now upload a file

--- software/open-qm-base-station/src/main/resources/META-INF/resources/res/js/obj/media/fileAttachment/FileAttachmentAddEdit.js
+++ software/open-qm-base-station/src/main/resources/META-INF/resources/res/js/obj/media/fileAttachment/FileAttachmentAddEdit.js
@@ -2,9 +2,11 @@ const FileAttachmentAddEdit = {
 	formMessages: $("#fileAttachmentAddEditFormMessages"),
 	form: $("#fileAttachmentAddEditForm"),
 	fileInput: $("#fileAttachmentAddEditFormFileInput"),
+	descriptionInput: $("#fileAttachmentAddEditFormDescriptionInput"),
 
 	resetForm(){
 		this.fileInput.val(null);
+		this.descriptionInput.val("");
 	},
 	setupForAdd(){
 		this.resetForm();
@@ -15,7 +17,26 @@ const FileAttachmentAddEdit = {
 		console.log("Submitting FileAttachmentAddEdit form.");
 
 		// https://docs.getform.io/installations/ajax/sending-submissions-with-jquery-ajax/#uploading-files-using-jquery-ajax
+		let formData = new FormData();
+		let file = FileAttachmentAddEdit.fileInput[0].files[0];
 
+		formData.append("fileName", file.name);
+		formData.append("file", file);
+		formData.append("description", FileAttachmentAddEdit.descriptionInput.val())
+
+		doRestCall({
+			url: '/api/v1/media/fileAttachments',
+			method: "post",
+			data: formData,
+			done: function (data){
+				console.log("Successfully added file attachment.");
+				FileAttachmentAddEdit.fileAttachmentAdded(data);
+			},
+			failMessagesDiv: FileAttachmentAddEdit.formMessages
+		});
+	},
+	fileAttachmentAdded(data) {
+		PageMessages.reloadPageWithMessage("Added item(s) successfully!", "success", "Success!");
 	}
 };
 
