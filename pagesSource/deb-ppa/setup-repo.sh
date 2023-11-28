56b5b8c7b1d702aa319371075c4ac077cb0dd4f3
GregJohnStewart <contact@gjstewart.net>
2023-11-27 22:59:34 -0500

    Station Captain - Error checking added to repo setup script

--- pagesSource/deb-ppa/setup-repo.sh
+++ pagesSource/deb-ppa/setup-repo.sh
@@ -4,14 +4,24 @@
 # get GPG key
 curl -s --compressed "https://deployment.openquartermaster.com/deb-ppa/KEY.gpg" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/oqm_ppa.gpg >/dev/null
 #add repo to list
-sudo curl -s --compressed -o /etc/apt/sources.list.d/oqm_file.list "https://deployment.openquartermaster.com/deb-ppa/deb_list_file.list"
+curl -s --compressed "https://deployment.openquartermaster.com/deb-ppa/deb_list_file.list" | sudo tee /etc/apt/sources.list.d/oqm_file.list
 # update apt and install
-sudo apt-get update && sudo apt-get install open+quarter+master-manager-station+captain
+sudo apt-get update
+if [ $? -ne 0 ]; then
+	echo "FAILED to update apt. See above output for information."
+	exit 1;
+fi
+
+sudo apt-get install open+quarter+master-manager-station+captain
+if [ $? -ne 0 ]; then
+	echo "FAILED to install Station Captain. See above output for information."
+	exit 2;
+fi
 
 clear
 
 echo "Setup of OQM repo and installation of OQM Captain utility complete."
-echo ""
-echo "Run `sudo oqm-captain` to get started."
+echo
+echo "Run 'sudo oqm-captain' to get started."
 echo
 
