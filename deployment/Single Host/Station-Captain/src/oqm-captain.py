8247c22c32faac8e5f3304d131cbb4828937a7c5
GregJohnStewart <contact@gjstewart.net>
2023-12-26 10:04:33 -0500

    Station Captain- Added command to package logs, instead of only through UI

--- deployment/Single Host/Station-Captain/src/oqm-captain.py
+++ deployment/Single Host/Station-Captain/src/oqm-captain.py
@@ -7,6 +7,7 @@ from ScriptInfos import ScriptInfo
 import UserInteraction
 from SnapshotUtils import *
 from ContainerUtils import *
+from LogManagement import *
 import argparse
 # This script manages an installation of Open QuarterMaster on a single host
 #
@@ -33,14 +34,17 @@ argParser.add_argument('-v', '--version', dest="v", action="store_true", help="G
 argParser.add_argument('--take-snapshot', dest="takeSnapshot", action="store_true", help="Takes a snapshot. Will pause and restart services.")
 argParser.add_argument('--prune-container-resources', dest="pruneContainerResources", action="store_true", help="Prunes all unused container resources. Roughly equivalent to running both `docker system prune --volumes` and `docker image prune -a`")
 argParser.add_argument('--ensure-container-setup', dest="ensureContainerSetup", action="store_true", help="Ensures all container based resources (i.e, network) are setup and ready.")
+argParser.add_argument('--package-logs', dest="packageLogs", action="store_true", help="Packages service logs for debugging.")
 args = argParser.parse_args()
 
 if args.v:
     print(ScriptInfo.SCRIPT_VERSION)
     exit(1)
+
 if not os.geteuid() == 0:
     print("\n\nPlease run this script as root. ( sudo oqm-captain )\n")
     exit(2)
+
 if args.takeSnapshot:
     trigger = SnapshotTrigger.manual
     if args.takeSnapshot[0]:
@@ -50,5 +54,11 @@ if args.pruneContainerResources:
     ContainerUtils.pruneContainerResources()
 if args.ensureContainerSetup:
     ContainerUtils.ensureSharedDockerResources()
+if args.packageLogs:
+    result, message = LogManagement.packageLogs()
+    if not result:
+        print("Failed to package logs: " + message)
+        exit(2)
+    print(message)
 else:
     UserInteraction.ui.startUserInteraction()
