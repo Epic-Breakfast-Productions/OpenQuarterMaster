3898d5c2a8de819cca3079770ff4bd770e0c5309
GregJohnStewart <contact@gjstewart.net>
2023-12-29 14:16:27 -0500

    Updated test dev repos

--- pagesSource/repos/test-dev/deb/setup-repo.sh
+++ pagesSource/repos/test-dev/deb/setup-repo.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
-# Script to setup the Debian OQM repo and install oqm-captain
+#Script to setup the Debian OQM repo and install oqm-captain
+
+SUDOTXT=""
+if [ "1000" -ne 0 ]; then
+  SUDOTXT="sudo"
+fi
+
+AUTO_INSTALL=""
+if [ "" == "--auto" ]; then
+  AUTO_INSTALL="-y"
+fi
+
 
 # get GPG key
-curl -s --compressed "https://deployment.openquartermaster.com/repos/test-dev/deb/KEY.gpg" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/oqm_ppa.gpg >/dev/null
+#curl -s --compressed "https://deployment.openquartermaster.com/repos/test-dev/deb/KEY.gpg" | gpg --dearmor |  tee /etc/apt/trusted.gpg.d/oqm_ppa.gpg >/dev/null
+wget -q -O - "https://deployment.openquartermaster.com/repos/test-dev/deb/KEY.gpg" | gpg --dearmor |  tee /etc/apt/trusted.gpg.d/oqm_ppa.gpg >/dev/null
 #add repo to list
-curl -s --compressed "https://deployment.openquartermaster.com/repos/test-dev/deb/deb_list_file.list" | sudo tee /etc/apt/sources.list.d/oqm_file.list
+#curl -s --compressed "https://deployment.openquartermaster.com/repos/test-dev/deb/deb_list_file.list" |  tee /etc/apt/sources.list.d/oqm_file.list
+wget -q -O - "https://deployment.openquartermaster.com/repos/test-dev/deb/deb_list_file.list" |  tee /etc/apt/sources.list.d/oqm_file.list
 # update apt and install
-sudo apt-get update
+ apt-get update
 if [ 0 -ne 0 ]; then
 	echo "FAILED to update apt. See above output for information."
 	exit 1;
 fi
 
-sudo apt-get install open+quarter+master-manager-station+captain
+ apt-get install  open+quarter+master-manager-station+captain
 if [ 0 -ne 0 ]; then
 	echo "FAILED to install Station Captain. See above output for information."
 	exit 2;
@@ -20,7 +33,7 @@ fi
 
 clear
 
-echo "Setup of TEST OQM repo for branch dev and installation of OQM Captain utility complete."
+echo "Setup of OQM repo and installation of OQM Captain utility complete."
 echo
 echo "Run 'sudo oqm-captain' to get started."
 echo
