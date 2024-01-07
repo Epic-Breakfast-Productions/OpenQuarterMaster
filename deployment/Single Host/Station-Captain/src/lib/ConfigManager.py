ad98629206312b66dc1e36a1d90467d2100e0801
GregJohnStewart <contact@gjstewart.net>
2024-01-07 15:29:06 -0500

    Station Captain - rmeoved debug message

--- deployment/Single Host/Station-Captain/src/lib/ConfigManager.py
+++ deployment/Single Host/Station-Captain/src/lib/ConfigManager.py
@@ -233,7 +233,7 @@ class ConfigManager:
 
     def getSecretManager(self) -> SecretManager:
         if self.secretManager is None:
-            print("DEBUG:: getting new secret manager")
+            # print("DEBUG:: getting new secret manager")
             self.secretManager = SecretManager()
         return self.secretManager
 
