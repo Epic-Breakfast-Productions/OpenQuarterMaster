a7808a6edbe49162cfafdc63a4d83fa80dfe7895
GregJohnStewart <contact@gjstewart.net>
2024-01-20 19:03:58 -0500

    Core - Base Station - Initial dump of php project for base station

---
+++ software/oqm-core-base-station/webroot/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ebprod/oqm-core-base-station',
+        'pretty_version' => '1.0.0',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ebprod/oqm-core-base-station' => array(
+            'pretty_version' => '1.0.0',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
