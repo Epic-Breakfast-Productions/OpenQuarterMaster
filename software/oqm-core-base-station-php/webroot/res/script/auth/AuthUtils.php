0f9d5d95503858b298f86c8d08cc71a258a6ee47
GregJohnStewart <contact@gjstewart.net>
2024-01-23 20:46:55 -0500

    Core - Base Station - Moved php impl for future work

---
+++ software/oqm-core-base-station-php/webroot/res/script/auth/AuthUtils.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Ebprod\OqmCoreDepot\auth;
+
+use Ebprod\OqmCoreDepot\context\Context;
+use Ebprod\OqmCoreDepot\LogUtils;
+use Jumbojett\OpenIDConnectClient;
+use Monolog\Logger;
+
+/**
+ * Refs:
+ *  - https://github.com/jumbojett/OpenID-Connect-PHP
+ */
+class AuthUtils {
+	private static ?Logger $log = null;
+	private static ?OpenIDConnectClient $oidc = null;
+	private static string $CLAIM_USERNAME = "name";
+	
+	private static function log():Logger{
+		if(self::$log == null){
+			self::$log = LogUtils::getLogger(self::class);
+		}
+		return self::$log;
+	}
+	
+	private static function getOidc(): OpenIDConnectClient {
+		if (self::$oidc == null) {
+			self::$oidc = new OpenIDConnectClient(Context::instance()->getOidc()->getHost(),
+				Context::instance()->getOidc()->getClientId(),
+				Context::instance()->getOidc()->getClientSecret()
+			);
+//			self::$oidc->setAllowImplicitFlow(true);
+			
+			self::$oidc->addScope(array('openid'));
+			self::$oidc->setAllowImplicitFlow(true);
+			self::$oidc->addAuthParam(array('response_mode' => 'form_post'));
+			
+//			self::log()->debug("Implicit flow enabled? ".(self::$oidc->getAllowImplicitFlow()?"true":"false"));
+			if(Context::instance()->devModeEnabled()){
+				self::$oidc->setHttpUpgradeInsecureRequests(false);
+			}
+		}
+		return self::$oidc;
+	}
+	
+	public static function ensureLoggedIn(): void {
+		self::log()->info("Authenticating user.");
+		self::getOidc()->authenticate();
+		self::log()->info("Authenticated user: " . self::getUsername());
+	}
+	
+	public static function getUsername(): string {
+		self::log()->debug("Getting username.");
+		return self::getOidc()->getVerifiedClaims(self::$CLAIM_USERNAME);
+	}
+	
+	public static function getUsersToken(): string {
+		return self::getOidc()->getAccessToken();
+	}
+	
+}
