689ff89541c25fadc9c92cb48b5d3d439c807993
GregJohnStewart <contact@gjstewart.net>
2024-02-02 12:16:48 -0500

    Core - Base Station - Added new datetime service to use in ui

--- software/oqm-core-base-station/src/main/java/tech/ebp/oqm/core/baseStation/interfaces/ui/UiProvider.java
+++ software/oqm-core-base-station/src/main/java/tech/ebp/oqm/core/baseStation/interfaces/ui/UiProvider.java
@@ -8,7 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.eclipse.microprofile.config.ConfigProvider;
 import tech.ebp.oqm.core.baseStation.interfaces.RestInterface;
 
-import java.time.ZonedDateTime;
 import java.util.Currency;
 
 
@@ -23,6 +22,6 @@ public abstract class UiProvider extends RestInterface {
 				   .data("userInfo", this.getUserInfo())
 				   .data("traceId", this.span.getSpanContext().getTraceId())
 				   .data("currency", ConfigProvider.getConfig().getValue("service.ops.currency", Currency.class))
-				   .data("generateDatetime", ZonedDateTime.now());
+				   ;
 	}
 }
--- software/oqm-core-base-station/src/main/java/tech/ebp/oqm/core/baseStation/service/extItemLookup/DateTimeService.java
+++ software/oqm-core-base-station/src/main/java/tech/ebp/oqm/core/baseStation/service/extItemLookup/DateTimeService.java
@@ -1,24 +1,43 @@
 package tech.ebp.oqm.core.baseStation.service.extItemLookup;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import jakarta.inject.Named;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
-import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
 import java.util.Date;
 
 @Named("DateTimeService")
 @ApplicationScoped
 public class DateTimeService {
 	
-	public String formatForUi(Date date){
-		return "DATETIME";
+	private final DateTimeFormatter formatter;
+	
+	@Inject
+	public DateTimeService(
+		@ConfigProperty(name = "service.dateTimeFormat.default")
+		String datetimeFormat
+	){
+		this.formatter = DateTimeFormatter.ofPattern(datetimeFormat);
 	}
 	
-	public String formatForUi(LocalDateTime date){
-		return "LDATETIME";
+	public String formatForUi(Date date){
+		if (date == null) {
+			date = new Date();
+		}
+		return formatter.format(date.toInstant());
 	}
-	public String formatForUi(ZonedDateTime date){
-		return "ZDATETIME";
+	
+	public String formatForUi(TemporalAccessor date){
+		if(date == null){
+			date = ZonedDateTime.now();
+		}
+		return formatter.format(date);
+	}
+	public String formatForUi(){
+		return this.formatForUi((TemporalAccessor) null);
 	}
 }
