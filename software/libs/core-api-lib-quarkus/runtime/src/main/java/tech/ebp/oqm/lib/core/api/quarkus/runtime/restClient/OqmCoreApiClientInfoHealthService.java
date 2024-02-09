51d98aaf35998d8041b7916f0d26bffdf899044d
GregJohnStewart <contact@gjstewart.net>
2024-02-09 11:40:11 -0500

    Lib - CoreAPI - Quarkus - Added unit endpoints to rest client

--- software/libs/core-api-lib-quarkus/runtime/src/main/java/tech/ebp/oqm/lib/core/api/quarkus/runtime/restClient/OqmCoreApiClientInfoHealthService.java
+++ software/libs/core-api-lib-quarkus/runtime/src/main/java/tech/ebp/oqm/lib/core/api/quarkus/runtime/restClient/OqmCoreApiClientInfoHealthService.java
@@ -11,16 +11,29 @@ import tech.ebp.oqm.lib.core.api.quarkus.runtime.Constants;
 import java.net.http.HttpHeaders;
 
 import static tech.ebp.oqm.lib.core.api.quarkus.runtime.Constants.INV_ITEM_ROOT_ENDPOINT;
+import static tech.ebp.oqm.lib.core.api.quarkus.runtime.Constants.ROOT_API_ENDPOINT_V1;
 
 
 @RegisterRestClient(configKey = Constants.CONFIG_ROOT_NAME)
 public interface OqmCoreApiClientInfoHealthService {
 	
-	@Path("/q/health")
 	@GET
+	@Path("/q/health")
 	ObjectNode getApiServerHealth();
 	
-	@Path(INV_ITEM_ROOT_ENDPOINT + "/stats")
+	//<editor-fold desc="Info">
 	@GET
+	@Path(ROOT_API_ENDPOINT_V1 + "/info/units")
+	ObjectNode getAllUnits(@HeaderParam(Constants.AUTH_HEADER_NAME) String token);
+	
+	@GET
+	@Path(ROOT_API_ENDPOINT_V1 + "/info/unitCompatibility")
+	ObjectNode getUnitCompatability(@HeaderParam(Constants.AUTH_HEADER_NAME) String token);
+	//</editor-fold>
+	
+	//<editor-fold desc="Inventory Items">
+	@GET
+	@Path(INV_ITEM_ROOT_ENDPOINT + "/stats")
 	ObjectNode getItemStats(@HeaderParam(Constants.AUTH_HEADER_NAME) String token);
+	//</editor-fold>
 }
