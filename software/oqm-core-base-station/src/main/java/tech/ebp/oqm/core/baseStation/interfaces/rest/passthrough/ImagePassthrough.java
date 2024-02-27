78de9778d500b33eeb1e9d497e57a2ac717d95db
GregJohnStewart <contact@gjstewart.net>
2024-02-27 02:31:41 -0500

    Core - Base Station - Filled out storage block search results

--- software/oqm-core-base-station/src/main/java/tech/ebp/oqm/core/baseStation/interfaces/rest/passthrough/ImagePassthrough.java
+++ software/oqm-core-base-station/src/main/java/tech/ebp/oqm/core/baseStation/interfaces/rest/passthrough/ImagePassthrough.java
@@ -7,6 +7,7 @@ import jakarta.enterprise.context.RequestScoped;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -15,26 +16,20 @@ import org.eclipse.microprofile.rest.client.inject.RestClient;
 import tech.ebp.oqm.lib.core.api.quarkus.runtime.restClient.OqmCoreApiClientService;
 
 @Slf4j
-@Path(PassthroughProvider.PASSTHROUGH_API_ROOT + "/inventory/storage-block")
+@Path(PassthroughProvider.PASSTHROUGH_API_ROOT + "/media/image")
 @Authenticated
 @RequestScoped
 @Produces(MediaType.TEXT_HTML)
-public class StorageBlockPassthrough extends PassthroughProvider {
+public class ImagePassthrough extends PassthroughProvider {
 	
 	@RestClient
 	OqmCoreApiClientService oqmCoreApiClient;
 	
-	@POST
-	public Uni<Response> addStorageBlock(ObjectNode newStorageBlock){
-		return this.oqmCoreApiClient.storageBlockAdd(this.getBearerHeaderStr(), newStorageBlock)
-				   .map( output ->
-							 Response.ok(output).build()
-				   );
-	}
-	
 	@GET
-	public Uni<Response> test(){
-		return Uni.createFrom().item(this::getBearerHeaderStr).map( output ->
+	@Path("/for/{type}/{id}")
+	public Uni<Response> imageForObj(@PathParam("type") String type,  @PathParam("id") String objId){
+		return this.oqmCoreApiClient.imageForObject(this.getBearerHeaderStr(), type, objId)
+				   .map( output ->
 							 Response.ok(output).build()
 				   );
 	}
