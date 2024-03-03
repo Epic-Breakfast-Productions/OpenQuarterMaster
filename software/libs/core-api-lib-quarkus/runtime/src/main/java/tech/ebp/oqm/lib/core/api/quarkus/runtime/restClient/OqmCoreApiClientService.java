ab8778565ac12f916e1b09981a51f642b9ee868c
GregJohnStewart <contact@gjstewart.net>
2024-03-02 23:37:29 -0500

    Lib - CoreAPI - Quarkus - Added storage block editing, deleting methods

--- software/libs/core-api-lib-quarkus/runtime/src/main/java/tech/ebp/oqm/lib/core/api/quarkus/runtime/restClient/OqmCoreApiClientService.java
+++ software/libs/core-api-lib-quarkus/runtime/src/main/java/tech/ebp/oqm/lib/core/api/quarkus/runtime/restClient/OqmCoreApiClientService.java
@@ -4,15 +4,7 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.smallrye.mutiny.Uni;
 import jakarta.annotation.security.RolesAllowed;
-import jakarta.ws.rs.BeanParam;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.HeaderParam;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
@@ -73,6 +65,15 @@ public interface OqmCoreApiClientService {
 	@GET
 	@Path(STORAGE_BLOCK_ROOT_ENDPOINT + "/{blockId}")
 	Uni<ObjectNode> storageBlockGet(@HeaderParam(Constants.AUTH_HEADER_NAME) String token, @PathParam("blockId") String storageBlockId);
+	
+	@PUT
+	@Path(STORAGE_BLOCK_ROOT_ENDPOINT + "/{id}")
+	Uni<ObjectNode> storageBlockUpdate(@HeaderParam(Constants.AUTH_HEADER_NAME) String token, @PathParam("id") String storageBlockId, ObjectNode updates);
+	
+	@DELETE
+	@Path(STORAGE_BLOCK_ROOT_ENDPOINT + "/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	Uni<ObjectNode> storageBlockDelete(@HeaderParam(Constants.AUTH_HEADER_NAME) String token, @PathParam("id") String storageBlockId);
 	
 	@GET
 	@Path(STORAGE_BLOCK_ROOT_ENDPOINT + "/{blockId}/history")
