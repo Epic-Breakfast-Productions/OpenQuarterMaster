0eb66b29d629eca0929b35bd67c4ffaf2c761001
GregJohnStewart <contact@gjstewart.net>
2024-03-22 23:55:47 -0400

    Core - API - First initial rework of object event streaming

--- software/oqm-core-api/src/main/java/tech/ebp/oqm/baseStation/service/notification/HistoryEventNotificationService.java
+++ software/oqm-core-api/src/main/java/tech/ebp/oqm/baseStation/service/notification/HistoryEventNotificationService.java
@@ -5,26 +5,43 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Message;
 import tech.ebp.oqm.baseStation.model.object.history.ObjectHistoryEvent;
 
 @ApplicationScoped
 public class HistoryEventNotificationService {
-	public static final String EVENT_CHANNEL = "events";
-	public static final String EVENT_TOPIC = "all-events";
+	public static final String INTERNAL_EVENT_CHANNEL = "events-internal";
+	public static final String OUTGOING_EVENT_CHANNEL = "events-outgoing";
+	public static final String ALL_EVENT_TOPIC = "all-events";
 	
 	@Inject
-	@Channel(EVENT_CHANNEL)
-	Emitter<ObjectHistoryEvent> eventEmitter;
+	@Channel(INTERNAL_EVENT_CHANNEL)
+	Emitter<EventNotificationWrapper> internalEventEmitter;
+	
+	@Inject
+	@Channel(OUTGOING_EVENT_CHANNEL)
+	Emitter<ObjectHistoryEvent> outgoingEventEmitter;
+	
+	//TODO:: add config for outgoing
+	//TODO:: use in update code
+	//TODO:: test and verify; ensure broadcast
+	
+	/**
+	 * Don't call this directly, use the other one
+	 */
+	@Incoming(INTERNAL_EVENT_CHANNEL)
+	public void sendEventOutgoing(EventNotificationWrapper notificationWrapper){
+		this.outgoingEventEmitter.send(Message.of(notificationWrapper.getEvent()).addMetadata(OutgoingKafkaRecordMetadata.<String>builder()
+																 .withTopic(notificationWrapper.getObjectName() + "-"+notificationWrapper.getEvent().getType())
+																 .build()));
+		this.outgoingEventEmitter.send(Message.of(notificationWrapper.getEvent()).addMetadata(OutgoingKafkaRecordMetadata.<String>builder()
+																 .withTopic(ALL_EVENT_TOPIC)
+																 .build()));
+	}
 	
 	public void sendEvent(Class<?> objectClass, ObjectHistoryEvent event){
-		this.eventEmitter.send(Message.of(event).addMetadata(OutgoingKafkaRecordMetadata.<String>builder()
-																 .withTopic(objectClass.getSimpleName() + "-"+event.getType())
-																 .build()));
-		this.eventEmitter.send(Message.of(event).addMetadata(OutgoingKafkaRecordMetadata.<String>builder()
-																 .withTopic(EVENT_TOPIC)
-																 .build()));
-		
+		this.internalEventEmitter.send(new EventNotificationWrapper(objectClass.getSimpleName(), event));
 	}
 	
 }
