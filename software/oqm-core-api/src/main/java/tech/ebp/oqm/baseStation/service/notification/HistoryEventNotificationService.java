7bf080467c5a21b234bc9f58ae5287e6172dbe1c
GregJohnStewart <contact@gjstewart.net>
2024-03-22 23:35:51 -0400

    Core - API - First initial rework of event streaming

--- software/oqm-core-api/src/main/java/tech/ebp/oqm/baseStation/service/notification/HistoryEventNotificationService.java
+++ software/oqm-core-api/src/main/java/tech/ebp/oqm/baseStation/service/notification/HistoryEventNotificationService.java
@@ -1,7 +1,30 @@
 package tech.ebp.oqm.baseStation.service.notification;
 
+import io.smallrye.reactive.messaging.kafka.api.OutgoingKafkaRecordMetadata;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.eclipse.microprofile.reactive.messaging.Message;
 import tech.ebp.oqm.baseStation.model.object.history.ObjectHistoryEvent;
 
-public abstract class HistoryEventNotificationService<T extends ObjectHistoryEvent> {
-
+@ApplicationScoped
+public class HistoryEventNotificationService {
+	public static final String EVENT_CHANNEL = "events";
+	public static final String EVENT_TOPIC = "all-events";
+	
+	@Inject
+	@Channel(EVENT_CHANNEL)
+	Emitter<ObjectHistoryEvent> eventEmitter;
+	
+	public void sendEvent(Class<?> objectClass, ObjectHistoryEvent event){
+		this.eventEmitter.send(Message.of(event).addMetadata(OutgoingKafkaRecordMetadata.<String>builder()
+																 .withTopic(objectClass.getSimpleName() + "-"+event.getType())
+																 .build()));
+		this.eventEmitter.send(Message.of(event).addMetadata(OutgoingKafkaRecordMetadata.<String>builder()
+																 .withTopic(EVENT_TOPIC)
+																 .build()));
+		
+	}
+	
 }
