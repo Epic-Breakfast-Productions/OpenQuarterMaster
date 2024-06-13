5b706874f38ca1141eea9f04f035f56332744fb6
GregJohnStewart <contact@gjstewart.net>
2024-06-13 12:03:42 -0400

    Core - Base Station - Cleanup

--- software/oqm-core-base-station/src/main/java/tech/ebp/oqm/core/baseStation/service/ExternalItemSearchClient.java
+++ software/oqm-core-base-station/src/main/java/tech/ebp/oqm/core/baseStation/service/ExternalItemSearchClient.java
@@ -2,7 +2,6 @@ package tech.ebp.oqm.core.baseStation.service;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.smallrye.mutiny.Uni;
-import jakarta.annotation.security.PermitAll;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
@@ -16,25 +15,21 @@ public interface ExternalItemSearchClient {
 
 	@GET
 	@Path("/providers")
-	@PermitAll
 	@Produces(MediaType.APPLICATION_JSON)
 	Uni<ObjectNode> allProviderInfo();
 
 	@GET
 	@Path("barcode/{barcode}")
-	@PermitAll
 	@Produces(MediaType.APPLICATION_JSON)
 	Uni<ObjectNode> searchBarcode(@PathParam("barcode") String barcode);
 
 	@GET
 	@Path("webpage-scrape/{webpage}")
-	@PermitAll
 	@Produces(MediaType.APPLICATION_JSON)
 	Uni<ObjectNode> scanWebpage(@PathParam("webpage") String page);
 
 	@GET
 	@Path("lego/part/{partNo}")
-	@PermitAll
 	@Produces(MediaType.APPLICATION_JSON)
 	Uni<ObjectNode> searchLegoPart(@PathParam("partNo") String partNo);
 }
