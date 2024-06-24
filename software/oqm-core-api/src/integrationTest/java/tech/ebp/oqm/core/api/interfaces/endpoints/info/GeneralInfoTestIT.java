2bc96d3ae2fae9dfd447c541381ab4212d704b95
GregJohnStewart <contact@gjstewart.net>
2024-06-24 10:41:20 -0400

    Core - API - Adding more integration tests

--- software/oqm-core-api/src/integrationTest/java/tech/ebp/oqm/core/api/interfaces/endpoints/info/GeneralInfoTestIT.java
+++ software/oqm-core-api/src/integrationTest/java/tech/ebp/oqm/core/api/interfaces/endpoints/info/GeneralInfoTestIT.java
@@ -1,15 +1,13 @@
 package tech.ebp.oqm.core.api.interfaces.endpoints.info;
 
 import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusIntegrationTest;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.response.ValidatableResponse;
-import org.eclipse.microprofile.config.ConfigProvider;
-import org.eclipse.microprofile.config.inject.ConfigProperties;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import tech.ebp.oqm.core.api.testResources.testClasses.RunningServerTest;
-import tech.ebp.oqm.core.api.interfaces.endpoints.info.GeneralInfo;
 
 import java.util.Currency;
 
@@ -17,22 +15,8 @@ import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Tag("integration")
-@QuarkusTest
+@QuarkusIntegrationTest
 @TestHTTPEndpoint(GeneralInfo.class)
-class GeneralInfoTest extends RunningServerTest {
+class GeneralInfoTestIT extends GeneralInfoTest {
 
-	Currency currency = ConfigProvider.getConfig().getValue("service.ops.currency", Currency.class);
-	
-	@Test
-	public void testGetCurrency(){
-		ValidatableResponse response = given()
-			.get("currency")
-			.then()
-			.statusCode(200);
-		
-		assertEquals(
-			this.currency,
-			response.extract().as(Currency.class)
-		);
-	}
 }
