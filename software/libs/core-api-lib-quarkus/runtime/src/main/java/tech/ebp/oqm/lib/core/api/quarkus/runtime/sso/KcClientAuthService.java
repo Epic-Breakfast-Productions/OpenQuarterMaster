138b8d60edf5f4c91c33acbb27126e5c045baaba
GregJohnStewart <contact@gjstewart.net>
2024-07-18 16:47:40 -0400

    Lib - CoreAPI - Quarkus - More efficient determination if new service account token is required, again.

--- software/libs/core-api-lib-quarkus/runtime/src/main/java/tech/ebp/oqm/lib/core/api/quarkus/runtime/sso/KcClientAuthService.java
+++ software/libs/core-api-lib-quarkus/runtime/src/main/java/tech/ebp/oqm/lib/core/api/quarkus/runtime/sso/KcClientAuthService.java
@@ -2,9 +2,12 @@ package tech.ebp.oqm.lib.core.api.quarkus.runtime.sso;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.quarkus.scheduler.Scheduled;
+import io.quarkus.scheduler.ScheduledExecution;
+import io.quarkus.scheduler.Scheduler;
 import io.vertx.ext.auth.impl.jose.JWT;
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
@@ -15,44 +18,58 @@ import java.util.concurrent.locks.ReentrantLock;
 @Slf4j
 @ApplicationScoped
 public class KcClientAuthService {
+	public static final String SERVICE_ACCOUNT_TOKEN_REFRESH_JOB_NAME = "Service Account Token Refresh";
+
 	@RestClient
 	KeycloakRestClient keycloakRestClient;
+	@Inject
+	Scheduler scheduler;
 
 	private final ReentrantLock mutex = new ReentrantLock();
 	private String authString = null;
-	private LocalDateTime refreshTime = LocalDateTime.MIN;
 
-//	@PostConstruct
-//	@Scheduled
-//	public void getNewToken(){
-//		//TODO:: get new token here, run as another thread
-//	}
+	public ObjectNode getSetNewToken(ScheduledExecution scheduledExecution){
+		log.info("Getting new service account token.");
+		ObjectNode result = this.keycloakRestClient.getServiceAccountToken();
+		String jwt = result.get("access_token").asText();
+		String bearer = "Bearer " + jwt;
 
+		try{
+			this.mutex.lock();
+			this.authString = bearer;
+		} finally {
+			this.mutex.unlock();
+		}
+		log.info("Got new service account token.");
+		log.debug(
+			"New service account auth token: {} / {}",
+			jwt,
+			JWT.parse(jwt)
+		);
 
+		return result;
+	}
 
+	@PostConstruct
+	public void setup(){
+		log.info("Initializing KC Auth Service.");
+		ObjectNode result = this.getSetNewToken(null);
+
+		log.info("Scheduling job to retrieve new tokens before expiration.");
+		String refreshAfter = ((result.get("expires_in").asInt() / 4) * 3) + "s";
+		this.scheduler.newJob(SERVICE_ACCOUNT_TOKEN_REFRESH_JOB_NAME)
+			.setInterval(refreshAfter)
+			.setTask(this::getSetNewToken)
+			.setConcurrentExecution(Scheduled.ConcurrentExecution.SKIP)
+			.schedule();
+
+		log.info("Scheduled job to retrieve new tokens before expiration. Occurs every {}", refreshAfter);
+	}
 	
-	public synchronized String getAuthString(){
+	public String getAuthString(){
 		try{
 			this.mutex.lock();
 
-			//TODO:: move entire if to new thread, mutex only when necessary
-			if(
-				this.authString == null
-					|| this.refreshTime.isBefore(LocalDateTime.now())
-			){
-				log.info("Getting new service account token.");
-				ObjectNode result = this.keycloakRestClient.getServiceAccountToken();
-				String jwt = result.get("access_token").asText();
-				this.authString = "Bearer " + jwt;
-				int refreshAfter = (result.get("expires_in").asInt() / 3) * 2;
-				this.refreshTime = LocalDateTime.now().plusSeconds(refreshAfter);
-				log.info("Got new service account token, next refresh at {}", this.refreshTime);
-				log.debug(
-					"New service account auth token: {} / {}",
-					jwt,
-					JWT.parse(jwt)
-				);
-			}
 			return authString;
 		} finally {
 			this.mutex.unlock();
