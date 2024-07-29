8da643a8e3a1f73e04d2ea91de3cc76417f719b2
GregJohnStewart <contact@gjstewart.net>
2024-07-28 22:00:23 -0400

    Plugin - Mss Controller - Added basic groundwork for mss module test server

---
+++ software/plugins/mss-controller/mss-controller-plugin-testModuleServer/src/main/java/tech/ebp/oqm/plugin/mssController/testModuleServer/interfaces/serial/SerialModuleInterface.java
@@ -0,0 +1,9 @@
+package tech.ebp.oqm.plugin.mssController.testModuleServer.interfaces.serial;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import tech.ebp.oqm.plugin.mssController.testModuleServer.interfaces.MssModuleInterface;
+
+@ApplicationScoped
+public class SerialModuleInterface extends MssModuleInterface {
+	//TODO:: socat, etc
+}
--- software/plugins/mss-controller/mss-controller-plugin-testModuleServer/src/test/java/tech/ebp/oqm/plugin/mssController/testModuleServer/GreetingResourceIT.java
+++ software/plugins/mss-controller/mss-controller-plugin-testModuleServer/src/test/java/tech/ebp/oqm/plugin/mssController/testModuleServer/GreetingResourceIT.java
@@ -1,4 +1,4 @@
-package tech.ebp.oqm.plugin;
+package tech.ebp.oqm.plugin.mssController.testModuleServer;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
