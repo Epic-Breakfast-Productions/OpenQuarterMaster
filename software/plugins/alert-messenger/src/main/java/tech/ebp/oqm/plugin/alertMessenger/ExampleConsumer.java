694c79dab720b766e5f6395db9acab4d18264e2c
GregJohnStewart <contact@gjstewart.net>
2024-08-21 07:44:06 -0400

    Plugin - Alert Messenger - Finally fixed reading messages from queue

--- software/plugins/alert-messenger/src/main/java/tech/ebp/oqm/plugin/alertMessenger/ExampleConsumer.java
+++ software/plugins/alert-messenger/src/main/java/tech/ebp/oqm/plugin/alertMessenger/ExampleConsumer.java
@@ -1,25 +1,28 @@
 package tech.ebp.oqm.plugin.alertMessenger;
 
-
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
-import org.eclipse.microprofile.reactive.messaging.Message;
-
-import java.util.concurrent.CompletionStage;
 
 @Slf4j
 @ApplicationScoped
-public class TestConsumer {
+public class ExampleConsumer {
 
 	@Getter
 	private boolean received = false;
 
-	@Incoming("events-incoming")
-	public CompletionStage<Void> toUpperCase(Message<String> message) {
+	@PostConstruct
+	public void init() {
+		log.info("Starting ExampleConsumer");
+	}
+
+	@Incoming("oqm-core-all-events")
+	public void receive(ObjectNode message) {
+		//TODO:: use a kafka ui to figure out why no worky
 		log.info("Received message {}", message);
 		received = true;
-		return message.ack();
 	}
 }
--- software/plugins/alert-messenger/src/test/java/tech/ebp/oqm/plugin/alertManager/TestConsumerTest.java
+++ software/plugins/alert-messenger/src/test/java/tech/ebp/oqm/plugin/alertManager/TestConsumerTest.java
@@ -3,8 +3,6 @@ package tech.ebp.oqm.plugin.alertManager;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
-import lombok.AccessLevel;
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.junit.jupiter.api.Assertions;
@@ -12,9 +10,11 @@ import org.junit.jupiter.api.Test;
 import tech.ebp.oqm.lib.core.api.quarkus.runtime.OqmDatabaseService;
 import tech.ebp.oqm.lib.core.api.quarkus.runtime.restClient.OqmCoreApiClientService;
 import tech.ebp.oqm.lib.core.api.quarkus.runtime.sso.KcClientAuthService;
-import tech.ebp.oqm.plugin.alertMessenger.TestConsumer;
+import tech.ebp.oqm.plugin.alertMessenger.ExampleConsumer;
 
 import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @Slf4j
 @QuarkusTest
@@ -30,10 +30,12 @@ public class TestConsumerTest {
 	ObjectMapper objectMapper;
 
 	@Inject
-	TestConsumer testConsumer;
+	ExampleConsumer testConsumer;
 
 	@Test
 	public void test() throws InterruptedException {
+		assertFalse(this.testConsumer.isReceived());
+
 		String newid = this.coreApiClientService
 			.storageBlockAdd(
 				this.serviceAccountService.getAuthString(),
@@ -45,7 +47,7 @@ public class TestConsumerTest {
 
 		log.info("Id of new block: {}", newid);
 
-		LocalDateTime timeout = LocalDateTime.now().plusSeconds(10);
+		LocalDateTime timeout = LocalDateTime.now().plusMinutes(30);
 		while(!this.testConsumer.isReceived()){
 			if(LocalDateTime.now().isAfter(timeout)){
 				Assertions.fail("Failed to receive message.");
