1492b22c6469ced8be0b33bf8ba900a6b66f500c
GregJohnStewart <contact@gjstewart.net>
2024-08-21 22:05:32 -0400

    SNH - Station Captain - Fix to permission issues logging when it doesn't need it

--- deployment/Single Host/Station-Captain/src/lib/LogUtils.py
+++ deployment/Single Host/Station-Captain/src/lib/LogUtils.py
@@ -16,25 +16,36 @@ class LogUtils:
 
     @staticmethod
     def setupLogging(logFile:str, console:bool=False):
-        Path(LogUtils.logDir).mkdir(parents=True, exist_ok=True)
-        LogUtils.logFile = logFile
+        filePresent:bool = False
+        if not os.path.exists(logFile):
+            try:
+                Path(LogUtils.logDir).mkdir(parents=True, exist_ok=True)
+                os.mknod(logFile)
+                LogUtils.logFile = logFile
+                filePresent = True
+            except Exception as e:
+                LogUtils.logFile = False
+                filePresent = False
+        else:
+            filePresent = True
         logging.basicConfig(level=logging.NOTSET)
-
         if console:
             LogUtils.logLevel = logging.DEBUG
 
     @staticmethod
     def setupLogger(name: str) -> logging.Logger:
-        fh = logging.handlers.RotatingFileHandler(LogUtils.logDir+LogUtils.logFile, maxBytes=10*1024*1024, backupCount=5)
-        fh.setLevel(logging.DEBUG)
-
         # sh = logging.StreamHandler()
         # sh.setLevel(LogUtils.logLevel)
 
         logOut = logging.getLogger(name)
         # print(LogUtils.logLevel == logging.DEBUG)
         logOut.setLevel(LogUtils.logLevel)
-        logOut.addHandler(fh)
+
+        if not LogUtils.logFile:
+            fh = logging.handlers.RotatingFileHandler(LogUtils.logDir+LogUtils.logFile, maxBytes=10*1024*1024, backupCount=5)
+            fh.setLevel(logging.DEBUG)
+            logOut.addHandler(fh)
+
         # logOut.addHandler(sh)
         return logOut
 
