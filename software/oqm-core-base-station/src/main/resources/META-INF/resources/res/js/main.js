e5cf1b7f25edef30034b3a7d2a9a219ce667ff40
GregJohnStewart <contact@gjstewart.net>
2024-08-27 22:35:06 -0400

    Core - Base Station - Top search bar now more compact

--- software/oqm-core-base-station/src/main/resources/META-INF/resources/res/js/main.js
+++ software/oqm-core-base-station/src/main/resources/META-INF/resources/res/js/main.js
@@ -2,14 +2,10 @@ const navSearchInput = $('#navSearchInput');
 const navSearchForm = $('#navSearchForm');
 const navSearchTypeSelect = $('#navSearchTypeSelect');
 
-navSearchTypeSelect.on("change", function(event){
-    console.log(
-        "Changing nav form to " +
-        "action: " + navSearchTypeSelect[0].options[event.target.selectedIndex].dataset.action +
-        " with fieldName: " + navSearchTypeSelect[0].options[event.target.selectedIndex].dataset.field
-    );
-    navSearchForm.attr("action", navSearchTypeSelect[0].options[event.target.selectedIndex].dataset.action);
-    navSearchInput.attr("name", navSearchTypeSelect[0].options[event.target.selectedIndex].dataset.field);
-});
+function updateNavSearchDestination(action, icon, fieldName){
+    navSearchForm.attr("action", action);
+    navSearchTypeSelect.html(icon);
+    navSearchInput.attr("name", fieldName);
+}
 
 TimeHelpers.setupDateTimeInputs();
