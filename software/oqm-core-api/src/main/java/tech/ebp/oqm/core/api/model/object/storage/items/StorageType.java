16e4193b254471a12bd9b2d209e9f928002a3913
GregJohnStewart <contact@gjstewart.net>
2024-09-04 14:12:02 -0400

    Core - API - Added new storage type, validations

--- software/oqm-core-api/src/main/java/tech/ebp/oqm/core/api/model/object/storage/items/StorageType.java
+++ software/oqm-core-api/src/main/java/tech/ebp/oqm/core/api/model/object/storage/items/StorageType.java
@@ -1,11 +1,21 @@
 package tech.ebp.oqm.core.api.model.object.storage.items;
 
+import tech.ebp.oqm.core.api.model.object.storage.items.stored.StoredType;
+
 /**
  * Enum to determine what type of storage the item uses.
  */
 public enum StorageType {
-	BULK,
-	AMOUNT_LIST,
-	UNIQUE_MULTI,
-	UNIQUE_SINGLE;
+	BULK(StoredType.AMOUNT),
+	AMOUNT_LIST(StoredType.AMOUNT),
+	UNIQUE_MULTI(StoredType.UNIQUE),
+	UNIQUE_SINGLE(StoredType.UNIQUE),
+	UNIQUE_GLOBAL(StoredType.UNIQUE)
+	;
+
+	public final StoredType storedType;
+
+	StorageType(StoredType storedType) {
+		this.storedType = storedType;
+	}
 }
--- software/oqm-core-api/src/main/java/tech/ebp/oqm/core/api/service/mongo/StoredService.java
+++ software/oqm-core-api/src/main/java/tech/ebp/oqm/core/api/service/mongo/StoredService.java
@@ -8,6 +8,7 @@ import io.quarkus.arc.InstanceHandle;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
+import jakarta.validation.ValidationException;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
@@ -26,12 +27,14 @@ import tech.ebp.oqm.core.api.model.object.storage.storageBlock.StorageBlock;
 import tech.ebp.oqm.core.api.model.rest.search.InventoryItemSearch;
 import tech.ebp.oqm.core.api.model.rest.search.StoredSearch;
 import tech.ebp.oqm.core.api.service.mongo.exception.DbNotFoundException;
+import tech.ebp.oqm.core.api.service.mongo.search.SearchResult;
 import tech.ebp.oqm.core.api.service.notification.HistoryEventNotificationService;
 
 import java.util.*;
 
 import static com.mongodb.client.model.Filters.eq;
 import static com.mongodb.client.model.Filters.exists;
+import static tech.ebp.oqm.core.api.model.object.storage.items.StorageType.*;
 
 @Named("StoredService")
 @Slf4j
@@ -41,6 +44,10 @@ public class StoredService extends MongoHistoriedObjectService<Stored, StoredSea
 	@Inject
 	@Getter(AccessLevel.PRIVATE)
 	CoreApiInteractingEntity coreApiInteractingEntity;
+
+	@Inject
+	@Getter(AccessLevel.PRIVATE)
+	InventoryItemService inventoryItemService;
 
 	@Inject
 	@Getter(AccessLevel.PRIVATE)
@@ -58,29 +65,72 @@ public class StoredService extends MongoHistoriedObjectService<Stored, StoredSea
 
 	public StoredService() {
 		super(Stored.class, false);
-		try(InstanceHandle<HistoryEventNotificationService> container = Arc.container().instance(HistoryEventNotificationService.class)){
+		try (InstanceHandle<HistoryEventNotificationService> container = Arc.container().instance(HistoryEventNotificationService.class)) {
 			this.hens = container.get();
 		}
 	}
-	
+
 	@WithSpan
 	@Override
 	public void ensureObjectValid(String oqmDbIdOrName, boolean newObject, Stored newOrChangedObject, ClientSession clientSession) {
 		super.ensureObjectValid(oqmDbIdOrName, newObject, newOrChangedObject, clientSession);
+
+		InventoryItem item;
+		try {
+			item = this.inventoryItemService.get(oqmDbIdOrName, newOrChangedObject.getItem());
+		} catch (DbNotFoundException e) {
+			throw new ValidationException("Item " + newOrChangedObject.getItem().toHexString() + " does not exist.", e);
+		}
+
+		if (!item.getStorageBlocks().contains(newOrChangedObject.getStorageBlock())) {
+			throw new ValidationException("Storage block " + newOrChangedObject.getStorageBlock().toHexString() + " not use to hold this item.");
+		}
+
+		if(item.getStorageType().storedType != newOrChangedObject.getStoredType()){
+			throw new ValidationException("Stored given of type "+newOrChangedObject.getStoredType()+" cannot be held in item of storage type" + item.getStorageType());
+		}
+
+		if (item.getStorageType() == BULK || item.getStorageType() == UNIQUE_SINGLE) {
+			SearchResult<Stored> inBlock = this.search(oqmDbIdOrName, StoredSearch.builder().inventoryItemId(item.getId()).storageBlockId(newOrChangedObject.getStorageBlock()).build());
+
+			if (!inBlock.isEmpty()) {
+				if (inBlock.getNumResults() != 1) {
+					throw new ValidationException("More than one stored held for item of type " + item.getStorageType());
+				}
+				Stored stored = inBlock.getResults().get(0);
+				if (newObject || !stored.getId().equals(newOrChangedObject.getId())) {
+					throw new ValidationException("Cannot add more than one stored held for type " + item.getStorageType());
+				}
+			}
+		}
+
+		if(item.getStorageType() == UNIQUE_GLOBAL){
+			SearchResult<Stored> inItem = this.search(oqmDbIdOrName, StoredSearch.builder().inventoryItemId(item.getId()).build());
+			if (!inItem.isEmpty()) {
+				if(inItem.getNumResults() != 1) {
+					throw new ValidationException("More than one globally unique stored held");
+				}
+
+				Stored stored = inItem.getResults().get(0);
+				if (newObject || !stored.getId().equals(newOrChangedObject.getId())) {
+					throw new ValidationException("Cannot store more than one globally unique stored item.");
+				}
+			}
+		}
 	}
-	
+
 	@Override
 	public CollectionStats getStats(String oqmDbIdOrName) {
 		return super.addBaseStats(oqmDbIdOrName, CollectionStats.builder())
-				   .build();
+			.build();
 	}
-	
+
 	@Override
 	public Stored update(String oqmDbIdOrName, Stored object) throws DbNotFoundException {
 		Stored item = super.update(oqmDbIdOrName, object);
 		return item;
 	}
-	
+
 	@Override
 	public Stored update(String oqmDbIdOrName, ObjectId id, ObjectNode updateJson, InteractingEntity interactingEntity) {
 		Stored item = super.update(oqmDbIdOrName, id, updateJson, interactingEntity);
