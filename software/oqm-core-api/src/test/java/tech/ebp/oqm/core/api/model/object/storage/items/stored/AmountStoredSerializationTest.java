37f255fe27a9bca92f5eba0f2414d01d957dc404
GregJohnStewart <contact@gjstewart.net>
2024-10-09 19:54:27 -0400

    Core - API - Fixed AmountStoredSerializationTest

--- software/oqm-core-api/src/test/java/tech/ebp/oqm/core/api/model/object/storage/items/stored/AmountStoredSerializationTest.java
+++ software/oqm-core-api/src/test/java/tech/ebp/oqm/core/api/model/object/storage/items/stored/AmountStoredSerializationTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.params.provider.Arguments;
 import systems.uom.common.USCustomary;
 import tech.ebp.oqm.core.api.model.testUtils.ObjectSerializationTest;
 import tech.ebp.oqm.core.api.model.units.OqmProvidedUnits;
+import tech.ebp.oqm.core.api.model.units.UnitUtils;
+import tech.units.indriya.quantity.Quantities;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -19,27 +21,13 @@ class AmountStoredSerializationTest extends ObjectSerializationTest<AmountStored
 	
 	public static Stream<Arguments> getObjects() {
 		return Stream.of(
-			Arguments.of(new AmountStored(OqmProvidedUnits.UNIT)),
 			Arguments.of(
-				new AmountStored(5, OqmProvidedUnits.UNIT)
-					.setExpires(LocalDateTime.now())
-					.setCondition(50)
-					.setConditionNotes(FAKER.lorem().paragraph())
-					.setImageIds(List.of(ObjectId.get()))
-					.setAttributes(Map.of("hello", "world"))
-					.setKeywords(List.of("hello", "world"))
-			
-			),
-			Arguments.of(
-				new AmountStored(5, USCustomary.INCH)
-					.setExpires(LocalDateTime.now())
-					.setCondition(50)
-					.setConditionNotes(FAKER.lorem().paragraph())
-					.setImageIds(List.of(ObjectId.get()))
-					.setAttributes(Map.of("hello", "world"))
-					.setKeywords(List.of("hello", "world"))
-			
+				AmountStored.builder()
+					.item(new ObjectId())
+				.amount(Quantities.getQuantity(0, OqmProvidedUnits.UNIT))
+				.build()
 			)
+				//TODO:: more
 		);
 	}
 }
