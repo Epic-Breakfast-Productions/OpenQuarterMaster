8780ffb16b9e2f7a96aa5ff1697f973cb59528d6
GregJohnStewart <contact@gjstewart.net>
2024-10-09 20:26:05 -0400

    Core - API - Fixed UniqueStoredValidationTest

--- software/oqm-core-api/src/test/java/tech/ebp/oqm/core/api/model/object/storage/items/stored/UniqueStoredValidationTest.java
+++ software/oqm-core-api/src/test/java/tech/ebp/oqm/core/api/model/object/storage/items/stored/UniqueStoredValidationTest.java
@@ -14,16 +14,20 @@ class UniqueStoredValidationTest extends ObjectValidationTest<UniqueStored> {
 	
 	public static Stream<Arguments> getValid() {
 		return Stream.of(
-			Arguments.of(new UniqueStored().setId(new ObjectId())),
 			Arguments.of(
-				new UniqueStored()
-					.setCondition(50)
-					.setExpires(LocalDateTime.now())
-					.setImageIds(List.of(ObjectId.get()))
-					.setConditionNotes(FAKER.lorem().paragraph())
-					.setAttributes(Map.of("hello", "world"))
-					.setKeywords(List.of("hello", "world"))
-					.setId(new ObjectId())
+				UniqueStored.builder().id(new ObjectId()).item(new ObjectId()).build()
+			),
+			Arguments.of(
+				UniqueStored.builder()
+					.item(new ObjectId())
+					.condition(50)
+					.expires(LocalDateTime.now())
+					.conditionNotes(FAKER.lorem().paragraph())
+					.imageIds(List.of(ObjectId.get()))
+					.attributes(Map.of("hello", "world"))
+					.keywords(List.of("hello", "world"))
+					.id(new ObjectId())
+					.build()
 			)
 		);
 	}
@@ -31,17 +35,12 @@ class UniqueStoredValidationTest extends ObjectValidationTest<UniqueStored> {
 	public static Stream<Arguments> getInvalid() {
 		return Stream.of(
 			Arguments.of(
-				new UniqueStored().setCondition(-1).setId(new ObjectId()),
+				UniqueStored.builder().id(new ObjectId()).item(new ObjectId()).condition(-1).build(),
 				new HashMap<>() {{
 					put("condition", "must be greater than or equal to 0");
 				}}
-			),
-			Arguments.of(
-				new UniqueStored().setCondition(101).setId(new ObjectId()),
-				new HashMap<>() {{
-					put("condition", "must be less than or equal to 100");
-				}}
 			)
+			//TODO:: more
 		);
 	}
 }
