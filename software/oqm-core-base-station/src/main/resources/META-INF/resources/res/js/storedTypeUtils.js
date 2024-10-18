72ddae9e54c1e016239e0ac930e8c6acdb57952a
GregJohnStewart <contact@gjstewart.net>
2024-10-17 20:39:07 -0400

    Core - Base Station - Initial rework of item adding. Added hto the help page

--- software/oqm-core-base-station/src/main/resources/META-INF/resources/res/js/storedTypeUtils.js
+++ software/oqm-core-base-station/src/main/resources/META-INF/resources/res/js/storedTypeUtils.js
@@ -1,32 +1,82 @@
 const StoredTypeUtils = {
-	foreachStoredType(
-		storedType,
-		whenAmountSimple,
-		whenAmountList,
-		whenTracked
-	) {
-		if (storedType === "AMOUNT_SIMPLE") {
-			if (whenAmountSimple !== null) {
-				return whenAmountSimple();
-			}
-		} else if (storedType === "AMOUNT_LIST") {
-			if (whenAmountList !== null) {
-				return whenAmountList();
-			}
-		} else if (storedType === "TRACKED") {
-			if (whenTracked !== null) {
-				return whenTracked();
-			}
-		}
-	},
-	storedTypeToDisplay(storedType){
-		if (storedType === "AMOUNT_SIMPLE") {
-			return "Amount Simple";
-		} else if (storedType === "AMOUNT_LIST") {
-			return "Amount List";
-		} else if (storedType === "TRACKED") {
-			return "Tracked";
-		}
-		return "FAIL";
-	}
+    types: ["AMOUNT", "UNIQUE"],
+    runForType(
+        storedType,
+        whenAmount,
+        whenUnique,
+    ) {
+        if (storedType === "AMOUNT") {
+            if (whenAmount !== null) {
+                return whenAmount();
+            }
+        } else if (storedType === "UNIQUE") {
+            if (whenUnique !== null) {
+                return whenUnique();
+            }
+        }
+    },
+    typeToDisplay(storedType) {
+        if (storedType === "AMOUNT") {
+            return "Amount";
+        } else if (storedType === "UNIQUE") {
+            return "Unique";
+        }
+        return "FAIL";
+    }
+};
+
+const StorageTypeUtils = {
+    types: ["BULK", "AMOUNT_LIST", "UNIQUE_MULTI", "UNIQUE_SINGLE"],
+    runForType(
+        storedType,
+        whenBulk,
+        whenAmountList,
+        whenUniqueMulti,
+        whenUniqueSingle
+    ) {
+        if (storedType === "BULK") {
+            if (whenBulk !== null) {
+                return whenBulk();
+            }
+        } else if (storedType === "AMOUNT_LIST") {
+            if (whenAmountList !== null) {
+                return whenAmountList();
+            }
+        } else if (storedType === "UNIQUE_MULTI") {
+            if (whenUniqueMulti !== null) {
+                return whenUniqueMulti();
+            }
+        } else if (storedType === "UNIQUE_SINGLE") {
+            if (whenUniqueSingle !== null) {
+                return whenUniqueSingle();
+            }
+        }
+    },
+    runForStoredType(
+        storedType,
+        whenAmount,
+        whenUnique,
+    ) {
+        if (storedType === "BULK" || storedType === "AMOUNT_LIST") {
+            if (whenAmount !== null) {
+                return whenAmount();
+            }
+        } else if (storedType === "UNIQUE_MULTI" || storedType === "UNIQUE_SINGLE") {
+            if (whenUnique !== null) {
+                return whenUnique();
+            }
+        }
+    },
+    typeToDisplay(storedType) {
+        if (storedType === "BULK") {
+            return "Bulk"
+        } else if (storedType === "AMOUNT_LIST") {
+            return "Amount List"
+        } else if (storedType === "UNIQUE_MULTI") {
+            return "Unique - Multiple"
+        } else if (storedType === "UNIQUE_SINGLE") {
+            return "Unique - Single"
+        }
+        return "FAIL";
+    }
 };
