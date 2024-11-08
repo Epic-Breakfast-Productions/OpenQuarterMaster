e3c613527960d58fe266e1cb804a755c64ffd970
GregJohnStewart <contact@gjstewart.net>
2024-11-07 22:37:35 -0500

    Core - Base Station - Fleshed out logic for either searching for items or using the provided one

--- software/oqm-core-base-station/src/main/resources/META-INF/resources/res/js/obj/itemStored/itemStoredTransaction.js
+++ software/oqm-core-base-station/src/main/resources/META-INF/resources/res/js/obj/itemStored/itemStoredTransaction.js
@@ -6,10 +6,16 @@ const ItemStoredTransaction = {
 
 	storedFormUtils: {
 		getBasicInputs(stored){
-
+			
 		},
-		getAmountInputs(stored){
-
+		getAmountInputs: async function(item, stored){
+			let output = $('' +
+				'<div class="mb-3">\n' +
+				'    <label class="form-label">To Stored</label>\n' +
+				'    <select class="form-select" id="">\n' +
+				'    </select>\n' +
+				'</div>' +
+				'');
 		},
 		getUniqueInputs(stored){
 
@@ -23,33 +29,62 @@ const ItemStoredTransaction = {
 		messages: $("#itemStoredTransactionAddMessages"),
 		form: $("#itemStoredTransactionAddForm"),
 
-		idInput: $("#itemStoredTransactionAddFormItemIdInput"),
+		storedIdInput: $("#itemStoredTransactionAddFormStoredIdInput"),
+		itemInputContainer: $("#itemStoredTransactionAddFormItemInputContainer"),
+		itemIdInput: $("#itemStoredTransactionAddFormItem-itemInputId"),
+		itemSearchButton: $("#itemStoredTransactionAddFormItem-itemInputSearchButton"),
+		itemNameInput: $("#itemStoredTransactionAddFormItem-itemInputName"),
+		itemClearButton: $("#itemStoredTransactionAddFormItem-itemInputClearButton"),
+
+		itemDisplayContainer: $("#itemStoredTransactionAddFormItemDisplayContainer"),
+		itemDisplayName: $("#itemStoredTransactionAddFormItemDisplayName"),
+
+
+
+
+
+
 		typeInputContainer: $("#itemStoredTransactionAddFormTypeInputContainer"),
 		typeInput: $("#itemStoredTransactionAddFormTypeInput"),
 		toBlockInputContainer: $("#itemStoredTransactionAddFormToBlockInputContainer"),
 		toBlockInput: $("#itemStoredTransactionAddFormToBlockInput"),
 		toStoredInputContainer: $("#itemStoredTransactionAddFormToStoredInputContainer"),
 		toStoredInput: $("#itemStoredTransactionAddFormToStoredInput"),
-		amountContainer: $("#itemStoredTransactionAddFormAmountContainer"),
-		storedContainer: $("#itemStoredTransactionAddFormStoredContainer"),
+		inputsContainer: $("#itemStoredTransactionAddFormInputsContainer"),
 
-		resetForm: function () {
+		updateInputs: function(){
+
+		},
+		resetForm: function (changeItemRelated = true) {
 			console.log("Resetting item stored add transaction form.");
 			this.form.trigger("reset");
-			this.idInput.val("");
+			this.storedIdInput.val("");
+
+			if(changeItemRelated) {
+				this.itemIdInput.val("");
+				this.itemInputContainer.hide();
+				this.itemNameInput.val("");
+				this.itemNameInput.prop("disabled", true);
+				this.itemClearButton.prop("disabled", true);
+				this.itemSearchButton.prop("disabled", true);
+				this.itemDisplayContainer.hide();
+				this.itemDisplayName.text('');
+			}
+
 			this.typeInputContainer.hide();
 			this.toStoredInputContainer.hide();
 			this.toStoredInput.html("");
 			this.toBlockInput.html("");
 		},
-		setupForm(itemId, preselectedStored = null, buttonElement = null) {
-			console.log("Setting up item stored add transaction form for item", itemId);
-			ModalHelpers.setReturnModal(this.modal, buttonElement);
-			this.resetForm();
+		setupFormForItem: async function(itemId){
+			console.log("Setting up item stored add form for item ", itemId);
+			this.resetForm(false);
 			Getters.InventoryItem.get(itemId, async function (item) {
 				let promises = [];
-				ItemStoredTransaction.Add.resetForm();
-				ItemStoredTransaction.Add.idInput.val(itemId);
+
+				ItemStoredTransaction.Add.itemIdInput.val(item.id);
+				ItemStoredTransaction.Add.itemNameInput.val(item.name);
+				ItemStoredTransaction.Add.itemDisplayName.text(item.name);
 
 				item.storageBlocks.forEach(function(blockId){
 					let blockOp = $("<option></option>");
@@ -79,6 +114,22 @@ const ItemStoredTransaction = {
 				);
 				await Promise.all(promises);
 			});
+		},
+		setupForm: async function(itemId = null, preselectedStored = null, buttonElement = null) {
+			console.log("Setting up item stored add transaction form for item", itemId);
+			ModalHelpers.setReturnModal(this.modal, buttonElement);
+			this.resetForm();
+			if(itemId != null){
+				console.log("Given an item, keeping inputs disabled.");
+				this.itemDisplayContainer.show();
+				await this.setupFormForItem(itemId, false);
+			} else {
+				console.log("Enabling for searching for item.");
+				this.itemInputContainer.show();
+				this.itemNameInput.prop("disabled", false);
+				this.itemClearButton.prop("disabled", false);
+				this.itemSearchButton.prop("disabled", false);
+			}
 		}
 	},
 	Checkin: {
