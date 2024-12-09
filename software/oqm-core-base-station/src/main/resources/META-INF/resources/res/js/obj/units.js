9301ae5cbba87f5c7e47a129bed92fb80c51b3f1
GregJohnStewart <contact@gjstewart.net>
2024-12-09 14:40:55 -0500

    Core - Base Station - Reworked getting unit options js (not usages)

--- software/oqm-core-base-station/src/main/resources/META-INF/resources/res/js/obj/units.js
+++ software/oqm-core-base-station/src/main/resources/META-INF/resources/res/js/obj/units.js
@@ -1,5 +1,52 @@
 const UnitUtils = {
-	unitOptions: $("#unitSelectOptions").children(),
+	allUnitOptionsCache: null,
+	compatibleUnitOptionsCache: {},
+
+	getAllUnitOptions: async function (selectedVal = null) {
+		if (this.allUnitOptionsCache == null) {
+			await Rest.call({
+				method: "GET",
+				url: Rest.apiRoot + "/pageComponents/unit/inputs/all",
+				returnType: "html",
+				extraHeaders: {
+					"accept": "text/html",
+				},
+				done: function (allUnitOptions) {
+					UnitUtils.allUnitOptionsCache = allUnitOptions;
+				}
+			});
+		}
+
+		let output = $(this.allUnitOptionsCache);
+
+		if (selectedVal == null) {
+			//TODO:: find value, mark as selected
+		}
+		return output;
+	},
+	getCompatibleUnitOptions: async function (unit, selectedVal = null) {
+		if (unit in this.compatibleUnitOptionsCache) {
+			await Rest.call({
+				method: "GET",
+				url: Rest.apiRoot + "/pageComponents/unit/inputs/all",
+				returnType: "html",
+				extraHeaders: {
+					"accept": "text/html",
+				},
+				done: function (compatibleUnitOptions) {
+					UnitUtils.compatibleUnitOptionsCache[unit] = compatibleUnitOptions;
+				}
+			});
+		}
+
+		let output = $(this.compatibleUnitOptionsCache[unit]);
+
+		if (selectedVal == null) {
+			//TODO:: find value, mark as selected
+		}
+		return output;
+	},
+
 	/**
 	 * Example input: {"unit":{"string":"units","name":"Units","symbol":"units"},"scale":"ABSOLUTE","value":8}
 	 * @param quantityObj
@@ -7,41 +54,7 @@ const UnitUtils = {
 	quantityToDisplayStr(quantityObj) {
 		return quantityObj.value + quantityObj.unit.symbol;
 	},
-	getUnitOptions(selectedVal) {
-		let output = UnitUtils.unitOptions.clone();
 
-//    if(selectedVal){
-//        output.each(function(i, curOptGrp){
-//            $(curOptGrp).children().each(function(j, curOption){
-//                if(curOption.value == selectedVal){
-//                    curOption.attributes["selected"] = true;
-//                }
-//            });
-//        });
-//    }
-		return output;
-	},
-	//TODO:: review usage of ItemAddEdit.compatibleUnitOptions, and where it/this function should live
-	updateCompatibleUnits(unitToCompatWith, containerToSearch = null) {
-		Rest.call({
-			url: Rest.passRoot + "/inventory/unit/compatibility/" + unitToCompatWith,
-			extraHeaders: {accept: "text/html"},
-			dataType: "html",
-			returnType: "html",
-			async: false,
-			done: function (data) {
-				ItemAddEdit.compatibleUnitOptions = data;
-
-				if(containerToSearch != null) {
-					containerToSearch.find(".unitInput").each(function (i, selectInput) {
-						var selectInputJq = $(selectInput);
-						selectInputJq.html(ItemAddEdit.compatibleUnitOptions);
-						selectInputJq.change();
-					});
-				}
-			}
-		});
-	},
 	getUnitObj(unitStr) {
 		let output = {
 			string: unitStr
@@ -58,7 +71,7 @@ const UnitUtils = {
 
 		return output;
 	},
-	getQuantityFromInputs(inputsContainerJq){
+	getQuantityFromInputs(inputsContainerJq) {
 		let output = this.getQuantityObj(
 			inputsContainerJq.find('input[name="amountStored"]').val(),
 			inputsContainerJq.find('select[name="amountStoredUnit"]').val()
