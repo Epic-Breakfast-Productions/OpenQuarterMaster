f58fd240fb78621b89e5c59d6a46cc7e12aa36e9
GregJohnStewart <contact@gjstewart.net>
2025-01-21 19:14:37 -0500

    Infrastructure- Added Traefik, last attempts with nginx, some adjustment for keycloak

--- deployment/Single Host/Infrastructure/nginx/setup-proxy-config.py
+++ deployment/Single Host/Infrastructure/nginx/setup-proxy-config.py
@@ -1,4 +1,9 @@
 #!/bin/python3
+#
+# Setup Proxy Config Script
+# This script sets up the nginx proxy configuration file for Open QuarterMaster.
+#
+
 import logging
 import os
 import json
@@ -6,6 +11,12 @@ import sys
 from html.parser import HTMLParser
 sys.path.append("/usr/lib/oqm/station-captain/")
 from ConfigManager import *
+from LogUtils import *
+LogUtils.setupLogging("infra-nginx-proxy-config.log", "--verbose" in sys.argv)
+
+log = LogUtils.setupLogger("main")
+log.info("==== STARTING NGINX CONFIG GENERATION ====")
+log.info("Adding proxy config to nginx config.")
 
 CONFIG_TEMPLATE_FILE = "/etc/oqm/serviceConfig/infra/nginx/default.conf.template"
 PROXY_CONFIG_DIR = "/etc/oqm/proxyConfig.d"
@@ -14,25 +25,21 @@ RESULT_CONFIG_FILE = "/tmp/oqm/serviceConfig/infra/nginx/config.d/default.conf"
 INDEX_TEMPLATE_FILE = "/etc/oqm/serviceConfig/infra/nginx/indexTemplate.html"
 INDEX_FILE = "/etc/oqm/serviceConfig/infra/nginx/webroot/index.html"
 
-logging.info("Adding proxy config to nginx config.")
 
-upstreamText = """
-# Upstreams. Added via script
-"""
-
-locationText = """
-    # Locations for upstreams. Added via script
-"""
+upstreamText = "\n"
+locationText = "\n"
+redirect = ""
 
 coreLinks = ""
 infraLinks = ""
 
 for curProxyConfigFile in os.listdir(PROXY_CONFIG_DIR):
     if curProxyConfigFile.endswith(".json"):
+        log.info("Cur proxy file: " + curProxyConfigFile)
         with open(PROXY_CONFIG_DIR + "/" + curProxyConfigFile, 'r') as stream:
             curProxyConfig = json.load(stream)
 
-        curProxyConfig["path"] = "/" + curProxyConfig["type"] + "/" + curProxyConfig["name"] + "/"
+        curProxyConfig["path"] = "/" + curProxyConfig["type"] + "/" + curProxyConfig["name"]
 
         if "host" not in curProxyConfig:
             curProxyConfig["host"] = mainCM.getConfigVal(curProxyConfig["hostConfig"])
@@ -40,6 +47,15 @@ for curProxyConfigFile in os.listdir(PROXY_CONFIG_DIR):
             curProxyConfig["port"] = mainCM.getConfigVal(curProxyConfig["portConfig"])
 
         curProxyConfig["upstreamName"] = curProxyConfig["host"]
+
+        log.info("Using proxy config: %s", curProxyConfig)
+
+        if curProxyConfig["type"] == mainCM.getConfigVal("infra.nginx.defaultUi.type") and curProxyConfig["name"] == mainCM.getConfigVal("infra.nginx.defaultUi.name"):
+            redirect = ("""
+<script>
+	window.location.replace("{landingPage}");
+</script>
+""".format(landingPage=mainCM.getConfigVal("infra.nginx.externalRootUri")+curProxyConfig["path"]))
 
         upstreamText += ("""
 upstream {upstreamName} {{
@@ -54,64 +70,79 @@ upstream {upstreamName} {{
 
         locationText += """
     location {path} {{
-        proxy_pass http://{upstreamName}/;
+        resolver 127.0.0.11 valid=1s; # https://stackoverflow.com/questions/32845674/nginx-how-to-not-exit-if-host-not-found-in-upstream
+        #set $dockerHost {upstreamName};
+        
+        # TODO:: determine if https or not
+        proxy_pass https://{upstreamName};
         #proxy_redirect http://{upstreamName}/ /;
+        
+        proxy_buffering off;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-Host $host;
+        proxy_set_header X-Forwarded-Port $server_port;
+        
+        #proxy_redirect off;
+        
+        
+        # https://docs.nginx.com/nginx/admin-guide/security-controls/securing-http-traffic-upstream/
+        proxy_ssl_verify       off;
+        #proxy_ssl_certificate     /etc/nginx/client.pem;
+        #proxy_ssl_certificate_key /etc/nginx/client.key;
+
+        
+        
+        
+        
 
         # tell backend that message compression not allowed
-        proxy_set_header Accept-Encoding "";
+        # proxy_set_header Accept-Encoding "";
 
-        sub_filter_types *;
-        sub_filter 'action="/'  'action="{path}';
-        sub_filter 'href="/'  'href="{path}';
-        sub_filter 'src="/'  'src="{path}';
-        sub_filter 'action="./'  'action="{path}';
-        sub_filter 'href="./'  'href="{path}';
-        sub_filter 'src="./'  'src="{path}';
-        sub_filter_once off;
+        # sub_filter_types *;
+        # sub_filter 'action="/'  'action="{path}';
+        # sub_filter 'href="/'  'href="{path}';
+        # sub_filter 'src="/'  'src="{path}';
+        # sub_filter 'action="./'  'action="{path}';
+        # sub_filter 'href="./'  'href="{path}';
+        # sub_filter 'src="./'  'src="{path}';
+        # sub_filter_once off;
 
-        add_header Pragma "no-cache";
-        add_header Cache-Control "no-cache";
+        # add_header Pragma "no-cache";
+        # add_header Cache-Control "no-cache";
 
-        include  /etc/nginx/mime.types;
-        include /etc/nginx/conf.other.d/proxy_params;
+        # include  /etc/nginx/mime.types;
+        # include /etc/nginx/conf.other.d/proxy_params;
     }}
 
 """.format(
             path=curProxyConfig["path"],
             upstreamName=curProxyConfig["upstreamName"]
         )
-        if "showInProxyRoot" not in curProxyConfig or curProxyConfig["showInProxyRoot"]:
-            if curProxyConfig["type"] == "infra":
-                infraLinks += """
-                <li><a href="{path}">{title}</a></li>            
-                """.format(
-                    path=curProxyConfig["path"],
-                    title=curProxyConfig["title"]
-                )
-            if curProxyConfig["type"] == "core":
-                coreLinks += """
-                <li><a href="{path}">{title}</a></li>            
-                """.format(
-                    path=curProxyConfig["path"],
-                    title=curProxyConfig["title"]
-                )
     else:
         continue
 
-logging.info("Done reading in proxy config files.")
+log.info("Done reading in proxy config files.")
 
 with open(CONFIG_TEMPLATE_FILE, "r") as f:
     configData = f.read()
-configData = configData.format(upstreamText=upstreamText, locationText=locationText)
+configData = configData.format(
+    # upstreamText=upstreamText,
+    upstreamText="\n",
+    locationText=locationText,
+    hostHost=mainCM.getConfigVal("system.hostname")
+)
 os.makedirs(os.path.dirname(RESULT_CONFIG_FILE), exist_ok=True)
 with open(RESULT_CONFIG_FILE, "w") as f:
     f.write(configData)
-logging.info("Finished writing new config file.")
+log.info("Finished writing new config file.")
 
 with open(INDEX_TEMPLATE_FILE, "r") as f:
     index = f.read()
-index = index.format(coreLinks=coreLinks, infraLinks=infraLinks)
+index = index.format(redirect=redirect)
 os.makedirs(os.path.dirname(INDEX_FILE), exist_ok=True)
 with open(INDEX_FILE, "w") as f:
     f.write(index)
-logging.info("Finished writing new index file.")
+log.info("Finished writing new index file.")
+
+
+log.info("==== END OF NGINX CONFIG GENERATION ====")
---
+++ deployment/Single Host/Infrastructure/traefik/setup-proxy-config.py
@@ -0,0 +1,87 @@
+#!/bin/python3
+#
+# Setup Proxy Config Script
+# This script sets up the nginx proxy configuration file for Open QuarterMaster.
+#
+
+import logging
+import os
+import json
+import sys
+from os.path import basename
+
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+sys.path.append("/usr/lib/oqm/station-captain/")
+from ConfigManager import *
+from LogUtils import *
+
+LogUtils.setupLogging("infra-traefik-proxy-config.log", "--verbose" in sys.argv)
+
+log = LogUtils.setupLogger("main")
+log.info("==== STARTING TRAEFIK CONFIG GENERATION ====")
+log.info("Adding proxy config to traefik config.")
+
+CONFIG_TEMPLATE_FILE = "/etc/oqm/serviceConfig/infra/traefik/traefik-dynamic-config-template.yaml.j2"
+PROXY_CONFIG_DIR = "/etc/oqm/proxyConfig.d"
+RESULT_CONFIG_FILE = "/tmp/oqm/serviceConfig/infra/traefik/config.d/dynamicConfig/traefik-dynamic-config.yaml"
+TRAEFIK_CONTAINER_CERT_LOCATION = "/etc/traefik/certs/"
+
+templateData = {
+    "services": [],
+    "certs": {
+        "method": "self",
+        "default": {
+            "key": TRAEFIK_CONTAINER_CERT_LOCATION + "privateKey.pem",
+            "cert": TRAEFIK_CONTAINER_CERT_LOCATION + "publicCert.crt"
+        },
+        "certList": [
+            {
+                "key": TRAEFIK_CONTAINER_CERT_LOCATION + "privateKey.pem",
+                "cert": TRAEFIK_CONTAINER_CERT_LOCATION + "publicCert.crt"
+            },
+            {
+                "key": TRAEFIK_CONTAINER_CERT_LOCATION + "rootCertKey.pem",
+                "cert": TRAEFIK_CONTAINER_CERT_LOCATION + "rootCert.crt"
+            }
+        ]
+    }
+}
+
+for curProxyConfigFile in os.listdir(PROXY_CONFIG_DIR):
+    if curProxyConfigFile.endswith(".json"):
+        log.info("Cur proxy file: " + curProxyConfigFile)
+        with open(PROXY_CONFIG_DIR + "/" + curProxyConfigFile, 'r') as stream:
+            curProxyConfig = json.load(stream)
+
+        curProxyConfig["serviceName"] = curProxyConfig["type"] + "-" + curProxyConfig["name"]
+        curProxyConfig["proxyPath"] = "/" + curProxyConfig["type"] + "/" + curProxyConfig["name"]
+
+        if "internalBaseUri" not in curProxyConfig:
+            curProxyConfig["internalBaseUri"] = mainCM.getConfigVal(curProxyConfig["internalBaseUriConfig"])
+        if "preservePath" not in curProxyConfig:
+            curProxyConfig["preservePath"] = False
+        if "stripPrefixes" not in curProxyConfig:
+            curProxyConfig["stripPrefixes"] = False
+
+        log.info("Using proxy config: %s", curProxyConfig)
+        templateData["services"].append(curProxyConfig)
+    else:
+        continue
+
+log.info("Done reading in proxy config files. Data: %s", templateData)
+
+env = Environment(
+    loader=FileSystemLoader(os.path.dirname(CONFIG_TEMPLATE_FILE)),
+    autoescape=select_autoescape()
+)
+template = env.get_template(os.path.basename(CONFIG_TEMPLATE_FILE))
+
+traefikConfig = template.render(templateData)
+
+os.makedirs(os.path.dirname(RESULT_CONFIG_FILE), exist_ok=True)
+with open(RESULT_CONFIG_FILE, "w") as f:
+    f.write(traefikConfig)
+log.info("Finished writing new config file.")
+
+log.info("==== END OF TRAEFIK CONFIG GENERATION ====")
