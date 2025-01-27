625e530af7ad509275179bd6d25b92f9c7316965
GregJohnStewart <contact@gjstewart.net>
2025-01-27 10:27:33 -0500

    Plugin - Mapper - Initial dump of project files

---
+++ software/plugins/mapper/src/main/java/tech/ebp/oqm/plugin/MyEntityResource.java
@@ -0,0 +1,6 @@
+package tech.ebp.oqm.plugin;
+
+import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
+
+public interface MyEntityResource extends PanacheEntityResource<MyEntity, Long> {
+}
