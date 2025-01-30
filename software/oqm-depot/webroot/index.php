d1fb5697761876801f350422a12fed8fad40a252
GregJohnStewart <contact@gjstewart.net>
2025-01-29 19:34:07 -0500

    Core Depot - Fix to one of the urls

--- software/oqm-depot/webroot/index.php
+++ software/oqm-depot/webroot/index.php
@@ -169,7 +169,7 @@ function getEntryTable($name, $content) {
 			Version
 			<a href="https://github.com/Epic-Breakfast-Productions/OpenQuarterMaster" target="_blank"><?php echo $depotVersion; ?></a>, &copy; 2024
 			<a href="https://epic-breakfast-productions.tech/" target="_blank">EBP
-				<img src="/res/media/EBP-logo-icon.svg" style="max-height:1.2em;" alt="EBP Logo"/></a><br/>
+				<img src="<?=Context::getRootPath();?>/res/media/EBP-logo-icon.svg" style="max-height:1.2em;" alt="EBP Logo"/></a><br/>
 			Released under the
 			<a href="https://github.com/Epic-Breakfast-Productions/OpenQuarterMaster/blob/main/LICENSE" target="_blank">GPL v3.0 License</a><br/>
 		</div>
