0397868f0d18064f943c0cae4e0fe05f1196b0ea
GregJohnStewart <contact@gjstewart.net>
2025-02-01 15:25:08 -0500

    Core - Base Station - Additional config for working behind reverse proxy

--- software/oqm-core-base-station/makeInstallers.sh
+++ software/oqm-core-base-station/makeInstallers.sh
@@ -97,6 +97,8 @@ if [ ! -f "/etc/oqm/serviceConfig/core/base+station/user-config.list" ]; then
 # Configuration here will override those in base-station-config.list
 # Reference: https://github.com/Epic-Breakfast-Productions/OpenQuarterMaster/blob/main/software/open-qm-base-station/docs/BuildingAndDeployment.adoc
 
+#quarkus.log.level=DEBUG
+#quarkus.oqmCoreAPi.refreshDbCacheFrequency=600s
 
 EOF
 fi
