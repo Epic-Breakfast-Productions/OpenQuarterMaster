8faf7e565af4cd63227eebc45a91810fb51c979b
GregJohnStewart <contact@gjstewart.net>
2025-05-02 00:35:56 -0400

    Lib - CoreAPI - Quarkus - Minor tweaks after update from dev

--- software/libs/core-api-lib-quarkus/deployment/src/main/java/tech/ebp/oqm/lib/core/api/quarkus/deployment/CoreApiLibBuildTimeConfig.java
+++ software/libs/core-api-lib-quarkus/deployment/src/main/java/tech/ebp/oqm/lib/core/api/quarkus/deployment/CoreApiLibBuildTimeConfig.java
@@ -55,7 +55,7 @@ public interface CoreApiLibBuildTimeConfig {
 		/**
 		 * The version/ tag of the core api container image
 		 */
-		@WithDefault("2.1.7-SNAPSHOT")
+		@WithDefault("3.0.0-DEV")
 		String coreApiVersion();
 		
 		/**
