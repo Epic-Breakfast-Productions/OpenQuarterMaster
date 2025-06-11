d4d6ea49c0eaf31242eaae150a5e6932ad323f0b
GregJohnStewart <contact@gjstewart.net>
2025-06-10 21:55:56 -0400

    Core - Base Station - Fixed issue getting stored from search

--- software/oqm-core-base-station/src/main/java/tech/ebp/oqm/core/baseStation/interfaces/rest/passthrough/inventory/ItemStoredPassthrough.java
+++ software/oqm-core-base-station/src/main/java/tech/ebp/oqm/core/baseStation/interfaces/rest/passthrough/inventory/ItemStoredPassthrough.java
@@ -30,16 +30,16 @@ import java.util.Optional;
 @RequestScoped
 @Produces(MediaType.TEXT_HTML)
 public class ItemStoredPassthrough extends PassthroughProvider {
-
+	
 	@Getter
 	@Inject
 	@Location("tags/search/itemStored/searchResults")
 	Template searchResultTemplate;
-
+	
 	@Getter
 	@PathParam("itemId")
 	String itemId;
-
+	
 	@Inject
 	SearchResultTweak searchResultTweak;
 	
@@ -62,63 +62,69 @@ public class ItemStoredPassthrough extends PassthroughProvider {
 		@HeaderParam("showStorage") boolean showStorage,
 		@HeaderParam("actionType") Optional<String> actionType
 	) {
-		return this.processSearchResults(
-			this.getOqmCoreApiClient()
-				.invItemStoredSearch(this.getBearerHeaderStr(), this.getSelectedDb(), this.getItemId(), storedSearch)
-				.call(results -> searchResultTweak.addStorageBlockLabelToSearchResult(results, this.getSelectedDb(), "storageBlock", this.getBearerHeaderStr()))
-				.call(results -> searchResultTweak.addItemNameToSearchResult(results, this.getSelectedDb(), "item", this.getBearerHeaderStr()))
-			,
-			this.searchResultTemplate
-				.data("showItem", showItem)
-				.data("showStorage", showStorage)
-				.data("showType", false)
-			,
-			acceptType,
-			searchFormId,
-			otherModalId,
-			inputIdPrepend,
-			actionType.orElse("select")
-		);
+		return this.getOqmCoreApiClient()
+				   .invItemGet(this.getBearerHeaderStr(), this.getSelectedDb(), this.getItemId())
+				   .chain((item)->{
+						   return this.processSearchResults(
+							   this.getOqmCoreApiClient()
+								   .invItemStoredSearch(this.getBearerHeaderStr(), this.getSelectedDb(), this.getItemId(), storedSearch)
+								   .call(results->searchResultTweak.addStorageBlockLabelToSearchResult(results, this.getSelectedDb(), "storageBlock", this.getBearerHeaderStr()))
+								   .call(results->searchResultTweak.addItemNameToSearchResult(results, this.getSelectedDb(), "item", this.getBearerHeaderStr()))
+							   ,
+							   this.searchResultTemplate
+								   .data("showItem", showItem)
+								   .data("showStorage", showStorage)
+								   .data("showType", false)
+								   .data("inventoryItem", item)
+							   ,
+							   acceptType,
+							   searchFormId,
+							   otherModalId,
+							   inputIdPrepend,
+							   actionType.orElse("select")
+						   );
+					   }
+				   );
 	}
 	
-//	@GET //TODO if necessary
-//	@Path("history")
-//	@Operation(
-//		summary = "Searches all of an item's stored entries."
-//	)
-//	@APIResponse(
-//		responseCode = "200",
-//		description = "Stored entries retrieved."
-//	)
-//	@Produces({MediaType.APPLICATION_JSON, MediaType.TEXT_HTML})
-//	public Uni<Response> searchHistory(
-//		@BeanParam StoredSearch storedSearch,
-//		@HeaderParam("Accept") String acceptType,
-//		@HeaderParam("searchFormId") String searchFormId,
-//		@HeaderParam("otherModalId") String otherModalId,
-//		@HeaderParam("inputIdPrepend") String inputIdPrepend,
-//		@HeaderParam("showItem") boolean showItem,
-//		@HeaderParam("showStorage") boolean showStorage,
-//		@HeaderParam("actionType") Optional<String> actionType
-//	) {
-//		return this.processSearchResults(
-//			this.getOqmCoreApiClient()
-//				.invItemStoredSearch(this.getBearerHeaderStr(), this.getSelectedDb(), this.getItemId(), storedSearch)
-//				.call(results -> searchResultTweak.addStorageBlockLabelToSearchResult(results, this.getSelectedDb(), "storageBlock", this.getBearerHeaderStr()))
-//				.call(results -> searchResultTweak.addItemNameToSearchResult(results, this.getSelectedDb(), "item", this.getBearerHeaderStr()))
-//			,
-//			this.searchResultTemplate
-//				.data("showItem", showItem)
-//				.data("showStorage", showStorage)
-//				.data("showType", false)
-//			,
-//			acceptType,
-//			searchFormId,
-//			otherModalId,
-//			inputIdPrepend,
-//			actionType.orElse("select")
-//		);
-//	}
+	//	@GET //TODO if necessary
+	//	@Path("history")
+	//	@Operation(
+	//		summary = "Searches all of an item's stored entries."
+	//	)
+	//	@APIResponse(
+	//		responseCode = "200",
+	//		description = "Stored entries retrieved."
+	//	)
+	//	@Produces({MediaType.APPLICATION_JSON, MediaType.TEXT_HTML})
+	//	public Uni<Response> searchHistory(
+	//		@BeanParam StoredSearch storedSearch,
+	//		@HeaderParam("Accept") String acceptType,
+	//		@HeaderParam("searchFormId") String searchFormId,
+	//		@HeaderParam("otherModalId") String otherModalId,
+	//		@HeaderParam("inputIdPrepend") String inputIdPrepend,
+	//		@HeaderParam("showItem") boolean showItem,
+	//		@HeaderParam("showStorage") boolean showStorage,
+	//		@HeaderParam("actionType") Optional<String> actionType
+	//	) {
+	//		return this.processSearchResults(
+	//			this.getOqmCoreApiClient()
+	//				.invItemStoredSearch(this.getBearerHeaderStr(), this.getSelectedDb(), this.getItemId(), storedSearch)
+	//				.call(results -> searchResultTweak.addStorageBlockLabelToSearchResult(results, this.getSelectedDb(), "storageBlock", this.getBearerHeaderStr()))
+	//				.call(results -> searchResultTweak.addItemNameToSearchResult(results, this.getSelectedDb(), "item", this.getBearerHeaderStr()))
+	//			,
+	//			this.searchResultTemplate
+	//				.data("showItem", showItem)
+	//				.data("showStorage", showStorage)
+	//				.data("showType", false)
+	//			,
+	//			acceptType,
+	//			searchFormId,
+	//			otherModalId,
+	//			inputIdPrepend,
+	//			actionType.orElse("select")
+	//		);
+	//	}
 	
 	
 	@GET
@@ -196,7 +202,9 @@ public class ItemStoredPassthrough extends PassthroughProvider {
 		@HeaderParam("Accept") String acceptType,
 		@HeaderParam("searchFormId") String searchFormId
 	) {
-		Uni<ObjectNode> searchUni = this.getOqmCoreApiClient().invItemStoredSearchHistory(this.getBearerHeaderStr(), this.getSelectedDb(), this.getItemId(), storedId, searchObject);
+		Uni<ObjectNode>
+			searchUni =
+			this.getOqmCoreApiClient().invItemStoredSearchHistory(this.getBearerHeaderStr(), this.getSelectedDb(), this.getItemId(), storedId, searchObject);
 		return this.processHistoryResults(searchUni, acceptType, searchFormId);
 	}
 }
