911347c112fb9bc583e8422de5d24de6ab147a5d
GregJohnStewart <contact@gjstewart.net>
2025-09-03 11:05:38 -0400

    Core - API - Fixed generic not being able to select if barcode or not

--- software/core/oqm-core-api/src/main/java/tech/ebp/oqm/core/api/model/object/storage/items/identifiers/general/Generic.java
+++ software/core/oqm-core-api/src/main/java/tech/ebp/oqm/core/api/model/object/storage/items/identifiers/general/Generic.java
@@ -1,5 +1,6 @@
 package tech.ebp.oqm.core.api.model.object.storage.items.identifiers.general;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,6 +19,9 @@ public class Generic extends GeneralId {
 	
 	@NotBlank
 	private String value;
+	
+	@JsonProperty(access = JsonProperty.Access.READ_WRITE)
+	private boolean barcode = false;
 	
 	@Override
 	public GeneralIdType getType() {
