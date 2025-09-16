41602d6e6934ec12ca9020d8672406669f7e4989
GregJohnStewart <contact@gjstewart.net>
2025-09-15 22:21:51 -0400

    Lib - Core API for Quarkus - Version bumps for dev, adding unique id endpoints

---
+++ software/libs/core-api-lib-quarkus/runtime/src/main/java/tech/ebp/oqm/lib/core/api/quarkus/runtime/restClient/searchObjects/UniqueIdGeneratorSearch.java
@@ -0,0 +1,13 @@
+package tech.ebp.oqm.lib.core.api.quarkus.runtime.restClient.searchObjects;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@ToString(callSuper = true)
+@Getter
+@Setter
+public class UniqueIdGeneratorSearch extends SearchObject {
+}
