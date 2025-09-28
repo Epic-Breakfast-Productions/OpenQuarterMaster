0b26f14923bdba1015e85dc4bdb8d2724543bcab
GregJohnStewart <contact@gjstewart.net>
2025-09-27 20:55:42 -0400

    Core - API - Adjusting general id barcode endpoints, added unique id barcode endpoints

--- software/core/oqm-core-api/src/main/java/tech/ebp/oqm/core/api/interfaces/endpoints/media/identifiers/UniqueIdImageEndpoints.java
+++ software/core/oqm-core-api/src/main/java/tech/ebp/oqm/core/api/interfaces/endpoints/media/identifiers/UniqueIdImageEndpoints.java
@@ -17,34 +17,22 @@ import tech.ebp.oqm.core.api.interfaces.endpoints.EndpointProvider;
 import tech.ebp.oqm.core.api.model.object.storage.items.identifiers.general.GeneralIdType;
 import tech.ebp.oqm.core.api.model.rest.auth.roles.Roles;
 import tech.ebp.oqm.core.api.service.identifiers.general.GeneralIdBarcodeService;
-import tech.ebp.oqm.core.api.service.identifiers.general.GeneralIdUtils;
+import tech.ebp.oqm.core.api.service.identifiers.unique.UniqueIdBarcodeService;
 
 /**
  */
 @Slf4j
-@Path("/api/media/code/identifier/general")
+@Path("/api/media/code/identifier/unique")
 @Tags({@Tag(name = "Media", description = "Endpoints for media CRUD")})
 @RolesAllowed(Roles.INVENTORY_VIEW)
 @RequestScoped
-public class GeneralIdImageEndpoints extends EndpointProvider {
+public class UniqueIdImageEndpoints extends EndpointProvider {
 	
 	@Inject
-	GeneralIdBarcodeService generalIdBarcodeService;
-	
-	private Response getBarcodeResponse(
-		GeneralIdType codeType,
-		String content,
-		String svgData
-	) {
-		return Response.status(Response.Status.OK)
-				   .entity(svgData)
-				   .header("Content-Disposition", "attachment;filename=" + codeType + "_" + content.replaceAll("\\W+", "") + ".svg")
-				   .type(GeneralIdBarcodeService.DATA_MEDIA_TYPE)
-				   .build();
-	}
+	UniqueIdBarcodeService uniqueIdBarcodeService;
 	
 	@GET
-	@Path("{type}/{value}")
+	@Path("{value}")
 	@Operation(
 		summary = "A barcode that represents the string given."
 	)
@@ -54,17 +42,14 @@ public class GeneralIdImageEndpoints extends EndpointProvider {
 	)
 	@Produces(GeneralIdBarcodeService.DATA_MEDIA_TYPE)
 	public Response getBarcode(
-		@PathParam("type") GeneralIdType type,
 		@PathParam("value") String data
 	) {
-		log.info("Getting {} barcode.", type);
-		if(!GeneralIdUtils.isValidCode(type, data)){
-			return Response.status(Response.Status.BAD_REQUEST).entity("Value not a valid " + type).build();
-		}
+		log.info("Getting barcode for unique id value: {}", data);
+		
 		return Response.status(Response.Status.OK)
-				   .entity(this.generalIdBarcodeService.getGeneralIdData(type, data))
+				   .entity(this.uniqueIdBarcodeService.getUniqueIdData(data))
 				   .header("Content-Disposition", "attachment;filename=" + "code.svg")
-				   .type(GeneralIdBarcodeService.DATA_MEDIA_TYPE)
+				   .type(UniqueIdBarcodeService.DATA_MEDIA_TYPE)
 				   .build();
 	}
 	
