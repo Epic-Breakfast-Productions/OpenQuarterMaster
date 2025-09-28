98f75521a2c79b942784716301660153b6b49d89
GregJohnStewart <contact@gjstewart.net>
2025-09-27 20:55:42 -0400

    Core - API - Adjusting general id barcode endpoints, added unique id barcode endpoints

--- software/core/oqm-core-api/src/test/java/tech/ebp/oqm/core/api/service/identifiers/unique/UniqueIdBarcodeServiceTest.java
+++ software/core/oqm-core-api/src/test/java/tech/ebp/oqm/core/api/service/identifiers/unique/UniqueIdBarcodeServiceTest.java
@@ -1,4 +1,4 @@
-package tech.ebp.oqm.core.api.service.identifiers.general;
+package tech.ebp.oqm.core.api.service.identifiers.unique;
 
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
@@ -9,27 +9,29 @@ import org.bson.types.ObjectId;
 import org.eclipse.microprofile.config.ConfigProvider;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import tech.ebp.oqm.core.api.model.object.storage.items.identifiers.general.GeneralId;
+import tech.ebp.oqm.core.api.model.object.storage.items.identifiers.unique.ProvidedUniqueId;
+import tech.ebp.oqm.core.api.model.object.storage.items.identifiers.unique.UniqueId;
 import tech.ebp.oqm.core.api.testResources.lifecycleManagers.TestResourceLifecycleManager;
+import tech.ebp.oqm.core.api.testResources.testClasses.WebServerTest;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
-
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.stream.Stream;
 
 @Slf4j
 @QuarkusTest
 @QuarkusTestResource(TestResourceLifecycleManager.class)
-class BarcodeServiceTest extends CodeUtilTestBase {
+class UniqueIdBarcodeServiceTest extends WebServerTest {
 	
 	private static void writeToFile(String data, String dir, String fileName) {
 		Path outputPath = Path.of(
 			"build/test-results/" +
 			ConfigProvider.getConfig().getValue("quarkus.profile", String.class) +
-			"/barcodes/" + dir + "/" +
+			"/barcodes/unique/" + dir + "/" +
 			fileName + ".svg"
 		);
 		
@@ -41,32 +43,43 @@ class BarcodeServiceTest extends CodeUtilTestBase {
 		}
 	}
 	
-	@Inject BarcodeService barcodeService;
-	
-	@ParameterizedTest
-	@MethodSource("getCodes")
-	public void generalIdBarcodeGenTest(String code, GeneralId identifier) {
-		StopWatch sw = StopWatch.createStarted();
-		String data = barcodeService.getGeneralIdData(identifier);
-		sw.stop();
-		log.info("Generated barcode for {} in {}", identifier, sw);
-		
-		assertNotNull(data);
-		assertFalse(data.isEmpty());
-		
-		writeToFile(data, "generalIds", identifier.getType() + "-" + identifier.getValue());
-	}
+	@Inject
+	UniqueIdBarcodeService uniqueIdBarcodeService;
 	
 	@Test
 	public void objectIdBarcodeGenTest() {
 		ObjectId identifier = new ObjectId();
 		
 		StopWatch sw = StopWatch.createStarted();
-		String data = barcodeService.getObjectIdData(identifier);
+		String data = uniqueIdBarcodeService.getObjectIdData(identifier);
 		sw.stop();
 		log.info("Generated barcode for ObjectId in {}", sw);
 		
 		writeToFile(data, "objectId", identifier.toHexString());
 	}
 	
+	public static Stream<Arguments> uniqueIds() {
+		return Stream.of(
+			Arguments.of(ProvidedUniqueId.builder()
+							 .barcode(true)
+							 .value(FAKER.idNumber().ssnValid())
+							 .label(FAKER.name().name())
+							 .build())
+		);
+	}
+	
+	@ParameterizedTest
+	@MethodSource("uniqueIds")
+	public void uniqueIdBarcodeGenTest(UniqueId identifier) {
+		
+		StopWatch sw = StopWatch.createStarted();
+		String data = uniqueIdBarcodeService.getUniqueIdData(identifier);
+		sw.stop();
+		log.info("Generated barcode for unique Id {} in {}", identifier, sw);
+		
+		writeToFile(data, "uniqueIds", identifier.getValue());
+	}
+	
+	
+
 }
