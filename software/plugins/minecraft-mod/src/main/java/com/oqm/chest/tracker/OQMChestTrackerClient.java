a96cd980fd7d643493955157d575bfea2e53125e
Simon <ramsey2005s@gmail.com>
2025-11-06 21:05:27 -0500

    change mod name to OQMChestTracker

--- software/plugins/minecraft-mod/src/main/java/com/oqm/chest/tracker/OQMChestTrackerClient.java
+++ software/plugins/minecraft-mod/src/main/java/com/oqm/chest/tracker/OQMChestTrackerClient.java
@@ -1,4 +1,4 @@
-package com.chest.tracker;
+package com.oqm.chest.tracker;
 
 import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
@@ -11,11 +11,11 @@ import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 // This class will not load on dedicated servers. Accessing client side code from here is safe.
-@Mod(value = ChestTracker.MODID, dist = Dist.CLIENT)
+@Mod(value = OQMChestTracker.MODID, dist = Dist.CLIENT)
 // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-@EventBusSubscriber(modid = ChestTracker.MODID, value = Dist.CLIENT)
-public class ChestTrackerClient {
-    public ChestTrackerClient(ModContainer container) {
+@EventBusSubscriber(modid = OQMChestTracker.MODID, value = Dist.CLIENT)
+public class OQMChestTrackerClient {
+    public OQMChestTrackerClient(ModContainer container) {
         // Allows NeoForge to create a config screen for this mod's configs.
         // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
         // Do not forget to add translations for your config options to the en_us.json file.
@@ -25,7 +25,7 @@ public class ChestTrackerClient {
     @SubscribeEvent
     static void onClientSetup(FMLClientSetupEvent event) {
         // Some client setup code
-        ChestTracker.LOGGER.info("HELLO FROM CLIENT SETUP");
-        ChestTracker.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        OQMChestTracker.LOGGER.info("HELLO FROM CLIENT SETUP");
+        OQMChestTracker.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
     }
 }
