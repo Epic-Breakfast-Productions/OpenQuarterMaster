36a23f63f6ccd0c34b841999534550168f5be9be
Coletrane315 <xcoletrane315x@gmail.com>
2025-11-14 23:41:11 -0500

    Testing setup, made method to make an item from images in testImages, currently throwing error on invItemCreate()

--- software/plugins/image-search/src/test/java/tech/ebp/oqm/plugin/imageSearch/BasicSetupTest.java
+++ software/plugins/image-search/src/test/java/tech/ebp/oqm/plugin/imageSearch/BasicSetupTest.java
@@ -1,5 +1,5 @@
 package tech.ebp.oqm.plugin.imageSearch;
-
+//TEST NEW FUNCTION
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.io.FileBackedOutputStream;
@@ -8,16 +8,18 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.core.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import tech.ebp.oqm.lib.core.api.quarkus.runtime.restClient.OqmCoreApiClientService;
 import tech.ebp.oqm.lib.core.api.quarkus.runtime.restClient.files.FileUploadBody;
 import tech.ebp.oqm.lib.core.api.quarkus.runtime.sso.KcClientAuthService;
 import tech.ebp.oqm.plugin.imageSearch.testResources.testClasses.RunningServerTest;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.net.URISyntaxException;
+import java.util.Objects;
+
+import static com.google.common.io.MoreFiles.listFiles;
 
 @Slf4j
 @QuarkusTest
@@ -30,8 +32,24 @@ public class BasicSetupTest extends RunningServerTest {
 	KcClientAuthService serviceAccountService;
 	
 	@Test
-	public void testBasicSetup() throws IOException {
-		
+	public void testBasicSetup() throws IOException, URISyntaxException {
+
+        //Parses testImages folder and gets a list of filenames
+        File directory = new File((Objects.requireNonNull(BasicSetupTest.class.getClassLoader().getResource("testImages/"))).toURI());
+        File[] fileList = directory.listFiles();
+        Assertions.assertNotNull(fileList);
+        String[] imageList = new String[fileList.length];
+        for(int i = 0; i < fileList.length; i++){
+            imageList[i] = fileList[i].getName();
+        }
+
+        //Makes an item for each image in testImages folder
+        for(int i = 0; i < imageList.length; i++){
+            uploadSingleImage(imageList[i], "test" + (i + 1) + ".jpg");
+        }
+
+        //uploadSingleImage("bin.jpg", "test1.jpg");
+		/*
 		//create image
 		String imageId;
 		try(
@@ -70,7 +88,7 @@ public class BasicSetupTest extends RunningServerTest {
 		
 		ObjectNode imageObj = this.oqmCoreApiClientService.imageGet(serviceAccountService.getAuthString(), "default", imageId).subscribeAsCompletionStage().join();
 		log.info("imageObj: {}", imageObj);
-		
+
 		//get image object, data
 		Response response = this.oqmCoreApiClientService.imageGetRevisionData(
 			this.serviceAccountService.getAuthString(), "default",
@@ -84,8 +102,71 @@ public class BasicSetupTest extends RunningServerTest {
 		){
 			((InputStream)response.getEntity()).transferTo(is);
 		}
-		
+		*/
+
 	}
+
+    //takes in the name of an image in the temporary testImages folder
+    //
+    public void uploadSingleImage(String resourceLocation, String outputFilename) throws IOException {
+        //uploads image object and generates image id
+        String imageId;
+        try(
+           InputStream inputStream = BasicSetupTest.class.getClassLoader().getResourceAsStream("testImages/" + resourceLocation);
+           ){
+           FileUploadBody testImageUploadObj = new FileUploadBody();
+           testImageUploadObj.file = inputStream;
+           testImageUploadObj.fileName = resourceLocation;
+           testImageUploadObj.description = FAKER.lorem().sentence();
+           testImageUploadObj.source = resourceLocation;
+
+           imageId = this.oqmCoreApiClientService.imageAdd(
+                   this.serviceAccountService.getAuthString(),
+                   "default",
+                   testImageUploadObj
+           ).subscribeAsCompletionStage().join();
+        }
+
+        imageId = imageId.replace("\"", "");
+        log.info("imageId: {}", imageId);
+
+        //create an item object for the image
+        ObjectNode item = JsonNodeFactory.instance.objectNode();
+        item.put("name", FAKER.appliance().equipment());
+        item.put("storageType", "BULK");
+        item.putObject("unit").put("string", "units");
+        item.putArray("imageIds").add(imageId);
+
+        //make item id
+        String itemId = this.oqmCoreApiClientService.invItemCreate(serviceAccountService.getAuthString(), "default", item).subscribeAsCompletionStage().join();
+        log.info("itemId: {}", itemId);
+
+        //establish item object
+        ObjectNode newItem = this.oqmCoreApiClientService.invItemGet(serviceAccountService.getAuthString(), "default", itemId.replace("\"", "")).subscribeAsCompletionStage().join();
+        log.info("Item: {}", newItem);
+
+        //check to make sure image object is good
+        ObjectNode imageObj = this.oqmCoreApiClientService.imageGet(serviceAccountService.getAuthString(), "default", imageId).subscribeAsCompletionStage().join();
+        log.info("imageObj: {}", imageObj);
+
+
+        //Get image object data, don't fully understand this part
+        Response response = this.oqmCoreApiClientService.imageGetRevisionData(
+            this.serviceAccountService.getAuthString(),
+            "default",
+            imageId,
+            "latest"
+        ).subscribeAsCompletionStage().join();
+        log.info("Response: {}", response);
+
+        //Pull the image data back down into a test image in a new folder
+        try(
+             OutputStream outputStream = new FileOutputStream("build/test-results/" + outputFilename);
+        ){
+            ((InputStream)response.getEntity()).transferTo(outputStream);
+        }
+
+    }
 	
 	
 }
