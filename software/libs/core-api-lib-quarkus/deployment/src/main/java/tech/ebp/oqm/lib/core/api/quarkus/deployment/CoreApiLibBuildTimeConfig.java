9d210b9000c476727006965ba1685a1659d492bf
GregJohnStewart <contact@gjstewart.net>
2025-11-19 19:20:55 -0500

    Lib - Core API for Quarkus - version bump for dev

--- software/libs/core-api-lib-quarkus/deployment/src/main/java/tech/ebp/oqm/lib/core/api/quarkus/deployment/CoreApiLibBuildTimeConfig.java
+++ software/libs/core-api-lib-quarkus/deployment/src/main/java/tech/ebp/oqm/lib/core/api/quarkus/deployment/CoreApiLibBuildTimeConfig.java
@@ -55,7 +55,7 @@ public interface CoreApiLibBuildTimeConfig {
 		/**
 		 * The version/ tag of the core api container image
 		 */
-		@WithDefault("4.0.1-DEV")
+		@WithDefault("4.1.0-DEV")
 		String coreApiVersion();
 		
 		/**
