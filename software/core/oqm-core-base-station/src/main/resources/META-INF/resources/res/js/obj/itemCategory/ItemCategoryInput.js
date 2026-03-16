66004088ec382b0bfc9a1a9c0db1d3f2b0f76aa0
GregJohnStewart <contact@gjstewart.net>
2026-03-16 00:19:14 -0400

    Core - Base Station - Now properly alerting use if couldn't add category

--- software/core/oqm-core-base-station/src/main/resources/META-INF/resources/res/js/obj/itemCategory/ItemCategoryInput.js
+++ software/core/oqm-core-base-station/src/main/resources/META-INF/resources/res/js/obj/itemCategory/ItemCategoryInput.js
@@ -1,5 +1,6 @@
 import {Rest} from "../../Rest.js";
 import {DselectUtils} from "../../DselectUtils.js";
+import {PageMessageUtils} from "../../PageMessageUtils.js";
 
 export const ItemCategoryInput = {
 	getValueFromInput(itemCatInputJq){
@@ -23,19 +24,22 @@ export const ItemCategoryInput = {
 		return val;
 	},
 
-	// Handle newly created categories from dselect creatable mode
-	handleNewCategory(selectElement, newCategoryName) {
+	/**
+	 * Handle newly created categories from dselect creatable mode
+ 	 */
+	handleNewCategory(selectElementJs, newCategoryName) {
 		console.log("Creating new category: ", newCategoryName);
+		let selectElement = $(selectElementJs);
 
 		Rest.call({
 			url: Rest.passRoot + "/inventory/item-category",
 			method: "POST",
 			data: { name: newCategoryName },
-			async: false,
+			failMessagesDiv: selectElement.parent().find(".category-input-messages"),
 			done: function(data) {
 				console.log("Created new category with ID: " + data.id);
 				// Update the option value from the text to the actual ID
-				let option = $(selectElement).find('option').filter(function() {
+				let option = selectElement.find('option').filter(function() {
 					return $(this).val() === newCategoryName && $(this).text() === newCategoryName;
 				});
 				if (option.length) {
@@ -45,19 +49,10 @@ export const ItemCategoryInput = {
 			fail: function(data) {
 				console.error("Failed to create category: ", data);
 				// Remove the invalid option
-				$(selectElement).find('option').filter(function() {
+				selectElement.find('option').filter(function() {
 					return $(this).val() === newCategoryName;
 				}).remove();
-				DselectUtils.setupDselect(selectElement);
-				// Consider adding user notification here
-				if (typeof PageMessageUtils !== 'undefined') {
-					PageMessageUtils.addMessageToDiv(
-						$(".messages-container:first"),
-						"warning",
-						"Failed to create category: " + newCategoryName,
-						"Category Creation Failed"
-					);
-				}
+				DselectUtils.setupDselect(selectElementJs);
 			}
 		});
 	},
@@ -76,7 +71,9 @@ export const ItemCategoryInput = {
 			// (dselect creates options with value === text for new items)
 			values.forEach(function(val) {
 				if (val && val !== "") {
-					let option = $(selectElement).find('option[value="' + val + '"]');
+					let option = $(selectElement).find('option').filter(function() {
+							return $(this).val() === val;
+						});
 					// If value equals text and doesn't look like a MongoDB ObjectId, it's new
 					if (option.length && option.text() === val && !val.match(/^[0-9a-fA-F]{24}$/)) {
 						ItemCategoryInput.handleNewCategory(selectElement, val);
