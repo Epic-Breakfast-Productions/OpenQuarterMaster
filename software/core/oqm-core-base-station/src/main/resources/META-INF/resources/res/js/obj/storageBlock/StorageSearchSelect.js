3aa1c0ec9325eaa01f01ec0c4ca5acb91d3e3f3c
GregJohnStewart <contact@gjstewart.net>
2026-03-25 20:44:47 -0400

    Core - Base Station - Initial setup of reworked storage block search related methods

--- software/core/oqm-core-base-station/src/main/resources/META-INF/resources/res/js/obj/storageBlock/StorageSearchSelect.js
+++ software/core/oqm-core-base-station/src/main/resources/META-INF/resources/res/js/obj/storageBlock/StorageSearchSelect.js
@@ -1,18 +1,72 @@
 import {Rest} from "../../Rest.js";
+import {ModalUtils} from "../../ModalUtils.js";
+import {Getters} from "../Getters";
 
 export const StorageSearchSelect = {
 	storageSearchSelectModal: $("#storageSearchSelectModal"),
 	storageSearchSelectForm: $("#storageSearchSelectForm"),
 	storageSearchSelectResults: $("#storageSearchSelectResults"),
-	setupStorageSearchModal(inputIdPrepend) {
+
+	setupStorageSearchModal(inputIdPrepend, initialModal) { //TODO:: make play nice with multiple inputs on one page
 		StorageSearchSelect.storageSearchSelectModal.attr("data-bs-inputIdPrepend", inputIdPrepend);
+
+		ModalUtils.setReturnModal(StorageSearchSelect.storageSearchSelectModal, initialModal);
 	},
-	selectStorageBlock(blockName, blockId, inputIdPrepend, otherModalId) {
+	selectStorageBlock(blockName, blockId, inputIdPrepend) {
 		let nameInputId = inputIdPrepend + "Id";
 		let nameInputName = inputIdPrepend + "Name";
 
 		$("#" + nameInputId).val(blockId);
 		$("#" + nameInputName).val(blockName);
+	},
+
+
+	input: {
+		clear(inputJq){
+			StorageSearchSelect.input.getIdInput(inputJq).val("");
+			StorageSearchSelect.input.getNameInput(inputJq).val("");
+		},
+		getSearchButton(inputJq){
+			return inputJq.find("button.searchButton");
+		},
+		getIdInput(inputJq){
+			return inputJq.find("input.idInput");
+		},
+		getNameInput(inputJq){
+			return inputJq.find("input.nameInput");
+		},
+		getClearButton(inputJq){
+			return inputJq.find("button.clearButton");
+		},
+		setReadonly(inputJq, readonly=true){
+			StorageSearchSelect.input.getSearchButton(inputJq).prop("disabled", readonly);
+			StorageSearchSelect.input.getNameInput(inputJq).prop("disabled", readonly);
+			StorageSearchSelect.input.getClearButton(inputJq).prop("disabled", readonly);
+		},
+		setValue(inputJq, storageBlockId, storageBlockName = null){
+			StorageSearchSelect.input.getIdInput(inputJq).val(storageBlockId);
+
+			if(storageBlockName){
+				StorageSearchSelect.input.getNameInput(inputJq).val(storageBlockName);
+			} else {
+				Getters.StorageBlock.getName(storageBlockId).then(function(name){
+					StorageSearchSelect.input.getNameInput(inputJq).val(name);
+				});
+			}
+		},
+		setup(
+			inputJq,
+			storageBlockId = null,
+			readOnly = false
+		){
+			StorageSearchSelect.input.clear(inputJq);
+
+			if(storageBlockId){
+				StorageSearchSelect.input.setValue(inputJq);
+			}
+
+			StorageSearchSelect.input.setReadonly(inputJq, readOnly);
+		}
 	},
 	initPage: function () {
 		console.log("Initializing storage block search select.");
