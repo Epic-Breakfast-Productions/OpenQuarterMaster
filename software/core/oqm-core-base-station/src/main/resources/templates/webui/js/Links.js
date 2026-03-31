52c6fe6f55caa9018cd90172efb95cf946518fa0
GregJohnStewart <contact@gjstewart.net>
2026-03-30 22:24:56 -0400

    Core - Base Station - links and page components to new js utilities

--- software/core/oqm-core-base-station/src/main/resources/templates/webui/js/Links.js
+++ software/core/oqm-core-base-station/src/main/resources/templates/webui/js/Links.js
@@ -1,52 +1,60 @@
 import { Icons } from "./Icons.js";
 
-//TODO:: determine if necessary
-export const Links = {
-	storage: '<a href="{rootPrefix}/storage">'+Icons.storageBlock+' Storage</a>',
+export class Links {
+	static storage = '<a href="{rootPrefix}/storage">' + Icons.storageBlock + ' Storage</a>'
 
-	getStorageViewLink: function (id, text=""){
+	static getStorageViewLink(id, text = "") {
 		let newLink = $(Links.storage);
 
 		newLink.prop("href", newLink.prop("href") + "?view=" + id);
 
-		if(text){
+		if (text) {
 			newLink.html(Icons.storageBlock);
 
 			newLink.append($(" \n<span></span>").text(text));
 		}
 
 		return newLink;
-	},
-	getStorageViewButton: function (id, text=""){
-		let newButton= Links.getStorageViewLink(id, text);
+	}
+
+	static getStorageViewButton(id, text = "") {
+		let newButton = Links.getStorageViewLink(id, text);
 		newButton.addClass("btn");
 		newButton.addClass("btn-primary");
 
 		return newButton;
-	},
-	getStorageViewButtonAsHtml(id, text=""){ return Links.getStorageViewButton(id, text).prop("outerHTML")},
+	}
+
+	static getStorageViewButtonAsHtml(id, text = "") {
+		return Links.getStorageViewButton(id, text).prop("outerHTML");
+	}
 
 
-	items: '<a href="{rootPrefix}/items">'+Icons.items+' Items</a>',
-	getItemViewLink: function (id, text=""){
+	static items = '<a href="{rootPrefix}/items">' + Icons.items + ' Items</a>';
+
+	static getItemViewLink(id, text = "") {
 		let newLink = $(Links.items);
 
 		newLink.prop("href", newLink.prop("href") + "?view=" + id);
 
-		if(text){
+		if (text) {
 			newLink.html(Icons.item);
 
 			newLink.append($(" \n<span></span>").text(text));
 		}
 
 		return newLink;
-	},
-	getItemViewButton: function (id, text=""){
-		let newButton= Links.getItemViewLink(id, text);
+	}
+
+	static getItemViewButton(id, text = "") {
+		let newButton = Links.getItemViewLink(id, text);
 		newButton.addClass("btn");
 		newButton.addClass("btn-primary");
 
 		return newButton;
-	},
-	getItemViewButtonAsHtml(id, text=""){ return Links.getItemViewButton(id, text).prop("outerHTML")},
+	}
+
+	static getItemViewButtonAsHtml(id, text = "") {
+		return Links.getItemViewButton(id, text).prop("outerHTML")
+	}
 }
