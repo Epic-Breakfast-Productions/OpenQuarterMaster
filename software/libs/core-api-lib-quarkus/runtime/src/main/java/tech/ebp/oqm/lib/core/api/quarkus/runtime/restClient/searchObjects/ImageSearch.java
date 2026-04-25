7ff355f8451901096cf48cdf2956b40b981f7b0e
GregJohnStewart <contact@gjstewart.net>
2026-04-25 01:10:51 -0400

    Lib - Core API for Quarkus - Fixed issue searching for files and images

--- software/libs/core-api-lib-quarkus/runtime/src/main/java/tech/ebp/oqm/lib/core/api/quarkus/runtime/restClient/searchObjects/ImageSearch.java
+++ software/libs/core-api-lib-quarkus/runtime/src/main/java/tech/ebp/oqm/lib/core/api/quarkus/runtime/restClient/searchObjects/ImageSearch.java
@@ -2,11 +2,15 @@ package tech.ebp.oqm.lib.core.api.quarkus.runtime.restClient.searchObjects;
 
 import jakarta.ws.rs.QueryParam;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
+@NoArgsConstructor
 @ToString(callSuper = true)
 @Getter
+@Setter
 @SuperBuilder(toBuilder = true)
 public class ImageSearch extends FileSearchObject {
 	@QueryParam("title") String imageTitle;
