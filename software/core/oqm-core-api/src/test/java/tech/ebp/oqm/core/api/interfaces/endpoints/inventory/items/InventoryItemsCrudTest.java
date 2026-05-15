7d863fa1ba157ca95c442807ab8a80b831450101
GregJohnStewart <contact@gjstewart.net>
2026-05-15 13:38:06 -0400

    Core - API - Added thread-based test for updating inventory items to ensure no deadlocking

--- software/core/oqm-core-api/src/test/java/tech/ebp/oqm/core/api/interfaces/endpoints/inventory/items/InventoryItemsCrudTest.java
+++ software/core/oqm-core-api/src/test/java/tech/ebp/oqm/core/api/interfaces/endpoints/inventory/items/InventoryItemsCrudTest.java
@@ -3,28 +3,53 @@ package tech.ebp.oqm.core.api.interfaces.endpoints.inventory.items;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import io.restassured.response.ValidatableResponse;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.time.StopWatch;
 import org.bson.types.ObjectId;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import tech.ebp.oqm.core.api.model.object.interactingEntity.user.User;
 import tech.ebp.oqm.core.api.model.object.storage.items.InventoryItem;
 import tech.ebp.oqm.core.api.service.mongo.InventoryItemService;
+import tech.ebp.oqm.core.api.service.serviceState.InstanceMutexService;
+import tech.ebp.oqm.core.api.service.serviceState.InstanceMutexServiceTest;
 import tech.ebp.oqm.core.api.testResources.data.InventoryItemTestObjectCreator;
 import tech.ebp.oqm.core.api.testResources.data.TestUserService;
 import tech.ebp.oqm.core.api.testResources.testClasses.RunningServerTest;
 
 import jakarta.inject.Inject;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static tech.ebp.oqm.core.api.testResources.TestConstants.DEFAULT_TEST_DB_NAME;
 import static tech.ebp.oqm.core.api.testResources.TestRestUtils.setupJwtCall;
 
@@ -59,26 +84,26 @@ class InventoryItemsCrudTest extends RunningServerTest {
 		String id = OBJECT_MAPPER.readValue(json, InventoryItem.class).getId().toHexString();
 		
 		String resultStr = setupJwtCall(given(), testUser.getAttributes().get(TestUserService.TEST_JWT_ATT_KEY))
-			.body(objectMapper.writeValueAsString(testObjectCreator.getTestObject()))
-			.contentType(ContentType.JSON)
-			.pathParam("oqmDbIdOrName", DEFAULT_TEST_DB_NAME)
-			.params(Map.of("id", id))
-			.get()
-			.then().statusCode(200)
-			.extract().body().asString();
+							   .body(objectMapper.writeValueAsString(testObjectCreator.getTestObject()))
+							   .contentType(ContentType.JSON)
+							   .pathParam("oqmDbIdOrName", DEFAULT_TEST_DB_NAME)
+							   .params(Map.of("id", id))
+							   .get()
+							   .then().statusCode(200)
+							   .extract().body().asString();
 		
 		ObjectNode resultNode = (ObjectNode) OBJECT_MAPPER.readTree(resultStr);
 		
 		assertEquals(1, resultNode.get("numResults").asInt());
 		
 		resultStr = setupJwtCall(given(), testUser.getAttributes().get(TestUserService.TEST_JWT_ATT_KEY))
-							   .body(objectMapper.writeValueAsString(testObjectCreator.getTestObject()))
-							   .contentType(ContentType.JSON)
-							   .pathParam("oqmDbIdOrName", DEFAULT_TEST_DB_NAME)
-							   .params(Map.of("id", new ObjectId().toHexString()))
-							   .get()
-							   .then().statusCode(200)
-							   .extract().body().asString();
+						.body(objectMapper.writeValueAsString(testObjectCreator.getTestObject()))
+						.contentType(ContentType.JSON)
+						.pathParam("oqmDbIdOrName", DEFAULT_TEST_DB_NAME)
+						.params(Map.of("id", new ObjectId().toHexString()))
+						.get()
+						.then().statusCode(200)
+						.extract().body().asString();
 		
 		resultNode = (ObjectNode) OBJECT_MAPPER.readTree(resultStr);
 		
@@ -126,7 +151,6 @@ class InventoryItemsCrudTest extends RunningServerTest {
 		//get again
 		response = setupJwtCall(given(), testUser.getAttributes().get(TestUserService.TEST_JWT_ATT_KEY))
 					   .when()
-					   .body(updates)
 					   .accept(ContentType.JSON)
 					   .pathParam("oqmDbIdOrName", DEFAULT_TEST_DB_NAME)
 					   .get(id)
@@ -140,5 +164,120 @@ class InventoryItemsCrudTest extends RunningServerTest {
 		assertEquals("mol", result.get("unit").get("string").asText());
 		assertEquals("mol", result.get("stats").get("total").get("unit").get("string").asText());
 	}
-
+	
+	public static Stream<Arguments> getParams() {
+		return Stream.of(
+			Arguments.of(2, 10),
+			Arguments.of(2, 10),
+			Arguments.of(3, 10),
+			Arguments.of(3, 10),
+			Arguments.of(5, 10),
+			Arguments.of(5, 10),
+			
+			Arguments.of(2, 20),
+			Arguments.of(2, 20),
+			Arguments.of(3, 20),
+			Arguments.of(3, 20),
+			Arguments.of(5, 20),
+			Arguments.of(5, 20),
+			
+			Arguments.of(10, 10),
+			Arguments.of(10, 10),
+			Arguments.of(20, 20),
+			Arguments.of(20, 20)
+		);
+	}
+	
+	@ParameterizedTest
+	@MethodSource("getParams")
+	public void basicThreadTest(int numThreads, int numIterations) throws InterruptedException, ExecutionException, JsonProcessingException {
+		User testUser = this.getTestUserService().getTestUser();
+		String json = setupJwtCall(given(), testUser.getAttributes().get(TestUserService.TEST_JWT_ATT_KEY))
+						  .body(objectMapper.writeValueAsString(testObjectCreator.getTestObject()))
+						  .contentType(ContentType.JSON)
+						  .pathParam("oqmDbIdOrName", DEFAULT_TEST_DB_NAME)
+						  .post()
+						  .then().statusCode(200)
+						  .extract().body().asString();
+		
+		log.info("Initial item: {}", json);
+		
+		String id = OBJECT_MAPPER.readValue(json, InventoryItem.class).getId().toHexString();
+		List<Future<Void>> futures = new ArrayList<>(numThreads);
+		ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
+		
+		
+		BasicItemUpdateTestThread.BasicItemUpdateTestThreadBuilder threadBuilder = BasicItemUpdateTestThread.builder()
+																					   .testUser(testUser)
+																					   .inventoryItemId(id)
+																					   .numIterations(numIterations);
+		
+		StopWatch overall = StopWatch.createStarted();
+		for (int i = 1; i <= numThreads; i++) {
+			threadBuilder.threadId("testThread-" + i);
+			
+			futures.add(executor.submit(threadBuilder.build()));
+		}
+		executor.shutdown();
+		while (!executor.awaitTermination(1, TimeUnit.MINUTES)) {
+			log.info("Still waiting on threads...");
+		}
+		overall.stop();
+		
+		
+		//TODO:: check results
+		ObjectNode result = setupJwtCall(given(), testUser.getAttributes().get(TestUserService.TEST_JWT_ATT_KEY))
+								.when()
+								.accept(ContentType.JSON)
+								.pathParam("oqmDbIdOrName", DEFAULT_TEST_DB_NAME)
+								.get(id)
+								.then()
+								.statusCode(200)
+								.extract().as(ObjectNode.class);
+		log.info("Got Result: {}", result);
+		
+	}
+	
+	
+	@Builder
+	@Slf4j
+	@AllArgsConstructor
+	static class BasicItemUpdateTestThread implements Callable<Void> {
+		
+		private String threadId;
+		private String inventoryItemId;
+		private User testUser;
+		
+		private int numIterations;
+		
+		@SneakyThrows
+		@Override
+		public Void call() {
+			log.info("Running test thread {}", this.threadId);
+			
+			ObjectNode updates = OBJECT_MAPPER.createObjectNode();
+			updates.putObject("attributes");
+			
+			for (int i = 1; i <= this.numIterations; i++) {
+				log.info("Thread {} waiting for lock on iteration {}", this.threadId, i);
+				
+				((ObjectNode) updates.get("attributes")).put(this.threadId, i);
+				
+				//initial update
+				ValidatableResponse response = setupJwtCall(given(), testUser.getAttributes().get(TestUserService.TEST_JWT_ATT_KEY))
+												   .when()
+												   .body(updates)
+												   .contentType(ContentType.JSON)
+												   .accept(ContentType.JSON)
+												   .pathParam("oqmDbIdOrName", DEFAULT_TEST_DB_NAME)
+												   .put(this.inventoryItemId)
+												   .then()
+												   .statusCode(200);
+				
+			}
+			log.info("DONE running test thread {}", this.threadId);
+			return null;
+		}
+	}
+	
 }
