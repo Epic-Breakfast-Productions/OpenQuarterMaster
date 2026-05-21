05ce88b9a6ebbd75f9058e602637b1062aee7214
GregJohnStewart <contact@gjstewart.net>
2026-05-21 12:48:06 -0400

    Core - Characteristics - Fixing potential issue in parsing ui entries

--- software/core/oqm-core-characteristics/app/UIs.py
+++ software/core/oqm-core-characteristics/app/UIs.py
@@ -35,7 +35,7 @@ class UiCache:
 	baseUri: str
 	icon: CachedImage | None
 	endpoints: UiEndpoints
-	
+
 	def to_response(self) -> UiResponse | None:
 		return UiResponse(
 			order=self.order,
@@ -64,7 +64,7 @@ class UisCache:
 	plugin: list[UiCache] = field(default_factory=list)
 	metrics: list[UiCache] = field(default_factory=list)
 	infra: list[UiCache] = field(default_factory=list)
-	
+
 	def to_response(self) -> UisResponse:
 		return UisResponse(
 			self.home,
@@ -73,7 +73,7 @@ class UisCache:
 			list(map(lambda c: c.to_response(), self.metrics)),
 			list(map(lambda c: c.to_response(), self.infra)),
 		)
-	
+
 	def getCategory(self, category: str):
 		if category == "core":
 			return self.core
@@ -89,18 +89,18 @@ class UisCache:
 class UiUtils:
 	ui_dir = os.getenv('CHARACTERISTICS_UI_DIR', '/data/uis/')
 	ui_cache = None
-	
+
 	@classmethod
 	def __get_ui_endpoints(cls, uiRaw) -> UiEndpoints:
 		return UiEndpoints(
 			uiRaw["monitorEndpoint"],
 			uiRaw["item"] if "item" in uiRaw else None,
 		)
-	
+
 	@classmethod
 	def __get_ui(cls, uiRaw) -> UiCache:
 		return UiCache(
-			uiRaw["order"],
+			uiRaw["order"] if "order" in uiRaw else 999,
 			uiRaw["id"] if "id" in uiRaw else None,
 			uiRaw["name"],
 			uiRaw["description"],
@@ -111,7 +111,7 @@ class UiUtils:
 			) if "icon" in uiRaw else None,
 			cls.__get_ui_endpoints(uiRaw)
 		)
-	
+
 	@classmethod
 	def validate_ui_list(cls, uiList: list[UiCache]) -> None:
 		# ensure id uniqueness
@@ -127,28 +127,28 @@ class UiUtils:
 				)
 			if len(filtered_list) != 1:
 				raise Exception("Invalid UI ID (duplicates): " + uiCache.id)
-	
+
 	@classmethod
 	def get_uis_cache(cls) -> UisCache:
 		if cls.ui_cache is not None:
 			return cls.ui_cache
-		
+
 		directory = os.getenv('UIS_DATA_DIR', "/data/uis/")
-		
+
 		uisRawData: list[dict] = list()
-		
+
 		for file in os.listdir(directory):
 			filename = os.path.basename(file)
 			if filename.endswith(".json"):
 				curUiFileLoc = os.path.join(directory, filename)
 				with open(curUiFileLoc) as curUiFile:
 					uisRawData.append(json.load(curUiFile))
-		
+
 		output: UisCache = UisCache(os.getenv('UIS_HOME_URL', " "))
-		
+
 		for rawUi in uisRawData:
 			uiCache = cls.__get_ui(rawUi)
-			
+
 			if rawUi["type"].casefold() == "core":
 				output.core.append(uiCache)
 			elif rawUi["type"].casefold() == "plugins":
@@ -159,25 +159,25 @@ class UiUtils:
 				output.infra.append(uiCache)
 			else:
 				print("WARN:: invalid type: " + rawUi["type"].casefold())
-		
+
 		cls.validate_ui_list(output.core)
 		cls.validate_ui_list(output.metrics)
 		cls.validate_ui_list(output.infra)
 		cls.validate_ui_list(output.plugin)
-		
+
 		output.core = sorted(output.core, key=lambda c: c.order)
 		output.metrics = sorted(output.metrics, key=lambda c: c.order)
 		output.infra = sorted(output.infra, key=lambda c: c.order)
 		output.plugin = sorted(output.plugin, key=lambda c: c.order)
-		
+
 		cls.ui_cache = output
-		
+
 		return output
-	
+
 	@classmethod
 	def get_uis_return(cls) -> UisResponse:
 		return cls.get_uis_cache().to_response()
-	
+
 	@classmethod
 	def get_ui_icon(cls, category: str, uiId: str) -> StreamingResponse:
 		try:
@@ -190,9 +190,9 @@ class UiUtils:
 			)
 		except ValueError as e:
 			raise HTTPException(status_code=404, detail="Invalid UI category: " + category)
-		
+
 		if len(ui) == 0:
 			raise HTTPException(status_code=404, detail="Invalid UI ID: " + uiId)
 		ui = ui[0]
-		
+
 		return ImageUtils.get_image_response(ui.icon)
