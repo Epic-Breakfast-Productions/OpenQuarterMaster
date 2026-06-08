8aae32a49c5ab4b5d70af636202e1daa12fedb3e
GregJohnStewart <contact@gjstewart.net>
2026-06-08 16:06:41 -0400

    Core - API - Minor tweak after review

--- software/core/oqm-core-api/src/main/java/tech/ebp/oqm/core/api/health/service/StartupHealthCheck.java
+++ software/core/oqm-core-api/src/main/java/tech/ebp/oqm/core/api/health/service/StartupHealthCheck.java
@@ -1,9 +1,9 @@
 package tech.ebp.oqm.core.api.health.service;
 
-import io.quarkus.runtime.Startup;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Instance;
 import jakarta.inject.Inject;
+import org.eclipse.microprofile.health.Startup;
 import tech.ebp.oqm.core.api.health.utils.GenericHealthCheck;
 import tech.ebp.oqm.core.api.health.utils.HasStartupCheck;
 import tech.ebp.oqm.core.api.health.utils.HealthStatus;
