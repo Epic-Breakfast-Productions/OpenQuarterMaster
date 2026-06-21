ca1d068159c6c3b877f6e6f3fbda37a5d0db948f
GregJohnStewart <contact@gjstewart.net>
2026-06-21 16:09:58 -0400

    Plugin - MSS Controller - Fleshing out of some test resources for module testing

--- software/plugins/mss-controller/src/main/java/tech/ebp/oqm/plugin/mssController/model/moduleComm/command/response/CommandResponse.java
+++ software/plugins/mss-controller/src/main/java/tech/ebp/oqm/plugin/mssController/model/moduleComm/command/response/CommandResponse.java
@@ -3,10 +3,12 @@ package tech.ebp.oqm.plugin.mssController.model.moduleComm.command.response;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
