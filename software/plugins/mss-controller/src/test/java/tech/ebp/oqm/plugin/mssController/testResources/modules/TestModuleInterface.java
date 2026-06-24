75eeb9ced68603687348b6e4e4f9ec5645adb943
GregJohnStewart <contact@gjstewart.net>
2026-06-23 23:38:18 -0400

    Plugin - MSS Controller - fixing some test impl

--- software/plugins/mss-controller/src/test/java/tech/ebp/oqm/plugin/mssController/testResources/modules/TestModuleInterface.java
+++ software/plugins/mss-controller/src/test/java/tech/ebp/oqm/plugin/mssController/testResources/modules/TestModuleInterface.java
@@ -1,10 +1,34 @@
 package tech.ebp.oqm.plugin.mssController.testResources.modules;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import tech.ebp.oqm.plugin.mssController.model.moduleComm.command.Command;
+
 import java.util.Optional;
 
 public abstract class TestModuleInterface implements AutoCloseable {
 
+	public abstract ObjectMapper getObjectMapper();
+
 	public abstract void send(String message);
 
+	public void send(Command command) throws JsonProcessingException {
+		this.send(
+			this.getObjectMapper().writeValueAsString(command)
+		);
+	}
+
 	public abstract Optional<String> receive();
+
+	public Optional<Command> receiveCommand() throws JsonProcessingException {
+		Optional<String> op = this.receive();
+
+		if (op.isEmpty()) {
+			return Optional.empty();
+		}
+
+		return Optional.of(
+			this.getObjectMapper().readValue(op.get(), Command.class)
+		);
+	}
 }
