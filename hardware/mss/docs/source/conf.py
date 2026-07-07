07a2fde28a561e3913d2ae0c117cafc827fc29f1
GregJohnStewart <contact@gjstewart.net>
2026-07-06 23:33:14 -0400

    Hardware - MSS - Initial placement of sphinx docs

---
+++ hardware/mss/docs/source/conf.py
@@ -0,0 +1,54 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'OQM MSS Hardware'
+copyright = '2026, Greg Stewart'
+author = 'Greg Stewart'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+numbered_sections = True
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+html_theme_options = {
+	"logo": "logo.svg",
+	"logo_name": True,
+	# 'show_related': True,
+
+	'github_user': 'Epic-Breakfast-Productions',
+	'github_repo': 'OpenQuarterMaster',
+	'github_banner': True,
+	"github_button": False,
+	"show_relbars": True,
+	# 'github_count': True,
+	# 'extra_nav_links': {
+	# 	'Index': 'genindex.html',
+	# }
+
+	'body_max_width': 'none',
+	'page_width': '90%',
+}
+html_sidebars = {
+	'**': [
+		'about.html',
+		'localtoc.html',
+		'navigation.html',
+		'searchbox.html',
+	]
+}
---
+++ hardware/mss/docs/source/index.rst
@@ -0,0 +1,17 @@
+.. OQM MSS HArdware documentation master file, created by
+   sphinx-quickstart on Mon Jul  6 23:13:09 2026.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+OQM MSS Hardware documentation
+==============================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
