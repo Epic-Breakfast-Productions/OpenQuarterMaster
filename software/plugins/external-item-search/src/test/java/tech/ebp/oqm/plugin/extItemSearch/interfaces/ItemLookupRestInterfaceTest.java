c507d15a8a8363c6eb51da8fb25e1c4a26a39b32
GregJohnStewart <contact@gjstewart.net>
2026-08-23 23:08:02 -0400

    Plugin - Ext Item Search - fix to URL's in configuration. Added basic tests

---
+++ software/plugins/external-item-search/src/test/java/tech/ebp/oqm/plugin/extItemSearch/interfaces/ItemLookupRestInterfaceTest.java
@@ -0,0 +1,140 @@
+package tech.ebp.oqm.plugin.extItemSearch.interfaces;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import tech.ebp.oqm.plugin.extItemSearch.model.SearchType;
+import tech.ebp.oqm.plugin.extItemSearch.model.lookupResult.ResultType;
+import tech.ebp.oqm.plugin.extItemSearch.testResources.RunningServerTest;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static tech.ebp.oqm.plugin.extItemSearch.service.extItemSearchService.utils.LookupSource.BARCODE_LOOKUP;
+import static tech.ebp.oqm.plugin.extItemSearch.service.extItemSearchService.utils.LookupSource.DATAKICK;
+
+@Slf4j
+@QuarkusTest
+@TestHTTPEndpoint(ItemLookupRestInterface.class)
+public class ItemLookupRestInterfaceTest extends RunningServerTest {
+
+	private ObjectMapper objectMapper = new ObjectMapper();
+
+	@Test
+	void providersList() {
+		String providersList = given()
+								   .when().get("info/providers")
+								   .then()
+								   .statusCode(200)
+								   .extract().body().asString();
+
+		log.info("Got providers list: {}", providersList);
+
+		//TODO:: validate
+	}
+
+	@Test
+	void methodsList() {
+		String providersList = given()
+								   .when().get("info/methods")
+								   .then()
+								   .statusCode(200)
+								   .extract().body().asString();
+
+		log.info("Got search methods list: {}", providersList);
+
+		//TODO:: validate
+	}
+
+	public static Stream<Arguments> getSearches(){
+		return Stream.of(
+			Arguments.of(
+				Map.of(
+					"lookupMethod", SearchType.BARCODE,
+					"q", "foo"
+				),
+				List.of(
+				)
+			),
+			Arguments.of(
+				Map.of(
+					"lookupMethod",  SearchType.BARCODE,
+					"q", "00888109010058"
+				),
+				List.of(
+					new Result(DATAKICK.name(), ResultType.SUCCESS.name())
+				)
+			),
+			Arguments.of(
+				Map.of(
+					"lookupMethod",  SearchType.BARCODE,
+					"q", "886736874135"
+				),
+				List.of(
+					new Result(BARCODE_LOOKUP.name(), ResultType.SUCCESS.name())
+				)
+			)
+//			Arguments.of(
+//				Map.of(
+//					"lookupMethod",  SearchType.TEXT,
+//					"q", "GPS"
+//				),
+//				List.of(
+//					new Result(BARCODE_LOOKUP.name(), ResultType.SUCCESS.name())
+//				)
+//			)
+		);
+	}
+
+	public record Result(
+		String source,
+		String type
+	) {}
+
+	@ParameterizedTest
+	@MethodSource("getSearches")
+	public void searchTest(
+		Map<String, ?> parameters,
+		List<Result> expected
+	) throws JsonProcessingException {
+		String searchResultsStr = given()
+								   .when()
+									  .params(parameters)
+									  .get("search")
+								   .then()
+								   .statusCode(200)
+								   .extract().body().asString();
+		log.info("Search results: {}", searchResultsStr);
+
+		ArrayNode results = (ArrayNode) objectMapper.readTree(searchResultsStr);
+
+		assertEquals(expected.size(), results.size(), "Wrong number of search results.");
+
+		for(Result curExpected : expected){
+			boolean found = false;
+			for(JsonNode curResult : results){
+				if(
+					curExpected.source().equals(curResult.get("source").asText())
+				){
+					found = true;
+					assertEquals(curExpected.type(), curResult.get("type").asText());
+					break;
+				}
+			}
+			assertTrue(found, "Did not find expected result.");
+		}
+	}
+
+}
