ea2c2c5fd45162f85b3b9c5917c52d2e0f7da939
GregJohnStewart <contact@gjstewart.net>
2026-09-09 22:01:32 -0400

    Core - API - changed "objectName" to "objectType" in EventNotificationWrapper

--- software/core/oqm-core-api/src/main/java/tech/ebp/oqm/core/api/model/messaging/EventNotificationWrapper.java
+++ software/core/oqm-core-api/src/main/java/tech/ebp/oqm/core/api/model/messaging/EventNotificationWrapper.java
@@ -12,7 +12,7 @@ import tech.ebp.oqm.core.api.model.object.history.ObjectHistoryEvent;
 @Setter(AccessLevel.PRIVATE)
 public class EventNotificationWrapper {
 	private ObjectId database;
-	private String objectName;
+	private String objectType;
 	private ObjectHistoryEvent event;
 
 	@JsonProperty(access = JsonProperty.Access.READ_ONLY)
