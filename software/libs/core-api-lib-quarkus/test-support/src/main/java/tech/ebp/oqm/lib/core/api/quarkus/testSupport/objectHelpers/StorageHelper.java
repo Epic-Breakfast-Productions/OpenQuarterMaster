090c8455b060099bc0abc00a3b39eb4a235693a0
GregJohnStewart <contact@gjstewart.net>
2026-09-17 15:59:15 -0400

    Lib - Core API for Quarkus - Added storageHelper placeholder

---
+++ software/libs/core-api-lib-quarkus/test-support/src/main/java/tech/ebp/oqm/lib/core/api/quarkus/testSupport/objectHelpers/StorageHelper.java
@@ -0,0 +1,10 @@
+package tech.ebp.oqm.lib.core.api.quarkus.testSupport.objectHelpers;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import tech.ebp.oqm.lib.core.api.quarkus.testSupport.CoreApiLibClientHelper;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class StorageHelper extends CoreApiLibClientHelper {
+
+}
